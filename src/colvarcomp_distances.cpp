9b42e329aff686e0759d4a91c060b40cfaff901d
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2015-01-27 21:14:39 -0500

    Fixed handling of weights by cartesian

--- src/colvarcomp_distances.cpp
+++ src/colvarcomp_distances.cpp
@@ -1319,16 +1319,17 @@ void colvar::cartesian::calc_value()
 {
   size_t const dim = axes.size();
   size_t ia, j;
-  for (ia = 0; ia < atoms.size(); ia++) {
-    for (j = 0; j < dim; j++) {
-      x.vector1d_value[dim*ia + j] = atoms[ia].pos[axes[j]];
-    }
-  }
 
   if (atoms.weights.size()) {
     for (ia = 0; ia < atoms.size(); ia++) {
       for (j = 0; j < dim; j++) {
-        x.vector1d_value[dim*ia + j] *= atoms.weights[ia];
+        x.vector1d_value[dim*ia + j] *= atoms[ia].pos[axes[j]] * atoms.weights[ia];
+      }
+    }
+  } else {
+    for (ia = 0; ia < atoms.size(); ia++) {
+      for (j = 0; j < dim; j++) {
+        x.vector1d_value[dim*ia + j] = atoms[ia].pos[axes[j]];
       }
     }
   }
