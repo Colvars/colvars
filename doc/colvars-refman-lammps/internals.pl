f58243e1525734fdb47fa322203d7d92970bf37a
Jérôme Hénin <heninj@gmail.com>
2015-03-03 19:25:31 +0100

    Fix index in docs; split html into fewer pages

--- doc/colvars-refman-lammps/internals.pl
+++ doc/colvars-refman-lammps/internals.pl
@@ -2,284 +2,284 @@
 # Associate internals original text with physical files.
 
 
-$key = q/cite_Henin2004/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:cvc_superp/;
-$ref_files{$key} = "$dir".q|node51.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Ruiz-Montero1997/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvars_config/;
-$ref_files{$key} = "$dir".q|node8.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_alb/;
-$ref_files{$key} = "$dir".q|node69.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:cvc_eigenvector/;
-$ref_files{$key} = "$dir".q|node38.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvar_acf/;
-$ref_files{$key} = "$dir".q|node16.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvars_mdengine_parameters/;
-$ref_files{$key} = "$dir".q|node7.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_meta/;
-$ref_files{$key} = "$dir".q|node58.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvar_extended/;
-$ref_files{$key} = "$dir".q|node15.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Laio2002/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Grubmuller1995/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvar_atom_groups/;
-$ref_files{$key} = "$dir".q|node17.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Pitera2012/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_us/;
-$ref_files{$key} = "$dir".q|node66.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Iannuzzi2003/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_meta_mr/;
-$ref_files{$key} = "$dir".q|node62.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvar_atom_groups_ref_frame/;
-$ref_files{$key} = "$dir".q|node19.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:cvc_selfCoordNum/;
-$ref_files{$key} = "$dir".q|node34.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvars_input/;
-$ref_files{$key} = "$dir".q|node9.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:colvar_inertia_z/;
-$ref_files{$key} = "$dir".q|node41.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:cvc_rmsd/;
-$ref_files{$key} = "$dir".q|node36.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:colvar_inertia/;
-$ref_files{$key} = "$dir".q|node40.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_abf/;
-$ref_files{$key} = "$dir".q|node53.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Huber1994/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_histogram/;
-$ref_files{$key} = "$dir".q|node70.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_White2014/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_abf_req/;
-$ref_files{$key} = "$dir".q|node54.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:colvars_meta_fes/;
-$ref_files{$key} = "$dir".q|node58.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvars_output/;
-$ref_files{$key} = "$dir".q|node10.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/cite_Raiteri2005/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_smd/;
-$ref_files{$key} = "$dir".q|node65.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Darve2008/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:gradient_vector/;
-$ref_files{$key} = "$dir".q|node53.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:ortho_gradient/;
-$ref_files{$key} = "$dir".q|node53.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:abf/;
-$ref_files{$key} = "$dir".q|node53.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Ciccotti2005/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvar/;
-$ref_files{$key} = "$dir".q|node11.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Henin2010/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias/;
-$ref_files{$key} = "$dir".q|node52.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarmodule/;
-$ref_files{$key} = "$dir".q|node6.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:gradient/;
-$ref_files{$key} = "$dir".q|node53.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvar_atom_groups_sel/;
-$ref_files{$key} = "$dir".q|node18.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvar_atom_groups_scaling/;
-$ref_files{$key} = "$dir".q|node21.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/section:colvars/;
-$ref_files{$key} = "$dir".q|node5.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_meta_output/;
-$ref_files{$key} = "$dir".q|node59.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_meta_performance/;
-$ref_files{$key} = "$dir".q|node60.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Fiorin2013/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_well_tempered/;
-$ref_files{$key} = "$dir".q|node61.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:distanceInv/;
-$ref_files{$key} = "$dir".q|node29.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:colvars_diagram/;
-$ref_files{$key} = "$dir".q|node5.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_ti/;
-$ref_files{$key} = "$dir".q|node67.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Deng2009/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_linear/;
-$ref_files{$key} = "$dir".q|node68.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_denOtter2000/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:cvc/;
-$ref_files{$key} = "$dir".q|node22.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvarbias_harmonic/;
-$ref_files{$key} = "$dir".q|node64.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Bussi2006/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:colvars_meta_pot/;
-$ref_files{$key} = "$dir".q|node58.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvar_general/;
-$ref_files{$key} = "$dir".q|node12.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:colvar_atom_groups_wrapping/;
-$ref_files{$key} = "$dir".q|node20.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Coutsias2004/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:colvar_basic/;
-$ref_files{$key} = "$dir".q|node5.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:colvar_gyration/;
-$ref_files{$key} = "$dir".q|node39.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:ortho_constraints/;
-$ref_files{$key} = "$dir".q|node53.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Carter1989/;
-$ref_files{$key} = "$dir".q|node72.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/eq:cvc_coordNum/;
 $ref_files{$key} = "$dir".q|node33.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/eq:free/;
-$ref_files{$key} = "$dir".q|node53.html|; 
+$key = q/cite_Grubmuller1995/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvar_atom_groups_ref_frame/;
+$ref_files{$key} = "$dir".q|node18.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvar_atom_groups_wrapping/;
+$ref_files{$key} = "$dir".q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:cvc_superp/;
+$ref_files{$key} = "$dir".q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Bussi2006/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_meta_output/;
+$ref_files{$key} = "$dir".q|node27.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_Barducci2008/;
-$ref_files{$key} = "$dir".q|node72.html|; 
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvar_atom_groups_scaling/;
+$ref_files{$key} = "$dir".q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_abf/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvars_mdengine_parameters/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:gradient_vector/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:abf/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Carter1989/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:colvar_basic/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Darve2008/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Laio2002/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvar/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_abf_req/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Henin2004/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Iannuzzi2003/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:colvar_inertia_z/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Ruiz-Montero1997/;
+$ref_files{$key} = "$dir".q|node33.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/eq:colvar_combination/;
-$ref_files{$key} = "$dir".q|node51.html|; 
+$ref_files{$key} = "$dir".q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:colvars/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_meta_mr/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Fiorin2013/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Coutsias2004/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Pitera2012/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_smd/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:cvc_eigenvector/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:colvars_meta_fes/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvar_extended/;
+$ref_files{$key} = "$dir".q|node14.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvar_atom_groups/;
+$ref_files{$key} = "$dir".q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:cvc_rmsd/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_linear/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Huber1994/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Henin2010/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_harmonic/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:colvar_inertia/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:colvars_diagram/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:gradient/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_denOtter2000/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Ciccotti2005/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_White2014/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvar_acf/;
+$ref_files{$key} = "$dir".q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvars_config/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvars_input/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:ortho_gradient/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:free/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_histogram/;
+$ref_files{$key} = "$dir".q|node31.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:distanceInv/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_meta/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_well_tempered/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:cvc/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_us/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:colvar_gyration/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_ti/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarmodule/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_meta_performance/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:cvc_coordNum/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:ortho_constraints/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvars_output/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvar_atom_groups_sel/;
+$ref_files{$key} = "$dir".q|node17.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:colvars_meta_pot/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvar_general/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Deng2009/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:cvc_selfCoordNum/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:colvarbias_alb/;
+$ref_files{$key} = "$dir".q|node30.html|; 
 $noresave{$key} = "$nosave";
 
 1;
