7fcbdd59d890d9e6a402e24e13168ab6ac1abd6f
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2016-07-06 01:27:07 -0400

    Make reg test inputs code-independent (issue #57)

---
+++ namd/tests/library/Common/gen_index_file.tcl
@@ -0,0 +1,58 @@
+proc write_index_group { output sel label { n_atoms_max 0 } } {
+    if { [${sel} num] == 0 } return
+    puts stderr "Writing index group \"${label}\": [${sel} num] atoms"
+    puts -nonewline ${output} [format "\[ %s \]\n" ${label}]
+    set line_buf 0
+    set atom_count 0
+    foreach num [${sel} get serial] {
+        incr atom_count
+        if { (${n_atoms_max} > 0) && (${atom_count} > ${n_atoms_max}) } break
+        puts -nonewline ${output} [format " %9d" ${num}]
+        set line_buf [expr ${line_buf} + 10]
+        if { ${line_buf} > 70 } {
+            set line_buf 0
+            puts -nonewline ${output} "\n"
+        }
+    }
+    if { ${line_buf} > 0 } {
+        puts -nonewline ${output} "\n"
+    }
+    puts -nonewline ${output} "\n"
+}
+
+
+set protein_sel "((protein) or (segid BH))" 
+
+
+set index_file [open "da.ndx" "w"]
+
+write_index_group ${index_file} \
+    [atomselect top "${protein_sel}"] \
+    "Protein"
+write_index_group ${index_file} \
+    [atomselect top "(${protein_sel} and (noh))"]\
+    "Protein_noH"
+write_index_group ${index_file} \
+    [atomselect top "(${protein_sel} and (backbone))"] \
+    "Protein_Backbone"
+write_index_group ${index_file} \
+    [atomselect top "(${protein_sel} and (alpha))"] \
+    "Protein_C-alpha"
+
+write_index_group ${index_file} \
+    [atomselect top "(${protein_sel} and (alpha) and resid 1 2)"] \
+    "Protein_C-alpha_1_2"
+write_index_group ${index_file} \
+    [atomselect top "(${protein_sel} and (alpha) and resid 9 10)"] \
+    "Protein_C-alpha_9_10"
+
+for { set resid 1 } { ${resid} <= 10 } { incr resid } {
+    write_index_group ${index_file} \
+        [atomselect top "(${protein_sel} and (alpha) and resid ${resid})"] \
+        "Protein_C-alpha_${resid}"
+}
+
+close ${index_file}
+
+#quit
+
