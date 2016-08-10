4df07e25f51b3e59d358dd714042536e2ebd61ea
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2016-08-10 00:25:05 -0400

    Allow setting a feature state while parsing its kewyord
    An example is implemented for debugGradients (TODO: get rid of more bool temporaries).
    
    Note 1: the new overloaded get_keyval() will call enable() through the calling deps object.  Is there a case where this may not be wanted?
    
    Note 2: the feature_state class now holds a pointer to the deps object: this is to avoid inheriting colvarparse from deps, but that may be standard behavior in the long run?

--- src/colvarparse.cpp
+++ src/colvarparse.cpp
@@ -354,6 +354,21 @@ bool colvarparse::get_keyval(std::string const &conf,
 }
 
 
+bool colvarparse::get_keyval(std::string const &conf,
+                             char const *key,
+                             cvm::deps::feature_state *value,
+                             bool const &def_value,
+                             Parse_Mode const parse_mode)
+{
+  bool feature_flag = def_value;
+  bool const b_found = get_keyval(conf, key, feature_flag, def_value, parse_mode);
+  if (feature_flag) {
+    value->object()->enable(value->feature_id());
+  }
+  return b_found;
+}
+
+
 // multiple-value keyword parsers
 
 bool colvarparse::get_keyval(std::string const &conf,
