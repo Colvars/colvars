b58d8cd497e8caa08eed48910151934d43784081
Jérôme Hénin <heninj@gmail.com>
2017-09-08 15:35:39 +0200

    Function to check for overlapping groups

--- src/colvaratoms.h
+++ src/colvaratoms.h
@@ -279,6 +279,10 @@ public:
   /// Allocates and populates the sorted list of atom ids
   int create_sorted_ids(void);
 
+  /// Detect whether two groups share atoms
+  /// If yes, returns 1-based number of a common atom; else, returns 0
+  static int overlap(const atom_group &g1, const atom_group &g2);
+
   /// \brief When updating atomic coordinates, translate them to align with the
   /// center of mass of the reference coordinates
   bool b_center;
