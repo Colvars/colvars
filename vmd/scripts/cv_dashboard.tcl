1d62faf08fcd472032bac16603d227e6dbbea534
Jérôme Hénin <heninj@gmail.com>
2018-07-17 10:59:13 +0200

    UI improvements in CV Dashboard

--- vmd/scripts/cv_dashboard.tcl
+++ vmd/scripts/cv_dashboard.tcl
@@ -1,10 +1,10 @@
-# GUI for the Colvars Module in VMD
+# Colvars Dashboard -- based on the Colvars Module for VMD
 # Jérôme Hénin <henin@ibpc.fr> 2018
 
 # At this stage, only acts on the "top" molecule
 # could query cvm to know what molecule is concerned
 
-namespace eval ::cvgui {
+namespace eval ::cv_dashboard {
   variable current_frame 0
   variable cv_values {}
   variable cvs {}
@@ -18,7 +18,7 @@ namespace eval ::cvgui {
 # Call the "cv" interface to Colvars, catching errors and displaying them to the user
 proc run_cv args  {
   if [ catch { cv {*}$args } res ] {
-    tk_messageBox -icon error -title "Colvars error" -parent .cvgui_window\
+    tk_messageBox -icon error -title "Colvars error" -parent .cv_dashboard_window\
       -message "Error running command:\n\n$args" -detail "$res"
     return -1
   }
@@ -27,34 +27,34 @@ proc run_cv args  {
 
 
 # Refresh the table with a list of existing CVs and their values
-proc ::cvgui::refresh_table {} {
-  set w .cvgui_window
-  set ::cvgui::cvs [run_cv list]
+proc ::cv_dashboard::refresh_table {} {
+  set w .cv_dashboard_window
+  set ::cv_dashboard::cvs [run_cv list]
   update_frame vmd_frame [molinfo top] w
 }
 
-proc ::cvgui::refresh_values {} {
+proc ::cv_dashboard::refresh_values {} {
   run_cv update
-  set w .cvgui_window
-  set ::cvgui::cv_values {}
+  set w .cv_dashboard_window
+  set ::cv_dashboard::cv_values {}
   foreach c [run_cv list] {
-    lappend ::cvgui::cv_values [format_value [run_cv colvar $c value] ]
+    lappend ::cv_dashboard::cv_values [format_value [run_cv colvar $c value] ]
   }
 }
 
-proc ::cvgui::update_frame { name molid op } {
+proc ::cv_dashboard::update_frame { name molid op } {
   # name == vmd_frame
   # molid == molecule id of the newly changed frame
   # op == w
   global vmd_frame
-  variable ::cvgui::plothandle
+  variable ::cv_dashboard::plothandle
 
   if { $molid != [molinfo top] } {
     return
   }
   set f [molinfo $molid get frame]
 
-  set ::cvgui::current_frame $f
+  set ::cv_dashboard::current_frame $f
   run_cv frame $f
   refresh_values
 
@@ -82,7 +82,7 @@ proc ::cvgui::update_frame { name molid op } {
 }
 
 
-proc ::cvgui::format_value val {
+proc ::cv_dashboard::format_value val {
   if {[llength $val] == 1} {
     return [format "%.4g" $val]
   } else {
@@ -96,32 +96,32 @@ proc ::cvgui::format_value val {
 }
 
 
-proc ::cvgui::selected {} {
-  set w .cvgui_window
+proc ::cv_dashboard::selected {} {
+  set w .cv_dashboard_window
   set l {}
   refresh_table ;# to make sure selected colvars haven't been deleted
   foreach i [$w.cvtable curselection] {
-    lappend l [lindex $::cvgui::cvs $i]
+    lappend l [lindex $::cv_dashboard::cvs $i]
   }
   return $l
 }
 
 
 # Enable or disable real-time tracking of VMD frame
-proc ::cvgui::change_track_frame {} {
+proc ::cv_dashboard::change_track_frame {} {
   global vmd_frame
 
   set molid [molinfo top]
-  if {$::cvgui::track_frame} {
-    trace add variable vmd_frame($molid) write ::cvgui::update_frame
+  if {$::cv_dashboard::track_frame} {
+    trace add variable vmd_frame($molid) write ::cv_dashboard::update_frame
     update_frame vmd_frame [molinfo top] w
   } else {
-    trace remove variable vmd_frame($molid) write ::cvgui::update_frame
+    trace remove variable vmd_frame($molid) write ::cv_dashboard::update_frame
   }
 }
 
 # Load config from file
-proc ::cvgui::load {} {
+proc ::cv_dashboard::load {} {
   set path [tk_getOpenFile -filetypes {{"Colvars cfg" .in} {"Colvars cfg" .colvars}}]
   if [string compare $path ""] {
     run_cv configfile $path
@@ -131,7 +131,7 @@ proc ::cvgui::load {} {
 
 
 # Save config of colvars to file (can we do it w/ biases ? need bias type keyword)
-proc ::cvgui::save {} {
+proc ::cv_dashboard::save {} {
 
   set path [tk_getSaveFile -filetypes {{"Colvars cfg" .in} {"Colvars cfg" .colvars}}]
 
@@ -150,7 +150,7 @@ proc ::cvgui::save {} {
 }
 
 
-proc ::cvgui::del {} {
+proc ::cv_dashboard::del {} {
 
   foreach c [selected] {
     # workaround bug in colvars pre-2018-07-02
@@ -168,49 +168,63 @@ proc ::cvgui::del {} {
 }
 
 
-proc ::cvgui::add {} {
+proc ::cv_dashboard::add {} {
   edit true
 }
 
 
 # Enable or disable real-time tracking of VMD frame
-proc ::cvgui::edit { {add false} } {
+proc ::cv_dashboard::edit { {add false} } {
   set cfg ""
 
   if $add {
     # do not remove existing vars
     set cvs {}
+    # Provide simple template
     set cfg "colvar {\n  name d\n  distance {\n    group1 { atomNumbers 1 2 }\n    group2 { atomNumbers 3 4 }\n  }\n}\n"
   } else {
     set cvs [selected]
+    if {[llength $cvs] == 0} {
+      # If no selection, edit all variables
+      set cvs $::cv_dashboard::cvs
+    }
     foreach c $cvs {
       append cfg "colvar {"
       append cfg [run_cv colvar $c getconfig]
       append cfg "}\n\n"
     }
-    set ::cvgui::backup_cfg $cfg
+    set ::cv_dashboard::backup_cfg $cfg
   }
-  set w .cvgui_window
+  set w .cv_dashboard_window
   set editor [toplevel $w.editor]
-  tk::text $w.editor.text
+  wm title $editor "Colvar config editor"
+  tk::text $w.editor.text -undo 1 -yscrollcommand [list $w.editor.vsb set]
+  ttk::scrollbar $w.editor.vsb -orient vertical -command [list $w.editor.text yview]
+
   $w.editor.text insert 1.0 $cfg
-  set ::cvgui::being_edited $cvs
+  set ::cv_dashboard::being_edited $cvs
 
   grid $w.editor.text -row 0 -columnspan 2 -sticky nsew
+  grid $w.editor.vsb -row 0 -column 2 -sticky ns
 
-  ttk::button $w.editor.apply -text "Apply" -command ::cvgui::edit_apply
-  ttk::button $w.editor.cancel -text "Cancel" -command ::cvgui::edit_cancel
-  grid $w.editor.apply -row 1 -column 0 -sticky e
-  grid $w.editor.cancel -row 1 -column 1 -sticky w
+  set gridrow 1
+
+  ttk::button $w.editor.apply -text "Apply" -command ::cv_dashboard::edit_apply
+  ttk::button $w.editor.cancel -text "Cancel" -command ::cv_dashboard::edit_cancel
+  grid $w.editor.apply -row $gridrow -column 0 -sticky e
+  grid $w.editor.cancel -row $gridrow -column 1 -sticky w
+
   grid columnconfigure $w.editor 0 -weight 1
   grid columnconfigure $w.editor 1 -weight 1
   grid rowconfigure $w.editor 0 -weight 1
+
+
 }
 
 
-proc ::cvgui::edit_apply {} {
-  set w .cvgui_window
-  foreach c $::cvgui::being_edited {
+proc ::cv_dashboard::edit_apply {} {
+  set w .cv_dashboard_window
+  foreach c $::cv_dashboard::being_edited {
     puts "Deleting $c"
     run_cv colvar $c delete
     puts [cv list]
@@ -220,30 +234,30 @@ proc ::cvgui::edit_apply {} {
     set res [run_cv config $cfg]
     if { [string compare $res ""] } {
       # error: restore backed up cfg
-      run_cv config $::cvgui::backup_cfg
+      run_cv config $::cv_dashboard::backup_cfg
     }
   }
-  set ::cvgui::being_edited {}
+  set ::cv_dashboard::being_edited {}
   destroy $w.editor
   refresh_table
 }
 
 
-proc ::cvgui::reset {} {
+proc ::cv_dashboard::reset {} {
   run_cv reset
   refresh_table
 }
 
 
-proc ::cvgui::edit_cancel {} {
-  set w .cvgui_window
-  set ::cvgui::being_edited {}
+proc ::cv_dashboard::edit_cancel {} {
+  set w .cv_dashboard_window
+  set ::cv_dashboard::being_edited {}
   destroy $w.editor
 }
 
 
-proc ::cvgui::plot {} {
-  variable ::cvgui::plothandle
+proc ::cv_dashboard::plot {} {
+  variable ::cv_dashboard::plothandle
 
   set nf [molinfo top get numframes]
   set x {}
@@ -252,7 +266,7 @@ proc ::cvgui::plot {} {
   set cvs [selected]
   if { [llength $cvs] == 0 } {
     # If no selection, plot all variables
-    set cvs [run_cv list]
+    set cvs $::cv_dashboard::cvs
     if { [llength $cvs] == 0 } {
       return
     }
@@ -282,7 +296,7 @@ proc ::cvgui::plot {} {
       }
     }
   }
-  set plothandle [multiplot -title {Colvars trajectory   [left-click, arrows (+ Shift/Ctrl) to navigate & zoom, v/h to fit vert/horizontally]} \
+  set plothandle [multiplot -title {Colvars trajectory   [left-click, keyb arrows (+ Shift/Ctrl) to navigate & zoom, v/h to fit vert/horizontally]} \
       -xlabel "Frame" -ylabel "Value" -nostats]
   foreach c $cvs {
     foreach n $names($c) {
@@ -292,26 +306,26 @@ proc ::cvgui::plot {} {
   $plothandle replot
 
   # bind the plot window to a callback that will change the frame whenever the user clicks the plot
-  set plot_ns [namespace qualifiers $::cvgui::plothandle]
-  bind [set ${plot_ns}::w] <Button-1> { ::cvgui::plot_clicked %x %y }
-  bind [set ${plot_ns}::w] <Left> { ::cvgui::chg_frame -1 }
-  bind [set ${plot_ns}::w] <Right> { ::cvgui::chg_frame 1 }
-  bind [set ${plot_ns}::w] <Shift-Left> { ::cvgui::chg_frame -10 }
-  bind [set ${plot_ns}::w] <Shift-Right> { ::cvgui::chg_frame 10 }
-  bind [set ${plot_ns}::w] <Control-Left> { ::cvgui::chg_frame -50 }
-  bind [set ${plot_ns}::w] <Control-Right> { ::cvgui::chg_frame 50 }
-  bind [set ${plot_ns}::w] <Up>  { ::cvgui::zoom 0.25 }
-  bind [set ${plot_ns}::w] <Down>  { ::cvgui::zoom 4 }
-  bind [set ${plot_ns}::w] <Shift-Up>  { ::cvgui::zoom 0.0625 }
-  bind [set ${plot_ns}::w] <Shift-Down>  { ::cvgui::zoom 16 }
-  bind [set ${plot_ns}::w] <v> { ::cvgui::fit_vertically }
-  bind [set ${plot_ns}::w] <h> { ::cvgui::fit_horizontally }
+  set plot_ns [namespace qualifiers $::cv_dashboard::plothandle]
+  bind [set ${plot_ns}::w] <Button-1> { ::cv_dashboard::plot_clicked %x %y }
+  bind [set ${plot_ns}::w] <Left> { ::cv_dashboard::chg_frame -1 }
+  bind [set ${plot_ns}::w] <Right> { ::cv_dashboard::chg_frame 1 }
+  bind [set ${plot_ns}::w] <Shift-Left> { ::cv_dashboard::chg_frame -10 }
+  bind [set ${plot_ns}::w] <Shift-Right> { ::cv_dashboard::chg_frame 10 }
+  bind [set ${plot_ns}::w] <Control-Left> { ::cv_dashboard::chg_frame -50 }
+  bind [set ${plot_ns}::w] <Control-Right> { ::cv_dashboard::chg_frame 50 }
+  bind [set ${plot_ns}::w] <Up>  { ::cv_dashboard::zoom 0.25 }
+  bind [set ${plot_ns}::w] <Down>  { ::cv_dashboard::zoom 4 }
+  bind [set ${plot_ns}::w] <Shift-Up>  { ::cv_dashboard::zoom 0.0625 }
+  bind [set ${plot_ns}::w] <Shift-Down>  { ::cv_dashboard::zoom 16 }
+  bind [set ${plot_ns}::w] <v> { ::cv_dashboard::fit_vertically }
+  bind [set ${plot_ns}::w] <h> { ::cv_dashboard::fit_horizontally }
 }
 
 
-proc ::cvgui::plot_clicked { x y } {
+proc ::cv_dashboard::plot_clicked { x y } {
 
-  set ns [namespace qualifiers $::cvgui::plothandle]
+  set ns [namespace qualifiers $::cv_dashboard::plothandle]
   set xplotmin [set ${ns}::xplotmin]
   set xplotmax [set ${ns}::xplotmax]
   set yplotmin [set ${ns}::yplotmin]
@@ -328,21 +342,21 @@ proc ::cvgui::plot_clicked { x y } {
   refresh_table
 }
 
-proc ::cvgui::chg_frame { shift } {
-  set f [expr $::cvgui::current_frame + $shift]
+proc ::cv_dashboard::chg_frame { shift } {
+  set f [expr $::cv_dashboard::current_frame + $shift]
   catch "animate goto $f"
   refresh_table
 }
 
 
-proc ::cvgui::zoom { factor } {
-  variable ::cvgui::plothandle
+proc ::cv_dashboard::zoom { factor } {
+  variable ::cv_dashboard::plothandle
 
   set ns [namespace qualifiers $plothandle]
   set xmin [set ${ns}::xmin]
   set xmax [set ${ns}::xmax]
 
-  set f $::cvgui::current_frame
+  set f $::cv_dashboard::current_frame
   # rescale current half-width
   set half_width [expr { int (($xmax - $xmin) * $factor / 2)}]
   if { $half_width < 1 } {
@@ -361,8 +375,8 @@ proc ::cvgui::zoom { factor } {
 }
 
 
-proc ::cvgui::fit_vertically {} {
-  variable ::cvgui::plothandle
+proc ::cv_dashboard::fit_vertically {} {
+  variable ::cv_dashboard::plothandle
 
   set ns [namespace qualifiers $plothandle]
   set xmin [set ${ns}::xmin]
@@ -382,8 +396,8 @@ proc ::cvgui::fit_vertically {} {
 }
 
 
-proc ::cvgui::fit_horizontally {} {
-  variable ::cvgui::plothandle
+proc ::cv_dashboard::fit_horizontally {} {
+  variable ::cv_dashboard::plothandle
 
   set ns [namespace qualifiers $plothandle]
   set xdata [$plothandle xdata]
@@ -395,16 +409,16 @@ proc ::cvgui::fit_horizontally {} {
 
 
 # The main window
-proc ::cvgui::createWindow {} {
-  set w [toplevel .cvgui_window]
+proc ::cv_dashboard::createWindow {} {
+  set w [toplevel .cv_dashboard_window]
 
   wm title $w "Colvars dashboard"
   wm protocol $w WM_DELETE_WINDOW {
     # window destructor that removes the trace we put in place, so they don't accumulate
     # if loaded multiple times
     set molid [molinfo top]
-    trace remove variable vmd_frame($molid) write ::cvgui::update_frame
-    destroy .cvgui_window
+    trace remove variable vmd_frame($molid) write ::cv_dashboard::update_frame
+    destroy .cv_dashboard_window
   }
 
   # setup Colvars if not already there
@@ -412,16 +426,16 @@ proc ::cvgui::createWindow {} {
     run_cv molid top
   }
   set gridrow 0
-  grid [ttk::button $w.load -text "Load file" -command ::cvgui::load -padding "2 0 2 0"] -row $gridrow -column 0 -pady 5 -padx 2 -sticky nsew
-  grid [ttk::button $w.save -text "Save colvars" -command ::cvgui::save -padding "2 0 2 0"] -row $gridrow -column 1 -pady 5 -padx 2 -sticky nsew
-  grid [ttk::button $w.reset -text "Reset" -command ::cvgui::reset -padding "2 0 2 0"] -row $gridrow -column 2 -pady 5 -padx 2 -sticky nsew
+  grid [ttk::button $w.load -text "Load file" -command ::cv_dashboard::load -padding "2 0 2 0"] -row $gridrow -column 0 -pady 5 -padx 2 -sticky nsew
+  grid [ttk::button $w.save -text "Save colvars" -command ::cv_dashboard::save -padding "2 0 2 0"] -row $gridrow -column 1 -pady 5 -padx 2 -sticky nsew
+  grid [ttk::button $w.reset -text "Reset" -command ::cv_dashboard::reset -padding "2 0 2 0"] -row $gridrow -column 2 -pady 5 -padx 2 -sticky nsew
 
   tablelist::tablelist $w.cvtable -columns {
     0 Colvars
-  } -stretch all -selectmode extended -selecttype row -listvariable ::cvgui::cvs
+  } -stretch all -selectmode extended -selecttype row -listvariable ::cv_dashboard::cvs
   tablelist::tablelist $w.valuetable -columns {
     0 Values
-  } -stretch all -listvariable ::cvgui::cv_values
+  } -stretch all -listvariable ::cv_dashboard::cv_values
 
   refresh_table
 
@@ -431,19 +445,18 @@ proc ::cvgui::createWindow {} {
   grid rowconfigure $w $gridrow -weight 1
 
   incr gridrow
-  grid [ttk::button $w.edit -text "Edit" -command ::cvgui::edit -padding "2 0 2 0"] -row $gridrow -column 0 -pady 5 -padx 2 -sticky nsew
-  grid [ttk::button $w.add -text "Add" -command ::cvgui::add -padding "2 0 2 0"] -row $gridrow -column 1 -pady 5 -padx 2 -sticky nsew
-  grid [ttk::button $w.del -text "Delete" -command ::cvgui::del -padding "2 0 2 0"] -row $gridrow -column 2 -pady 5 -padx 2 -sticky nsew
+  grid [ttk::button $w.edit -text "Edit" -command ::cv_dashboard::edit -padding "2 0 2 0"] -row $gridrow -column 0 -pady 5 -padx 2 -sticky nsew
+  grid [ttk::button $w.add -text "Add" -command ::cv_dashboard::add -padding "2 0 2 0"] -row $gridrow -column 1 -pady 5 -padx 2 -sticky nsew
+  grid [ttk::button $w.del -text "Delete" -command ::cv_dashboard::del -padding "2 0 2 0"] -row $gridrow -column 2 -pady 5 -padx 2 -sticky nsew
 
   incr gridrow
-  grid [ttk::button $w.refresh -text "Refresh" -command ::cvgui::refresh_table -padding "2 0 2 0"] -row $gridrow -column 0 -pady 5 -padx 2 -sticky nsew
-  grid [ttk::button $w.plot -text "Plot" -command ::cvgui::plot -padding "2 0 2 0"] -row $gridrow -column 1 -pady 5 -padx 2 -sticky nsew
-  #grid [ttk::button $w.find -text "Find frame" -command ::cvgui::find -padding "2 0 2 0"] -row $gridrow -column 2 -pady 5 -padx 2 -sticky nsew
+  grid [ttk::button $w.plot -text "Interactive plot" -command ::cv_dashboard::plot -padding "2 0 2 0"] -row $gridrow -column 0 -pady 5 -padx 2 -sticky nsew
+  grid [ttk::button $w.refresh -text "Refresh" -command ::cv_dashboard::refresh_table -padding "2 0 2 0"] -row $gridrow -column 1 -pady 5 -padx 2 -sticky nsew
 
   incr gridrow
   grid [label $w.frameTxt -text "Frame:"] -row $gridrow -column 0 -pady 5 -padx 2 -sticky nsew
-  grid [label $w.frame -textvariable ::cvgui::current_frame] -row $gridrow -column 1 -pady 5 -padx 2 -sticky nsew
-  grid [ttk::checkbutton $w.trackFrame -text "Track" -command ::cvgui::change_track_frame -variable ::cvgui::track_frame]  -row $gridrow -column 2  -pady 5 -padx 2 -sticky nsew
+  grid [label $w.frame -textvariable ::cv_dashboard::current_frame] -row $gridrow -column 1 -pady 5 -padx 2 -sticky nsew
+  grid [ttk::checkbutton $w.trackFrame -text "Track" -command ::cv_dashboard::change_track_frame -variable ::cv_dashboard::track_frame]  -row $gridrow -column 2  -pady 5 -padx 2 -sticky nsew
   change_track_frame ;# activate tracking if necessary
 
   grid columnconfigure $w 0 -weight 1
@@ -452,5 +465,5 @@ proc ::cvgui::createWindow {} {
 }
 
 # If window already exists, destroy it
-catch { destroy .cvgui_window }
-::cvgui::createWindow
+catch { destroy .cv_dashboard_window }
+::cv_dashboard::createWindow
