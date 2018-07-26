d8ce35bad97bb3b8b1e051c51b3b5705057d443d
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2018-07-26 12:56:57 -0400

    Restore version macros overwritten by merge

--- src/colvars_version.h
+++ src/colvars_version.h
@@ -1,3 +1,3 @@
 #ifndef COLVARS_VERSION
-#define COLVARS_VERSION "2018-07-25_pdbflags-test"
+#define COLVARS_VERSION "2018-07-25"
 #endif
--- vmd/src/colvarproxy_vmd_version.h
+++ vmd/src/colvarproxy_vmd_version.h
@@ -1,3 +1,3 @@
 #ifndef COLVARPROXY_VERSION
-#define COLVARPROXY_VERSION "2018-07-24_pdbflags-test"
+#define COLVARPROXY_VERSION "2018-07-24"
 #endif
