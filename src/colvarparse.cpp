e01806d9f94c78551ef4d063c42bc422b854c6f0
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2018-11-22 12:32:24 -0500

    Fix embarrassing mistake

--- src/colvarparse.cpp
+++ src/colvarparse.cpp
@@ -76,7 +76,7 @@ void colvarparse::mark_key_set_user(std::string const &key_str,
                                     Parse_Mode const &parse_mode)
 {
   key_set_modes[to_lower_cppstr(key_str)] = key_set_user;
-  if (parse_mode | parse_echo) {
+  if (parse_mode & parse_echo) {
     cvm::log("# "+key_str+" = "+cvm::to_str(value)+"\n");
   }
 }
@@ -88,7 +88,7 @@ void colvarparse::mark_key_set_default(std::string const &key_str,
                                        Parse_Mode const &parse_mode)
 {
   key_set_modes[to_lower_cppstr(key_str)] = key_set_default;
-  if (parse_mode | parse_echo_default) {
+  if (parse_mode & parse_echo_default) {
     cvm::log("# "+key_str+" = "+cvm::to_str(def_value)+
              " [default]\n");
   }
