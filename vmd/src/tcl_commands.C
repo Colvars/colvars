b14602c122195fdaab7554e2e5434b927b495db2
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2019-01-18 04:03:43 -0500

    Update VMD source files

--- vmd/src/tcl_commands.C
+++ vmd/src/tcl_commands.C
@@ -1,6 +1,6 @@
 /***************************************************************************
  *cr
- *cr            (C) Copyright 1995-2016 The Board of Trustees of the
+ *cr            (C) Copyright 1995-2019 The Board of Trustees of the
  *cr                        University of Illinois
  *cr                         All Rights Reserved
  *cr
@@ -11,7 +11,7 @@
  *
  *      $RCSfile: tcl_commands.C,v $
  *      $Author: johns $        $Locker:  $             $State: Exp $
- *      $Revision: 1.55 $       $Date: 2018/10/24 17:22:07 $
+ *      $Revision: 1.56 $       $Date: 2019/01/17 21:21:03 $
  *
  ***************************************************************************
  * DESCRIPTION:
