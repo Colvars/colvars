3ac2f63edee7bd1b47ef7d4f44613f450746139f
Axel Kohlmeyer <akohlmey@gmail.com>
2019-02-11 17:20:10 -0500

    small update to fix_modify interface as discussed on github:
    - test for existence of colvarproxy module to avoid segfault
    - rename option "add_config" to "configfile"
    - add option "config" for inline configuration text.

--- lammps/src/USER-COLVARS/fix_colvars.cpp
+++ lammps/src/USER-COLVARS/fix_colvars.cpp
@@ -475,9 +475,22 @@ void FixColvars::one_time_init()
 
 int FixColvars::modify_param(int narg, char **arg)
 {
-  if (strcmp(arg[0],"add_config") == 0) {
+  if (strcmp(arg[0],"configfile") == 0) {
     if (narg < 2) error->all(FLERR,"Illegal fix_modify command");
-    if (me == 0) proxy->add_config_file(arg[1]);
+    if (me == 0) {
+      if (! proxy)
+        error->one(FLERR,"Cannot use fix_modify before initialization");
+      proxy->add_config_file(arg[1]);
+    }
+    return 2;
+  } else if (strcmp(arg[0],"config") == 0) {
+    if (narg < 2) error->all(FLERR,"Illegal fix_modify command");
+    if (me == 0) {
+      if (! proxy)
+        error->one(FLERR,"Cannot use fix_modify before initialization");
+      std::string conf(arg[1]);
+      proxy->add_config_string(conf);
+    }
     return 2;
   }
   return 0;
