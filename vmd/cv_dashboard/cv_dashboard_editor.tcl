4576fe8147dd30bbf91d53d3f73bec32a19fe7b4
Jérôme Hénin <heninj@gmail.com>
2019-03-21 23:49:39 +0100

    Split Dashboard script file

---
+++ vmd/cv_dashboard/cv_dashboard_editor.tcl
@@ -0,0 +1,348 @@
+#################################################################
+# Editor window
+#################################################################
+
+
+# Edit new colvar config
+proc ::cv_dashboard::add {} {
+  edit true
+}
+
+
+# Colvar config editor window
+proc ::cv_dashboard::edit { {add false} } {
+  set cfg ""
+
+  if $add {
+    # do not remove existing vars
+    set cvs {}
+    if { [info exists ::cv_dashboard::template_base_dir] } {
+      # Open "official" colvar template
+      set in [open ${::cv_dashboard::template_base_dir}/colvar.in r]
+      set cfg [read $in]
+      close $in
+    } else {
+      # Provide simple template
+      set cfg "# You can edit or replace the example colvar config below.\n\
+  colvar {\n  name d\n  distance {\n    group1 { atomNumbers 1 2 }\n    group2 { atomNumbers 3 4 }\n  }\n}\n"
+    }
+  } else {
+    set cvs [selected_colvars]
+    if {[llength $cvs] == 0} {
+      # If no selection, edit all variables
+      set cvs $::cv_dashboard::cvs
+    }
+    foreach c $cvs {
+      append cfg "colvar {"
+      append cfg [run_cv colvar $c getconfig]
+      append cfg "}\n\n"
+    }
+    set ::cv_dashboard::backup_cfg $cfg
+  }
+  set w .cv_dashboard_window
+
+  catch { destroy $w.editor }
+  set editor [toplevel $w.editor]
+  wm title $editor "Colvar config editor"
+
+  # Left frame: utility buttons
+  frame $w.editor.fl
+  set gridrow 0
+
+  labelframe  $w.editor.fl.docs -bd 2 -text "Online documentation" -padx 2 -pady 2
+  set docs $w.editor.fl.docs
+  ttk::button $docs.onlinedoc1 -text "Collective variables" -padding "4 2 4 2"\
+    -command [list ::cv_dashboard::invokeBrowser "http://colvars.github.io/colvars-refman-vmd/colvars-refman-vmd.html#sec:colvar"]
+  ttk::button $docs.onlinedoc3 -text "Basis functions (components)" -padding "4 2 4 2"\
+    -command [list ::cv_dashboard::invokeBrowser "http://colvars.github.io/colvars-refman-vmd/colvars-refman-vmd.html#sec:cvc_list"]
+  ttk::button $docs.onlinedoc2 -text "Atom groups" -padding "4 2 4 2"\
+    -command [list ::cv_dashboard::invokeBrowser "http://colvars.github.io/colvars-refman-vmd/colvars-refman-vmd.html#sec:colvar_atom_groups"]
+
+  grid $docs.onlinedoc1 -row $gridrow -column 0 -pady 5 -padx 2 -sticky nsew
+  grid $docs.onlinedoc2 -row $gridrow -column 1 -pady 5 -padx 2 -sticky nsew
+  grid $docs.onlinedoc3 -row $gridrow -column 2 -pady 5 -padx 2 -sticky nsew
+  grid columnconfigure $docs 0 -weight 1
+  grid columnconfigure $docs 1 -weight 1
+  grid columnconfigure $docs 2 -weight 1
+
+  labelframe  $w.editor.fl.helpers -bd 2 -text "Editing helpers" -padx 2 -pady 2
+  set helpers $w.editor.fl.helpers
+  ############# Templates #########################################
+  tk::label $helpers.template_label -text "Insert template:"
+  ttk::button $helpers.insert_template -text "Pick template file" \
+    -command [list ::cv_dashboard::insert_template] -padding "2 0 2 0"
+
+  grid $helpers.template_label -row $gridrow -column 0 -pady 2 -padx 2
+  grid $helpers.insert_template -row $gridrow -column 1 -columnspan 2 -sticky ew -pady 2 -padx 2
+  incr gridrow
+
+  ############# Atoms from seltext ################################
+  tk::label $helpers.seltext_label -text "Atoms from selection text:"
+  tk::entry $helpers.seltext -bg white
+  # Bind Return key in seltext entry to proc creating the atomNumbers line
+  bind $helpers.seltext <Return> "::cv_dashboard::atoms_from_sel textbox"
+  ttk::button $helpers.fromsel -text "Insert \[Enter\]" \
+    -command "::cv_dashboard::atoms_from_sel textbox" -padding "2 0 2 0"
+
+  grid $helpers.seltext_label -row $gridrow -column 0 -pady 2 -padx 2
+  grid $helpers.seltext -row $gridrow -column 1 -sticky ew -pady 2 -padx 2
+  grid $helpers.fromsel -row $gridrow -column 2 -pady 2 -padx 2
+  incr gridrow
+
+  ############# Atoms from representation ################################
+  tk::label $helpers.rep_label -text "Atoms from representation:"
+  ttk::combobox $helpers.reps -justify left -state readonly
+  ttk::button $helpers.refresh_reps -text "Refresh list" -command ::cv_dashboard::refresh_reps
+  bind $helpers.reps <<ComboboxSelected>> "::cv_dashboard::atoms_from_sel reps"
+
+  grid $helpers.rep_label -row $gridrow -column 0 -pady 2 -padx 2
+  grid $helpers.reps -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  grid $helpers.refresh_reps -row $gridrow -column 2 -pady 2 -padx 2
+  incr gridrow
+
+  # Populate initial list of selection texts from reps
+  refresh_reps
+
+  ############# Atoms from atom, bond, angle, dihedral labels ####################
+  ttk::button $helpers.labeled_atoms -text "Insert labeled atoms" -command {::cv_dashboard::insert_labels Atoms}
+  ttk::button $helpers.labeled_var -text "Insert labeled..." -command {::cv_dashboard::insert_labels combo}
+  ttk::combobox $helpers.labels -justify left -state readonly
+  $helpers.labels configure -values [list Bonds Angles Dihedrals]
+  $helpers.labels set Bonds
+
+  grid $helpers.labeled_atoms -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid $helpers.labeled_var -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  grid $helpers.labels -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+  incr gridrow
+
+  ################# Insert file name from file picker ###########################
+  ttk::radiobutton $helpers.files1 -variable ::cv_dashboard::filetype -text "atomsFile" -value "atomsFile"
+  ttk::radiobutton $helpers.files2 -variable ::cv_dashboard::filetype -text "refPositionsFile" -value "refPositionsFile"
+  ttk::button $helpers.insert_file -text "Pick file" \
+    -command [list ::cv_dashboard::insert_filename] -padding "2 0 2 0"
+
+  grid $helpers.files1 -row $gridrow -column 0 -pady 2 -padx 2
+  grid $helpers.files2 -row $gridrow -column 1 -pady 2 -padx 2
+  grid $helpers.insert_file -row $gridrow -column 2 -pady 2 -padx 2
+  incr gridrow
+
+  grid columnconfigure $helpers 0 -weight 1
+  grid columnconfigure $helpers 1 -weight 1
+  grid columnconfigure $helpers 2 -weight 1
+
+  grid $docs -sticky ew
+  grid $helpers -sticky ew
+  grid columnconfigure $w.editor.fl 0 -weight 1
+
+
+  # Right frame: text widget w scrollbar and Apply/Cancel buttons
+  frame $w.editor.fr
+  tk::text $w.editor.fr.text -undo 1 -yscrollcommand [list $w.editor.fr.vsb set] -background white
+  ttk::scrollbar $w.editor.fr.vsb -orient vertical -command [list $w.editor.fr.text yview]
+  $w.editor.fr.text insert 1.0 $cfg
+  set ::cv_dashboard::being_edited $cvs
+  grid $w.editor.fr.text -row 0 -columnspan 3 -sticky nsew
+  grid $w.editor.fr.vsb -row 0 -column 3 -sticky nsew
+
+  # Ctrl-s anywhere in the window saves/applies
+  bind $w.editor <Control-s> ::cv_dashboard::edit_apply
+  bind $w.editor <Control-a> "$w.editor.fr.text tag add sel 1.0 end-1c"
+
+  set gridrow 1
+  ttk::button $w.editor.fr.apply -text "Apply \[Ctrl-s\]" -command ::cv_dashboard::edit_apply -padding "2 0 2 0"
+  ttk::button $w.editor.fr.cancel -text "Cancel" -command ::cv_dashboard::edit_cancel -padding "2 0 2 0"
+  ttk::button $w.editor.fr.clear -text "Clear" -command "$w.editor.fr.text delete 1.0 end" -padding "2 0 2 0"
+
+  grid $w.editor.fr.apply -row $gridrow -column 0 -sticky e -pady 2 -padx 2
+  grid $w.editor.fr.cancel -row $gridrow -column 1 -sticky w -pady 2 -padx 2
+  grid $w.editor.fr.clear -row $gridrow -column 2 -sticky w -pady 2 -padx 2
+
+  grid columnconfigure $w.editor.fr 0 -weight 1
+  grid columnconfigure $w.editor.fr 1 -weight 1
+  grid columnconfigure $w.editor.fr 2 -weight 1
+  grid rowconfigure $w.editor.fr 0 -weight 1
+
+  pack $w.editor.fl -fill both -side left
+  pack $w.editor.fr -fill both -expand yes -padx 2 -pady 2
+  # pack $w.editor.fr -side bottom -fill both -expand yes
+}
+
+
+# Multi-platform solution from http://wiki.tcl.tk/557
+proc ::cv_dashboard::invokeBrowser {url} {
+  # open is the OS X equivalent to xdg-open on Linux, start is used on Windows
+  set commands {xdg-open open start}
+  foreach browser $commands {
+    if {$browser eq "start"} {
+      set command [list {*}[auto_execok start] {}]
+    } else {
+      set command [auto_execok $browser]
+    }
+    if {[string length $command]} {
+      break
+    }
+  }
+
+  if {[string length $command] == 0} {
+    return -code error "couldn't find browser"
+  }
+  if {[catch {exec {*}$command $url &} error]} {
+    return -code error "couldn't execute '$command': $error"
+  }
+}
+
+
+# Insert atomNumbers command for given selection text
+proc ::cv_dashboard::atoms_from_sel { source } {
+  set w .cv_dashboard_window
+
+  # Called from textbox
+  if { $source == "textbox" } {
+    set seltext [$w.editor.fl.helpers.seltext get]
+  } elseif { $source == "reps" } {
+    set seltext [$w.editor.fl.helpers.reps get]
+  }
+
+  if {[llength $seltext] == 0 } {
+    return
+  }
+  set sel [atomselect top $seltext]
+  set serials [$sel get serial]
+  $sel delete
+
+  if {[llength $serials] == 0 } {
+    tk_messageBox -icon error -title "Colvars error" -parent .cv_dashboard_window\
+      -message "Selection text matches zero atoms"
+    return
+  }
+  editor_replace "        # Selection: \"$seltext\"\n        atomNumbers $serials\n"
+}
+
+
+# Replace selection in editor with given string
+proc ::cv_dashboard::editor_replace { text } {
+  set t .cv_dashboard_window.editor.fr.text
+
+  if {[$t tag ranges sel] != ""} {
+    $t delete sel.first sel.last
+  }
+  $t insert insert $text
+}
+
+
+# Insert atom numbers or components from currently labeled objects in VMD
+proc ::cv_dashboard::insert_labels {obj} {
+  set w .cv_dashboard_window
+  if {$obj == "combo"} {
+    set obj [$w.editor.fl.helpers.labels get]
+  }
+
+  if { $obj == "Atoms" } {
+    set serials [list]
+    foreach l [label list $obj] {
+      if { [lindex $l 2] == "hide" } { continue }
+      set a [lindex $l 0]
+      lappend serials [expr [lindex $a 1] + 1] ;# going from VMD 0-based to 1-based atomNumbers
+    }
+    if {[llength $serials] > 0} {
+      editor_replace "        # Atom labels\n        atomNumbers $serials\n"
+    }
+  } else {
+    set n(Bonds) 2
+    set n(Angles) 3
+    set n(Dihedrals) 4
+    set cvc(Bonds) distance
+    set cvc(Angles) angle
+    set cvc(Dihedrals) dihedral
+    foreach l [label list $obj] {
+      if { [lindex $l 2] == "hide" } { continue }
+      set cfg "    $cvc($obj) \{\n"
+      for {set i 0} { $i < $n($obj) } {incr i} {
+        set a [lindex $l $i]
+        set serial [expr [lindex $a 1] + 1]
+        append cfg "        group[expr $i+1] \{\n            atomNumbers $serial\n        \}\n"
+      }
+      append cfg "    \}\n"
+      editor_replace $cfg
+    }
+  }
+}
+
+# Insert contents of template file
+proc ::cv_dashboard::insert_template {} {
+  set w .cv_dashboard_window
+  if { [info exists ::cv_dashboard::template_dir] } {
+    set path [tk_getOpenFile -initialdir $::cv_dashboard::template_dir]
+  } else {
+    set path [tk_getOpenFile -initialdir [pwd]]
+  }
+  if [string compare $path ""] {
+    # Save directory for next invocation of this dialog
+    set ::cv_dashboard::template_dir [file dirname $path]
+    set in [open $path r]
+    editor_replace [read $in]
+    close $in
+  }
+}
+
+
+# Insert filename
+proc ::cv_dashboard::insert_filename {} {
+  variable ::cv_dashboard::filetype
+  set w .cv_dashboard_window
+
+  if { [info exists ::cv_dashboard::atomfile_dir] } {
+    set path [tk_getOpenFile -filetypes {{"PDB" .pdb} {"XYZ" .xyz} {"All files" *}} \
+        -initialdir $::cv_dashboard::atomfile_dir]
+  } else {
+    set path [tk_getOpenFile -filetypes {{"PDB" .pdb} {"XYZ" .xyz} {"All files" *}} -initialdir [pwd]]
+  }
+  if [string compare $path ""] {
+    # Save directory for next invocation of this dialog
+    set ::cv_dashboard::atomfile_dir [file dirname $path]
+    set coltype [string range $filetype 0 end-4]
+    editor_replace "    $filetype $path\n    ${coltype}Col O\n    ${coltype}ColValue 1\n"
+  }
+}
+
+
+# Submit current config text from editor to cvm
+proc ::cv_dashboard::edit_apply {} {
+  set w .cv_dashboard_window
+  foreach c $::cv_dashboard::being_edited {
+    run_cv colvar $c delete
+  }
+  set cfg [$w.editor.fr.text get 1.0 end-1c]
+  if { $cfg != "" } {
+    set res [run_cv config $cfg]
+    if { [string compare $res ""] } {
+      # error: restore backed up cfg
+      run_cv config $::cv_dashboard::backup_cfg
+      refresh_table
+      # Do not destroy editor window (give user a chance to fix input)
+      return
+    }
+  }
+  set ::cv_dashboard::being_edited {}
+  destroy $w.editor
+  refresh_table
+}
+
+
+# Close editor without applying
+proc ::cv_dashboard::edit_cancel {} {
+  set w .cv_dashboard_window
+  set ::cv_dashboard::being_edited {}
+  destroy $w.editor
+}
+
+
+proc ::cv_dashboard::refresh_reps {} {
+  set w .cv_dashboard_window
+  set numreps [molinfo top get numreps]
+  set reps [list]
+  for {set i 0} {$i < $numreps} {incr i} {
+    lappend reps [lindex [molinfo top get [list [list selection $i]]] 0]
+  }
+  $w.editor.fl.helpers.reps configure -values $reps
+}
---
+++ vmd/cv_dashboard/cv_dashboard_main.tcl
@@ -0,0 +1,433 @@
+#################################################################
+# Main UI: the dashboard
+#################################################################
+
+
+# Create main window
+proc ::cv_dashboard::createWindow {} {
+
+  if {[winfo exists .cv_dashboard_window]} {
+    wm deiconify .cv_dashboard_window
+    return
+  }
+
+  if {[molinfo num] == 0 } {
+    tk_messageBox -icon error -title "Colvars Dashboard Error"\
+      -message "No molecule loaded. Please load a molecule before running Colvars Dashboard.\n"
+    return -1
+  }
+
+  set w [toplevel .cv_dashboard_window]
+  wm title $w "Colvars dashboard"
+  wm protocol $w WM_DELETE_WINDOW {
+    # window destructor that removes the trace we put in place, so they don't accumulate
+    # if loaded multiple times
+    set molid [molinfo top]
+    trace remove variable vmd_frame($molid) write ::cv_dashboard::update_frame
+    destroy .cv_dashboard_window
+  }
+
+  # setup Colvars if not already there
+  if [catch { cv version}] {
+    run_cv molid top
+  }
+  set gridrow 0
+  grid [ttk::button $w.load -text "Load" -command ::cv_dashboard::load -padding "2 0 2 0"] \
+    -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::button $w.save -text "Save" -command ::cv_dashboard::save -padding "2 0 2 0"] \
+    -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::button $w.reset -text "Reset" -command ::cv_dashboard::reset -padding "2 0 2 0"] \
+    -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+
+  ttk::treeview $w.cvtable -selectmode extended -show tree
+  $w.cvtable configure -column val
+  $w.cvtable column #0 -width 50 -stretch 1 -anchor w
+  $w.cvtable column val -width 150 -stretch 1 -anchor w
+  bind $w.cvtable <e> ::cv_dashboard::edit
+  bind $w <Control-a> { .cv_dashboard_window.cvtable selection set $::cv_dashboard::cvs }
+
+  $w.cvtable tag configure parity0 -background white
+  $w.cvtable tag configure parity1 -background grey94
+  refresh_table
+
+  incr gridrow
+  grid $w.cvtable -row $gridrow -column 0 -sticky news -columnspan 3
+  grid rowconfigure $w $gridrow -weight 1
+
+  incr gridrow
+  grid [ttk::button $w.edit -text "Edit \[e\]" -command ::cv_dashboard::edit -padding "2 0 2 0"] \
+    -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::button $w.add -text "New" -command ::cv_dashboard::add -padding "2 0 2 0"] \
+    -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::button $w.del -text "Delete" -command ::cv_dashboard::del -padding "2 0 2 0"] \
+    -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+
+  incr gridrow
+  grid [ttk::button $w.plot -text "Timeline plot" -command ::cv_dashboard::plot -padding "2 0 2 0"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::button $w.plot2cv -text "Pairwise plot" -command {::cv_dashboard::plot 2cv} -padding "2 0 2 0"] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::button $w.refresh -text "Refresh table" -command ::cv_dashboard::refresh_table -padding "2 0 2 0"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+
+  # Cannot test directly for the presence of scripting methods in the absence of a defined colvar
+  # so we test the version number instead
+  if {[string compare [run_cv version] "2019-02-07"] >= 0} {
+    incr gridrow
+    grid [ttk::button $w.show_atoms -text "Show atoms" -command {::cv_dashboard::show_atoms} -padding "2 0 2 0"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+    grid [ttk::button $w.hide_atoms -text "Hide all" -command {::cv_dashboard::hide_atoms} -padding "2 0 2 0"] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  }
+
+  if {[string compare [run_cv version] "2019-03-18"] >= 0} {
+    incr gridrow
+    grid [ttk::button $w.show_gradients -text "Show gradients" -command {::cv_dashboard::show_gradients [::cv_dashboard::selected_colvars]} \
+      -padding "2 0 2 0"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+    grid [ttk::button $w.hide_gradients -text "Hide all" -command {::cv_dashboard::hide_gradients} -padding "2 0 2 0"] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  }
+
+  incr gridrow
+  grid [label $w.frameTxt -text "Frame:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [label $w.frame -textvariable ::cv_dashboard::current_frame] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::checkbutton $w.trackFrame -text "Track VMD frame" -command ::cv_dashboard::change_track_frame -variable ::cv_dashboard::track_frame] \
+    -row $gridrow -column 2  -pady 2 -padx 2 -sticky nsew
+  change_track_frame ;# activate tracking if necessary
+
+  grid columnconfigure $w 0 -weight 1
+  grid columnconfigure $w 1 -weight 1
+  grid columnconfigure $w 2 -weight 1
+
+  return $w
+}
+
+
+# Refresh the table with a list of existing CVs and their values
+proc ::cv_dashboard::refresh_table {} {
+
+  set w .cv_dashboard_window
+  set ::cv_dashboard::cvs [run_cv list]
+
+  foreach i [$w.cvtable children {}] {
+    $w.cvtable delete $i
+  }
+
+  # Get fresh coordinates from VMD
+  run_cv update
+
+  set parity 1
+  foreach c $::cv_dashboard::cvs {
+    $w.cvtable insert {} end -id $c -text $c
+    $w.cvtable tag add cvname [list $c]
+    # tag odd and even rows for alternating background colors
+    set parity [expr 1-$parity]
+    $w.cvtable tag add "parity$parity" [list $c]
+
+    set val [run_cv colvar $c update]
+    $w.cvtable set $c val [format_value $val]
+
+    set size [llength $val]
+    if { $size > 1 } {
+      for {set i 1} {$i <= $size} {incr i} {
+        set n "${c} ${i}"
+        $w.cvtable insert $c end -id $n -text $n
+        # all colvar/scalar comp items are tagged with tag cvname, to define common bindings on them
+        $w.cvtable tag add cvname [list $n]
+        $w.cvtable tag add "parity$parity" [list $n]
+
+        $w.cvtable set $n val [format_value [lindex $val [expr $i-1]]]
+      }
+    }
+  }
+
+  # Remove deleted variables from gradient display
+  set tmp {}
+  foreach cv $::cv_dashboard::grad_cvs {
+    if { [lsearch $::cv_dashboard::cvs $cv] > -1 } { lappend tmp $cv }
+  }
+  set ::cv_dashboard::grad_cvs $tmp
+  show_gradients $::cv_dashboard::grad_cvs
+
+  update_frame internal [molinfo top] w
+}
+
+
+# Refresh the table with a list of colvar values
+proc ::cv_dashboard::refresh_values {} {
+  run_cv update
+  set w .cv_dashboard_window
+
+  foreach c [$w.cvtable children {}] {
+    set val [run_cv colvar $c update]
+    $w.cvtable set $c val [format_value $val]
+
+    set size [llength $val]
+    if { $size > 1 } {
+      for {set i 1} {$i <= $size} {incr i} {
+        set n "${c} ${i}"
+        $w.cvtable set $n val [format_value [lindex $val [expr $i-1]]]
+      }
+    }
+  }
+}
+
+
+# Format colvar values returned by cv for display in table
+proc ::cv_dashboard::format_value val {
+  if {[llength $val] == 1} {
+    return [format "%.4g" $val]
+  } else {
+    set s "("
+    foreach v [lrange $val 0 end-1] {
+      append s "[format_value $v], "
+    }
+    append s "[format_value [lindex $val end]])"
+    return $s
+  }
+}
+
+
+# Return list of selected colvars in the table, or the lone colvar if there is just one
+proc ::cv_dashboard::selected_colvars {} {
+  set w .cv_dashboard_window
+
+  if { [llength $::cv_dashboard::cvs] == 1 } {
+    return [lindex $::cv_dashboard::cvs 0]
+  } else {
+    set cvs {}
+    set prev ""
+    foreach i [$w.cvtable selection] {
+      # reduce salar components to their parent CV
+      set cv [lindex $i 0]
+      # Skip series of duplicates
+      if {$cv != $prev} {
+        lappend cvs $cv
+        set prev $cv
+      }
+    }
+    return $cvs
+  }
+}
+
+
+# Return selected scalar components for a given selected colvar
+proc ::cv_dashboard::selected_comps { cv } {
+  set w .cv_dashboard_window
+  set comps {}
+
+  foreach i [$w.cvtable selection] {
+    # get comp index of matching cv elements
+    if {[lindex $i 0] == $cv && [llength $i] == 2 } {
+      lappend comps [expr [lindex $i 1] - 1]
+    }
+  }
+
+  # If none selected, select all
+  if { [llength $comps] == 0 } {
+    set val [run_cv colvar $cv value]
+    set i 0
+    foreach v $val { lappend comps $i; incr i }
+  }
+  # Order of selected components is not guaranteed
+  return [lsort $comps]
+}
+
+
+# Enable or disable real-time tracking of VMD frame
+proc ::cv_dashboard::change_track_frame {} {
+  global vmd_frame
+
+  set molid [molinfo top]
+  if {$::cv_dashboard::track_frame} {
+    trace add variable vmd_frame($molid) write ::cv_dashboard::update_frame
+    update_frame internal [molinfo top] w
+  } else {
+    trace remove variable vmd_frame($molid) write ::cv_dashboard::update_frame
+  }
+}
+
+
+# Load config from file
+proc ::cv_dashboard::load {} {
+  if { [info exists ::cv_dashboard::config_dir] } {
+    set path [tk_getOpenFile -filetypes {{"Colvars cfg" .in} {"Colvars cfg" .colvars} {"Gromacs Colvars cfg" .dat} {"All files" *}} \
+        -initialdir $::cv_dashboard::config_dir]
+  } else {
+    set path [tk_getOpenFile -filetypes {{"Colvars cfg" .in} {"Colvars cfg" .colvars} {"Gromacs Colvars cfg" .dat} {"All files" *}} \
+        -initialdir [pwd]]
+  }
+  if [string compare $path ""] {
+    run_cv configfile $path
+    refresh_table
+    # Save directory for next invocation of this dialog
+    set ::cv_dashboard::config_dir [file dirname $path]
+  }
+}
+
+
+# Save config of colvars to file (can we do it w/ biases ? need bias type keyword)
+proc ::cv_dashboard::save {} {
+  if { [info exists ::cv_dashboard::config_dir] } {
+    set path [tk_getSaveFile -filetypes {{"Colvars cfg" .in} {"Colvars cfg" .colvars} {"Gromacs Colvars cfg" .dat} {"All files" *}} \
+        -initialdir $::cv_dashboard::config_dir]
+  } else {
+    set path [tk_getSaveFile -filetypes {{"Colvars cfg" .in} {"Colvars cfg" .colvars} {"Gromacs Colvars cfg" .dat} {"All files" *}} \
+        -initialdir [pwd]]
+  }
+
+  if [string compare $path ""] {
+    # Save directory for next invocation of this dialog
+    set ::cv_dashboard::config_dir [file dirname $path]
+    set cfg ""
+    foreach c [run_cv list] {
+        append cfg "colvar {"
+        append cfg [run_cv colvar $c getconfig]
+        append cfg "}\n\n"
+    }
+
+    set o [open $path w]
+    puts $o $cfg
+    close $o
+  }
+}
+
+
+# Delete currently selected colvars
+proc ::cv_dashboard::del {} {
+  foreach c [selected_colvars] {
+    # workaround bug in colvars pre-2018-07-02
+    if {[string compare [run_cv version] "2018-07-02"] == -1} {
+      foreach b [run_cv list biases] {
+        if [string match *colvars*$c* [run_cv bias $b getconfig]] {
+          puts "Bug workaround: deleting bias $b"
+          run_cv bias $b delete
+        }
+      }
+    }
+    run_cv colvar $c delete
+  }
+  refresh_table
+}
+
+
+# Reset cvm: hard delete allows for reconstructing the module after changing top molecule.
+proc ::cv_dashboard::reset {} {
+  run_cv delete
+  run_cv molid top
+  refresh_table
+}
+
+
+#################################################################
+# Display atoms
+#################################################################
+
+
+# Display atoms in groups for selected colvars
+proc ::cv_dashboard::show_atoms {} {
+  set color 0
+  set ci 0
+  foreach c [selected_colvars] {
+    incr ci
+    set all_groups [run_cv colvar $c getatomgroups]
+    # Remove characters such as <> which are parsed as special in VMD selection texts
+    set sanitized_cvname [regsub -all {[^a-zA-Z0-9_@]} $c {} ]
+    set i 0
+    foreach list $all_groups {
+      incr i
+      # dummyAtoms will return empty lists
+      if {[llength $list] > 0} {
+        set group "${sanitized_cvname}_group_${i}"
+        # resolve ambiguous names due to colvar name sanitization
+        while {[lsearch $::cv_dashboard::macros $group] > -1} {
+          append sanitized_cvname "_"
+          set group "${sanitized_cvname}_group_${i}"
+        }
+        atomselect macro $group "index $list"
+        lappend ::cv_dashboard::macros $group
+        mol color ColorID $color
+        mol representation VDW 1.000000 12.000000
+        mol selection "$group"
+        mol material Opaque
+        mol addrep top
+        set repid [expr [molinfo top get numreps] - 1]
+        lappend ::cv_dashboard::repnames [mol repname top $repid]
+        incr color
+      }
+    }
+  }
+}
+
+
+# Remove atom representations
+proc ::cv_dashboard::hide_atoms {} {
+  foreach r $::cv_dashboard::repnames {
+    mol delrep [mol repindex top $r] top
+  }
+  set ::cv_dashboard::repnames {}
+  set ::cv_dashboard::macros [lsort -unique $::cv_dashboard::macros]
+  foreach m $::cv_dashboard::macros {
+    atomselect delmacro $m
+  }
+  set ::cv_dashboard::macros {}
+}
+
+
+#################################################################
+# Show Colvar gradients
+#################################################################
+
+
+proc ::cv_dashboard::show_gradients { list } {
+
+  foreach cv $list {
+    if { [run_cv colvar $cv set "collect gradient" 1] == -1 } { continue }
+    run_cv colvar $cv update ;# required to get inital values of gradients
+    if { [lsearch $::cv_dashboard::grad_cvs $cv] == -1 } { lappend ::cv_dashboard::grad_cvs $cv }
+  }
+  update_shown_gradients
+}
+
+
+proc ::cv_dashboard::update_shown_gradients {} {
+
+  foreach i $::cv_dashboard::grad_objects {
+    graphics top delete $i
+  }
+  set colorid 3 ;# avoid very common or less visible colors blue, red, gray
+  foreach cv $::cv_dashboard::grad_cvs {
+    set atomids [run_cv colvar $cv getatomids]
+    if { [llength $atomids] == 0 } { continue }
+    set grads [run_cv colvar $cv getgradients]
+    set sel [atomselect top "index $atomids"]
+    set coords [$sel get {x y z}]
+    $sel delete
+
+    set desired_max_length 5.
+    graphics top color [expr $colorid % 32]
+    incr colorid
+
+    set maxl2 0.
+
+    foreach g $grads {
+      set l2 [veclength2 $g]
+      if { $l2 > $maxl2 } { set maxl2 $l2 }
+    }
+    if {$maxl2 < 1e-10} { continue }
+    set fact [expr {$desired_max_length / sqrt($maxl2)}]
+
+    foreach i $atomids start $coords g $grads {
+      set vec [vecscale $fact $g]
+      set end [vecadd $start $vec]
+      set middle [vecadd $start [vecscale 0.9 $vec]]
+      set cyl [graphics top cylinder $start $middle radius 0.15]
+      set cone [graphics top cone $middle $end radius 0.25]
+      lappend ::cv_dashboard::grad_objects $cyl $cone
+    }
+  }
+}
+
+
+proc ::cv_dashboard::hide_gradients {} {
+
+  foreach i $::cv_dashboard::grad_objects {
+    graphics top delete $i
+  }
+  set ::cv_dashboard::grad_objects {}
+  set ::cv_dashboard::grad_cvs {}
+}
