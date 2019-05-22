8189b248894c6c6c156ff523d56dfb9b6daa0e96
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2019-05-22 13:48:51 -0400

    Fix typo

--- src/replace_stl_math.sh
+++ src/replace_stl_math.sh
@@ -2,7 +2,7 @@ for f in *.{cpp,h} ; do
 
     ## The following is not the full list under std::, but should catch all
     for fn in cos sin tan acos asin atan atan2 exp erf erfc log10 floor ceil fabs abs round sqrt ; do
-        sed -i 's/std::${fn}/cvm::${fn}/g' $f
+        sed -i "s/std::${fn}/cvm::${fn}/g" $f
     done
 
     ## log() is... special
