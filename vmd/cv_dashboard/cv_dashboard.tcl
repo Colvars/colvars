b2c9ee1cede7bb7cb72acc626345e70170b3baaa
Jérôme Hénin <heninj@gmail.com>
2019-06-06 12:13:54 +0200

    Dashboard: better regex for parsing colvar names

--- vmd/cv_dashboard/cv_dashboard.tcl
+++ vmd/cv_dashboard/cv_dashboard.tcl
@@ -167,7 +167,8 @@ proc ::cv_dashboard::extract_colvar_configs { cfg_in } {
       }
     }
     # Now we're parsing a line of colvar config, try to get name
-    regexp {^\s*name\s+([^\s]+)} $line match name
+    # non-word characters are spaces and {}# (do not use Tcl's restrictive \w)
+    regexp {^\s*name\s+([^\s{}#]+)} $line match name
 
     # Finally, the tedious fishing for braces
     regexp {^[^#]*} $line nocomments
