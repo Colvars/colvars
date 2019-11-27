ba0b11ffe4468f230a633c5bfe638abb40b0d726
Jérôme Hénin <heninj@gmail.com>
2019-11-28 00:27:53 +0100

    First examples calling Colvars from C and fortran
    Full functionality can be achieved either by explicitly exposing proxy features
    as C functions, or by using the generic version of the scripting interface.

---
+++ C_fortran/colvarproxy_C.cpp
@@ -0,0 +1,41 @@
+// Test linking Colvars through a C interface
+
+#include "colvarproxy_C.h"
+#include "colvarproxy_C_interface.h"
+#include "colvarscript.h"
+
+// This to avoid having to pass around proxy pointers
+static colvarproxy* unique_colvarproxy_object;
+
+colvarproxy_C::colvarproxy_C()
+{
+    std::cerr << "This is the colvarproxy_C constructor at address " << this << std::endl;
+    colvars = new colvarmodule(this);
+    colvars->log("This is the Module speaking.");
+    script = new colvarscript(this);
+}
+
+colvarproxy_C::~colvarproxy_C()
+{
+}
+
+void *allocate_Colvars(char *t)
+{
+    std::cout << "A message from our caller: " << t << std::endl;
+    colvarproxy_C *proxy = new colvarproxy_C();
+    unique_colvarproxy_object = proxy;
+    return proxy;
+}
+
+// This prototype C function can call any colvarproxy member by using the global static pointer
+void call_proxy_member_static()
+{
+    unique_colvarproxy_object->log("Called through static pointer.");
+}
+
+// This prototype C function can call any colvarproxy member by using the global static pointer
+void call_proxy_member(void *proxy_in)
+{
+    colvarproxy *proxy = static_cast<colvarproxy *>(proxy_in);
+    proxy->log("Called through pointer passed as argument.");
+}
---
+++ C_fortran/colvarproxy_C.h
@@ -0,0 +1,48 @@
+// Test linking Colvars through a C interface
+
+#include "colvarproxy.h"
+
+/// \brief Communication between colvars and LAMMPS
+/// (implementation of \link colvarproxy \endlink)
+class colvarproxy_C : public colvarproxy {
+
+public:
+  colvarproxy_C();
+  ~colvarproxy_C();
+
+private:
+  /// \brief Value of the unit for atomic coordinates with respect to
+  /// angstroms (used by some variables for hard-coded default values)
+  cvm::real unit_angstrom() { return 0.; }
+
+  /// \brief Boltzmann constant
+  cvm::real boltzmann() { return 0.; }
+
+  /// \brief Target temperature of the simulation (K units)
+  cvm::real temperature() { return 0.; }
+
+  /// \brief Time step of the simulation (fs)
+  cvm::real dt() { return 0.; }
+
+  /// \brief Pseudo-random number with Gaussian distribution
+  cvm::real rand_gaussian(void) { return 0.; }
+
+  /// Pass restraint energy value for current timestep to MD engine
+  void add_energy(cvm::real energy) {}
+
+  /// numeric index (1-based)
+  int init_atom(int atom_number) { return 0; }
+
+  /// Check that this atom number is valid, but do not initialize the
+  /// corresponding atom yet
+  int check_atom_id(int atom_number) { return 0; }
+
+  /// Print a message to the main log
+  void log(std::string const &message) { std::cout << "colvars: " << message << std::endl; }
+
+  /// Print a message to the main log and let the rest of the program handle the error
+  void error(std::string const &message) { std::cout << "colvars: " << message << std::endl; }
+
+  /// Print a message to the main log and exit with error code
+  void fatal_error(std::string const &message) { std::cout << "colvars: " <<  message << std::endl; exit(1); }
+};
