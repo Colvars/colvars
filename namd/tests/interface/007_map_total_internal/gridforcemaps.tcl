a25816cb98adb59e949da9d8b51ca74d02fe7764
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2020-06-03 17:55:55 -0400

    Add test for internally-computed mapTotal

---
+++ namd/tests/interface/007_map_total_internal/gridforcemaps.tcl
@@ -0,0 +1,9 @@
+# -*- tcl -*-
+
+mgridforce yes
+
+mgridforcepotfile       map ../Common/da.density.dx
+mgridforcefile          map ../Common/da.occ-beta-1.pdb
+mgridforcecol           map B
+mgridforcechargecol     map O
+mgridforcescale         map 0.0 0.0 0.0
