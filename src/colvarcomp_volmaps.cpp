bd8963ccb49c5bfb79a494acfa75b7cae24e7084
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2020-08-10 19:06:48 -0400

    feat: Add atomListFrequency keyword to mapTotal

--- src/colvarcomp_volmaps.cpp
+++ src/colvarcomp_volmaps.cpp
@@ -17,9 +17,9 @@
 colvar::map_total::map_total()
 {
   set_function_type("mapTotal");
+  provide(f_cvc_dynamic_atom_list);
   x.type(colvarvalue::type_scalar);
 }
-
 
 
 int colvar::map_total::init(std::string const &conf)
@@ -59,6 +59,11 @@ int colvar::map_total::init(std::string const &conf)
       error_code |= cvmodule->error(
           "Error: when using only atom selections internal to Colvars, mapFile should be used to load the map directly.\n",
           COLVARS_INPUT_ERROR);
+    }
+
+    get_keyval(conf, "atomListFrequency", atom_list_freq, atom_list_freq);
+    if (atom_list_freq > 0) {
+      enable(f_cvc_dynamic_atom_list);
     }
 
   } else {
@@ -102,8 +107,16 @@ int colvar::map_total::init(std::string const &conf)
 void colvar::map_total::calc_value()
 {
   colvarproxy *proxy = cvmodule->proxy;
+
   int flags = is_enabled(f_cvc_gradient) ? colvarproxy::volmap_flag_gradients :
     colvarproxy::volmap_flag_null;
+
+  if (atom_list_freq > 0) {
+    flags |= colvarproxy::volmap_flag_use_atomlist;
+    if (cvmodule->step_relative() % atom_list_freq == 0) {
+      flags |= colvarproxy::volmap_flag_rebuild_atomlist;
+    }
+  }
 
   if (atoms) {
     // Compute the map inside Colvars
