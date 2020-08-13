18166bc58261ecd86e725e750dccae17b4b8ff0e
Jérôme Hénin <heninj@gmail.com>
2020-08-13 17:02:37 +0200

    More experimenting with ctypes
    Can now run scripting commands through Python without any back-end running
    see C_fortran/colvars.py

--- C_fortran/colvars.py
+++ C_fortran/colvars.py
@@ -1,12 +1,48 @@
 import os, sys
 import ctypes
 
+# Convert python string to byte object for passing to C functions expecting char *
+def b(s):
+  return bytes(s, 'utf-8')
+
+def run_cmd(cmd):
+  words = [b(w) for w in cmd.split()]
+  objc = len(words) + 1
+  objv = (ctypes.c_char_p * objc) (b('cv'), *words)
+  return cv.run_colvarscript_command(objc, objv)
+
+def run_cmds(*cmds):
+  words = [b(w) for w in cmds]
+  objc = len(words) + 1
+  objv = (ctypes.c_char_p * objc) (b('cv'), *words)
+  return cv.run_colvarscript_command(objc, objv)
+
+def cv_result():
+  return cv.get_colvarscript_result().decode('utf-8')
+
+def run_echo(cmd):
+  print(f'Calling cv {cmd}')
+  run_cmd(cmd)
+  print(cv_result())
+
+
+MAX_ARRAY_SIZE = 10
+cmd_type = ctypes.c_char_p * MAX_ARRAY_SIZE
+
+
 cv = ctypes.cdll.LoadLibrary('./libcolvars_C.so')
 
-print(cv.allocate_Colvars('Allocated from Python through ctypes'))
+cv.allocate_Colvars(b('Allocated from Python through ctypes'))
 
-print(cv.get_colvarscript_result())
+cv.get_colvarscript_result.restype = ctypes.c_char_p
 
-print(cv.run_colvarscript_command('version'))
+if run_cmd('version'):
+  print('Error running cv version')
 
-print(cv.get_colvarscript_result())
+print(f'Detected cv version {cv_result()}')
+
+run_cmds('config', 'units electron')
+run_echo('help')
+run_echo('getconfig')
+run_echo('delete')
+run_echo('reset')
