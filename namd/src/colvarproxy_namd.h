d7e49410e2353702fca916b44dc46fa7ba462ac4
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2020-08-27 18:24:19 -0400

    feat: Add support for reupdating the atom list in the proxy, including NAMD implementation

--- namd/src/colvarproxy_namd.h
+++ namd/src/colvarproxy_namd.h
@@ -89,7 +89,10 @@ public:
   /// Allocate an atoms map with the same size as the NAMD topology
   void init_atoms_map();
 
-  // synchronize the local arrays with requested or forced atoms
+  /// Rebuild the list of requested atoms based on Colvars-internal refcounts
+  int update_requested_atoms();
+
+  /// Synchronize the local arrays with requested or forced atoms
   int update_atoms_map(AtomIDList::const_iterator begin,
                        AtomIDList::const_iterator end);
 
