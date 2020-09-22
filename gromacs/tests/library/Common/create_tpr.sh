e33e14638a7260ba7a4b98c00c44ef2ee03030c4
Hubert <hubert.santuz@gmail.com>
2020-09-22 14:02:22 +0200

    Update on the Gromacs tests:   - Add a README for information   - Regenerate the input and the reference files with Gromacs 2020.3,     works also on 2020.0 version. The previous version was based on a random commit on the gromacs master, make it hard to track the changes.   - Coordinates of input pdb for tpr has been updated to center the     peptide into the box, avoiding some arbitrary translations made by     Gromacs during mdrun.

--- gromacs/tests/library/Common/create_tpr.sh
+++ gromacs/tests/library/Common/create_tpr.sh
@@ -1,5 +1,12 @@
 # -*- sh-basic-offset: 2; sh-indentation: 2; -*-
 
+# Generated with Gromacs version 2020.3
+
+# Note: The peptide coordinates have to be changed compared to namd.
+# We need to center the peptides inside the box (editconf) to avoid some arbitrary translations made
+# by gromacs mdrun at the first step, due to the fact the peptide is at a corner of the box (without centering).
+# This change a bit (at 1e-5 precision) the results of some tests.
+
 # Command line to create the topologies files from da.coor.pdb
 # Termini  are NH2 and CT2
 # Forcefield is Charmm22 (Charmm27 without CMAP)
@@ -12,9 +19,11 @@ echo "1 2"|gmx_d pdb2gmx -f da.coor.pdb -ff charmm27 -water none -ter -nocmap -i
 # 3. Change also the type of the 3 first atoms. New types in order: NH3,HC,HC
 
 # Generate box
-gmx_d editconf -f da_nobox.pdb -bt cubic -d 1.5 -noc -o da.pdb
+# Center the peptide inside the box (cf Note)
+gmx_d editconf -f da_nobox.pdb -bt cubic -d 1.5 -o da.pdb
 
 # Generate trr for starting coordinates and velocities
+# Pick up the box dimension from editconf
 python create_trr.py
 
 # Command line to create the tpr files.
