730bb9451e1c0ba08d3c9ad1c500fc94152b4010
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2020-10-19 11:55:09 -0400

    Set material for all graphical reps, move volmap settings back with others

--- vmd/cv_dashboard/cv_dashboard_settings.tcl
+++ vmd/cv_dashboard/cv_dashboard_settings.tcl
@@ -30,13 +30,30 @@ proc ::cv_dashboard::createSettingsWindow { } {
   grid [label $settings.graphics_text -text "Graphics settings"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
-  grid [label $settings.material_text -text "Material:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [label $settings.material_text -text "Display material:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   ttk::combobox $settings.material -justify left -state readonly
   $settings.material configure -values [material list]
   grid $settings.material -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
   $settings.material set "Opaque"
 
+  incr gridrow
+  grid [label $settings.atom_radius_text -text "Sphere radius:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [tk::entry $settings.atom_radius -textvariable ::cv_dashboard::atom_radius] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  set ::cv_dashboard::atom_radius 0.5
+
+  incr gridrow
+  grid [label $settings.sel_text_label -text "Intersect sel:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [tk::entry $settings.sel_text -textvariable ::cv_dashboard::sel_text] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  set ::cv_dashboard::sel_text "all"
+
   # Gradient display settings
+
+  incr gridrow
+  grid [ttk::separator $settings.sepgradients -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
+
+  incr gridrow
+  grid [label $settings.gradients_text -text "Gradients settings"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+
   incr gridrow
   grid [ttk::radiobutton $settings.grad_scale_choice_norm -text "Max. gradient norm" -value "norm" -variable ::cv_dashboard::grad_scale_choice \
     -command ::cv_dashboard::update_shown_gradients -padding "2 0 2 0"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
@@ -52,6 +69,37 @@ proc ::cv_dashboard::createSettingsWindow { } {
   bind $settings.grad_scale <<keyb_enter>> "$settings.grad_scale_choice_scale invoke; ::cv_dashboard::update_shown_gradients"
 
   $settings.grad_scale_choice_norm invoke ;# Default to norm
+
+  incr gridrow
+  grid [label $settings.grad_radius_text -text "Arrow radius:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [tk::entry $settings.grad_radius -textvariable ::cv_dashboard::grad_radius] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  set ::cv_dashboard::grad_radius 0.1
+
+
+  # Volmap-specific settings
+
+  incr gridrow
+  grid [ttk::separator $settings.sepvolmap -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
+
+  incr gridrow
+  grid [label $settings.volmap_text -text "Volmaps settings"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+
+  incr gridrow
+  grid [label $settings.volmap_contour_text -text "Contour level:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [tk::entry $settings.volmap_contour -textvariable ::cv_dashboard::volmap_contour] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  set ::cv_dashboard::volmap_contour 0.5
+  grid [label $settings.volmap_contour_unit -text "(% of min-max range)"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+
+  incr gridrow
+  grid [ttk::checkbutton $settings.volmap_periodic_x -text "+/-X images" -variable ::cv_dashboard::volmap_periodic_x] \
+    -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::checkbutton $settings.volmap_periodic_y -text "+/-Y images" -variable ::cv_dashboard::volmap_periodic_y] \
+    -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::checkbutton $settings.volmap_periodic_z -text "+/-Z images" -variable ::cv_dashboard::volmap_periodic_z] \
+    -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+  set ::cv_dashboard::volmap_periodic_x 0
+  set ::cv_dashboard::volmap_periodic_y 0
+  set ::cv_dashboard::volmap_periodic_z 0
 
   grid columnconfigure $settings 0 -weight 1
   grid columnconfigure $settings 1 -weight 1
