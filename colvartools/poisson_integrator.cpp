cf34e6926ad1cf4f7ce4e51718cdc7cbbd3c63d1
Jérôme Hénin <heninj@gmail.com>
2021-05-10 00:05:20 +0200

    Add missing files

---
+++ colvartools/poisson_integrator.cpp
@@ -0,0 +1,33 @@
+#include "colvargrid.h"
+#include "colvarproxy.h"
+
+
+int main (int argc, char *argv[]) {
+
+  colvarproxy *proxy = new colvarproxy();
+  colvarmodule *colvars = new colvarmodule(proxy);
+
+  if (argc < 2) {
+    std::cerr << "One argument needed: file name.\n";
+    return 1;
+  }
+
+  std::string gradfile (argv[1]);
+  colvar_grid_gradient grad(gradfile);
+
+  int itmax = 1000;
+  cvm::real err;
+  cvm::real tol = 1e-6;
+
+  integrate_potential potential(&grad);
+  potential.set_div();
+  potential.integrate(itmax, tol, err);
+  potential.set_zero_minimum();
+
+  std::cout << "Writing integrated potential to " + gradfile + ".int\n";
+  std::ofstream os(gradfile + ".int");
+  potential.write_multicol(os);
+  os.close();
+
+  return 0;
+}
