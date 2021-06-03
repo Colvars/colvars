0d7729d5c62f9f6b49cb48ba547f387450bb7d8b
Jérôme Hénin <heninj@gmail.com>
2021-06-03 11:47:58 +0200

    Improvements to colvars_grid.py

--- colvartools/colvars_grid.py
+++ colvartools/colvars_grid.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from os import path
 
 class colvars_grid:
     '''
@@ -7,17 +7,20 @@ class colvars_grid:
 
     The data lists may have one or several elements depending on the number of data series.
     PMF files contain one data series.
-    Gradient files contain as many data series as there are dimensions
+    Gradient files contain as many data series as there are dimensions.
+    Data can be obtained in array shape with as_array()
 
-    # 3d surface plot for a PMF
-    fig = plt.figure()
+    # Example: 3d surface plot for a 2d free energy surface ("PMF"), with contour plot at z=0
     from mpl_toolkits.mplot3d import Axes3D
+    pmf = colvars_grid('run.pmf')
+    fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(*pmf.meshgrid(), np.clip(pmf.data[0], None, 40))
+    ax.plot_surface(*pmf.meshgrid(), pmf.as_array())
+    ax.contour(*pmf.meshgrid(), pmf.as_array(), zdir='z', offset=0)
 
     Args:
         filename (str): an optional filename to parse
-    
+
     Attributes:
         filename (str): name of source file
         data (list of array): data for last frame
@@ -51,7 +54,7 @@ class colvars_grid:
         '''Print a short summary of the contents of the object'''
         print('Source files:', self.filenames)
         print('Grid dimension:', self.dim, ['PBC' if p else 'non-PBC' for p in self.pbc])
-        print('Number of data series:', len(self.data))
+        print('Number of data series:', self.nsets)
         print('Number of grid points:', np.prod(self.nx), self.nx)
         print('Grid spacings:', self.dx)
         print('Number of time frames:', self.nframes)
@@ -62,28 +65,33 @@ class colvars_grid:
         return [np.array([self.xmin[i] + (k+0.5)*self.dx[i] for k in range(self.nx[i])]) for i in range(self.dim)]
 
 
-    def meshgrid(self, ):
+    def meshgrid(self):
         '''Returns a mesh grid suitable for plotting the data'''
         return np.meshgrid(*self.axes(), copy=True, indexing='ij')
 
+    def as_array(self):
+        '''Returns (latest) data frame as array with the shape of the grid'''
+        if self.nsets == 1:
+            return self.data[0].reshape(self.nx)
+        else:
+            return np.column_stack(self.data).reshape(self.nx + [self.nsets])
 
     def interp_on_gradient_grid(self, data=None):
         '''Return the data array interpolated on the corresponding gradient grid.
-
-        Valid for free energy surfaces.
-        By default, acts on self.data[0]: the final state of the first data series.
-        Can be used on another data array of the right shape, by passing the named
-        parameter data.
         The gradient grid is shifted by half a bin width to the right, and one
         bin smaller in non-periodic dimensions.
+        Valid for free energy surfaces.
+        By default, acts on self.data[0], the final state of the first data series.
+        Can be used on another data array of the right shape, by passing the named
+        parameter data.
         '''
         # Work on a copy of the data that we'll modify in place.
         if data is None:
-            if len(self.data) == 1:
+            if self.nsets == 1:
                 data = self.data[0]
             else:
                 print('Error: default is to interpolate scalar fields with a single data series, but this object has',
-                len(self.data), 'data series. Pass the data parameter to customize.')
+                self.nsets, 'data series. Pass the data parameter to customize.')
                 return None
 
         new = np.copy(data)
@@ -114,7 +122,7 @@ class colvars_grid:
                 self.xmin.append(float(l[1]))
                 self.dx.append(float(l[2]))
                 self.nx.append(int(l[3]))
-                self.pbc.append(l[4] == "1")
+                self.pbc.append(l[4] == '1')
             # Get number of columns
             f.readline() # empty line
             ncols = len(f.readline().split())
@@ -144,7 +152,7 @@ class colvars_grid:
                 xmin.append(float(l[1]))
                 dx.append(float(l[2]))
                 nx.append(int(l[3]))
-                pbc.append(l[4] == "1")
+                pbc.append(l[4] == '1')
 
             assert xmin == self.xmin
             assert dx == self.dx
@@ -194,35 +202,8 @@ class colvars_grid:
             self.data.append(self.histdata[i][-1])
 
 
-    def _append_data_loadtxt(self, filename):
-        '''An earlier version of _append_data using np.loadtxt - very slow for large datasets'''
-        # Do not read the first self.dim columns which contain x values
-        rawdata = np.loadtxt(filename, usecols=range(self.dim, self.dim + self.nsets), ndmin = 2)
-
-        grid_size = np.prod(self.nx)
-        nf = rawdata.shape[0] / grid_size
-        assert nf == int(nf), f'Data size {rawdata.shape[0]} is not a multiple of grid size {grid_size}'
-        nf = int(nf)
-        self.nframes += nf
-
-        self.data = [] # forget any previous references to non-final histdata
-
-        # Recast data into histdata : list of (dataset) list of (time frame) array
-        # data is a list of (dataset == latest time frame only) array
-        for i in range(rawdata.shape[1]):
-            for f in range(nf):
-                # Slice np array into list of 1 array per frame
-                # equivalent to a transpose().reshape() in numpy terms
-                # but we have no need for huge arrays with all the data
-                frame = rawdata[grid_size*f:grid_size*(f+1), i]
-                # Store a clean copy
-                self.histdata[i].append(np.copy(frame))
-            # Current data is ref to last frame of each data series
-            self.data.append(self.histdata[i][-1])
-
-
     def write(self, filename):
-        '''Write data (final, not history) to a Colvars multicolumn file (2d only)'''
+        '''Write data (final, not history) to a Colvars multicolumn file (2d / 3d only)'''
         if self.dim < 2 or self.dim > 3:
             print('write() only supports 2d and 3d grids at the moment')
             return
@@ -236,6 +217,7 @@ class colvars_grid:
             nx = self.nx[0]
             ny = self.nx[1]
 
+            flat_index = 0
             if self.dim == 2:
                 for i in range(nx):
                     for j in range(ny):
@@ -243,8 +225,9 @@ class colvars_grid:
                         y = self.xmin[1] + self.dx[1] * (j + .5)
                         f.write(f'{x} {y} ')
                         for d in self.data:
-                            f.write(f'{d[i][j]} ')
-                    f.write('\n')
+                            f.write(f'{d[flat_index]} ')
+                        flat_index += 1
+                        f.write('\n')
             elif self.dim == 3:
                 nz = self.nx[2]
                 for i in range(nx):
@@ -255,7 +238,8 @@ class colvars_grid:
                             z = self.xmin[2] + self.dx[2] * (k + .5)
                             f.write(f'{x} {y} {z} ')
                             for d in self.data:
-                                f.write(f'{d[i][j][k]} ')
+                                f.write(f'{d[flat_index]} ')
+                            flat_index += 1
                             f.write('\n')
             f.close()
 
@@ -266,16 +250,16 @@ class colvars_grid:
         return ' '.join([str(i) for i in l])
 
     def write_dx(self, filename, dataset=None, frame=None):
-        '''Write data to a dx file'''
+        '''Write a single dataset to a dx file'''
 
         if dataset is None:
-            if len(self.data) > 1:
+            if self.nsets > 1:
                 print(f'Dataset index must be specified for writing vector data to dx file.')
                 return
             else:
                 dataset = 0
-        elif len(self.data) > 1:
-                print(f'Writing dataset {dataset} of {len(self.data)}')
+        elif self.nsets > 1:
+                print(f'Writing dataset {dataset} of {self.nsets}')
 
 
         if frame is None or frame == -1:
@@ -289,17 +273,17 @@ class colvars_grid:
 
         l2s = self.list2str
         with open(filename, 'w') as f:
-            f.write(f'# DX file generated from {self.filename} by colvars_grid\n')
+            f.write(f'# DX file generated from {self.filenames} by colvars_grid\n')
             f.write(f'object 1 class gridpositions counts {l2s(self.nx)}\n')
             f.write(f'origin {l2s(self.xmin)}\n')
 
             for i, dx in enumerate(self.dx):
                 f.write(f'delta {l2s([0 for _ in range(i)])} {str(dx)} '
                         + f'{l2s([0 for _ in range(self.dim-1-i)])}\n')
-            
+
             f.write(f'object 2 class gridconnections counts {l2s(self.nx)}\n')
             f.write(f'object 3 class array type double rank 0 items {np.prod(self.nx)} data follows\n')
-    
+
             for i, d in enumerate(data.flat):
                 # Write 10 items per line
                 f.write(f'{d} ')
@@ -316,11 +300,11 @@ class colvars_grid:
 
 
     def entropy(self):
-        ''' Calculate the Shannon entropy of scalar data, interpreted as unnormalized distribution
+        ''' Calculate the Shannon entropy (in nats) of scalar data, interpreted as unnormalized distribution
             and the volume (number) of sampled bins
             If several frames are present, compute for all frames
         '''
-        if len(self.data) > 1:
+        if self.nsets > 1:
             print('Entropy is defined for a scalar dataset')
             return
         S_list = []
@@ -343,17 +327,19 @@ class colvars_grid:
             return S_list, V_list
 
 
-    def convergence(self, ref=None, do_KL=False):
-        '''Calculate the rmsd of data from a reference (by default, the final data)'''
+    def convergence(self, ref=None, do_KL=False, RT=0.596):
+        '''Calculate the rmsd of data from a reference (by default, the final data; can be a dataset of a colvars_grid object)'''
         if ref is None:
             ref = self.data
-        elif len(self.data) == 1 and type(ref) == np.ndarray and ref.shape == self.data[0].shape:
+        elif type(ref) == colvars_grid:
+            ref = ref.data
+        elif self.nsets == 1 and type(ref) == np.ndarray and ref.shape == self.data[0].shape:
             # Accept single data series instead of singleton
             ref = [ref]
-        elif len(ref) != len(self.data):
-            print('Error: got', len(ref), 'reference data series, expected', len(self.data))
+        elif len(ref) != self.nsets:
+            print('Error: got', len(ref), 'reference data series, expected', self.nsets)
             return None
-        # Adding the initial frame
+        # Adding 1 for the initial frame (all zeros)
         sum_sq = np.zeros(self.nframes + 1)
         #  Loop over data series
         for data_i, ref_i in zip(self.histdata, ref):
@@ -363,13 +349,10 @@ class colvars_grid:
             for t, d_t in enumerate(data_i):
                 sum_sq[t+1] += np.sum((d_t-ref_i)**2)
 
-        npoints = len(self.data) * self.data[0].size
+        npoints = self.nsets * self.data[0].size
         rmsd = np.sqrt(sum_sq / npoints)
 
         if do_KL:
-            # Convention: Boltzmann distribution at T=300K
-            # Assume kcal/mol
-            RT = 0.5962
             # Kullback-Leibler divergence
             KL = np.zeros(self.nframes + 1)
             if len(self.histdata) != 1:
@@ -380,13 +363,221 @@ class colvars_grid:
             refdist = np.exp(-1 * refdata / RT)
             refdist /= np.sum(refdist)
             # Initial time: our data is all zeros, dist = 1/N
-            KL[0] = -1.0 / data.size * np.sum(np.log(refdist))
+            KL[0] = -1.0 / data[0].size * np.sum(np.log(refdist))
             # Loop over time
             for t, d_t in enumerate(data):
                 dist_t = np.exp(-1 * d_t / RT)
                 dist_t /= np.sum(dist_t)
                 KL[t+1] = np.sum(dist_t * np.log(dist_t / refdist))
-            return rmsd, KL 
+            return rmsd, KL
         else:
             return rmsd
 
+
+    def marginal_fes(self, dofs, RT=0.596):
+        ''' Compute the marginal FES for specified degrees of freedom, at given thermal energy.
+            Default is 300K, unit kcal/mol: RT = 0.596 kcal/mol
+            Returns a new scalar grid.
+        '''
+        if self.nsets > 1:
+            print('Can only marginalize a scalar dataset')
+            return None
+
+        if not hasattr(dofs, '__len__'):
+            # replace single number with tuple
+            dofs = (dofs, )
+
+        if len(dofs) < 1 or len(dofs) > self.dim - 1:
+            print(f'Number of degrees of freedom to represent over must be between 1 and {self.dim-1}')
+            return None
+
+        for n in dofs:
+            if n not in range(self.dim):
+                print(f'Degree of freedom {n} is not among {range(self.dim)}')
+                return None
+
+        dofs_to_sum_over = []
+        for i in range(self.dim):
+            if not i in dofs:
+                dofs_to_sum_over.append(i)
+        dofs_to_sum_over = tuple(dofs_to_sum_over)
+
+        m = colvars_grid()
+        m.dim = len(dofs)
+        m.nsets = self.nsets
+        m.filenames = self.filenames
+        m.nframes = self.nframes
+        m.histdata = [[] for _ in range(m.nsets)] # Note: nsets must be one in current implemetation
+
+        for i in dofs:
+                m.nx.append(self.nx[i])
+                m.pbc.append(self.pbc[i])
+                m.xmin.append(self.xmin[i])
+                m.dx.append(self.dx[i])
+
+        for data in self.histdata[0]:
+            # Put into real shape, then sum over requested axes
+            marg = -RT * np.log(np.exp(data/(-RT)).reshape(self.nx).sum(axis=dofs_to_sum_over))
+            marg -= np.min(marg) # Set min to zero
+            m.histdata[0].append(marg.reshape(-1)) # Flattened array
+
+        m.data.append(m.histdata[0][-1])
+
+        return m
+
+
+    def marginal_count(self, dofs):
+        ''' Compute the marginal histogram for specified degrees of freedom
+            Returns a new scalar grid.
+        '''
+        if self.nsets > 1:
+            print('Can only marginalize a scalar dataset')
+            return None
+
+        if not hasattr(dofs, '__len__'):
+            # replace single number with tuple
+            dofs = (dofs, )
+
+        if len(dofs) < 1 or len(dofs) > self.dim - 1:
+            print(f'Number of degrees of freedom to represent over must be between 1 and {self.dim-1}')
+            return None
+
+        for n in dofs:
+            if n not in range(self.dim):
+                print(f'Degree of freedom {n} is not among {range(self.dim)}')
+                return None
+
+        dofs_to_sum_over = []
+        for i in range(self.dim):
+            if not i in dofs:
+                dofs_to_sum_over.append(i)
+        dofs_to_sum_over = tuple(dofs_to_sum_over)
+
+        m = colvars_grid()
+        m.dim = len(dofs)
+        m.nsets = self.nsets
+        m.filenames = self.filenames
+        m.nframes = self.nframes
+        m.histdata = [[] for _ in range(m.nsets)] # Note: nsets must be one in current implemetation
+
+        for i in dofs:
+                m.nx.append(self.nx[i])
+                m.pbc.append(self.pbc[i])
+                m.xmin.append(self.xmin[i])
+                m.dx.append(self.dx[i])
+
+        for data in self.histdata[0]:
+            # Put into real shape, then sum over requested axes
+            marg = data.reshape(self.nx).sum(axis=dofs_to_sum_over)
+            m.histdata[0].append(marg.reshape(-1)) # Flattened array
+
+        m.data.append(m.histdata[0][-1])
+
+        return m
+
+
+    def numerical_gradient(self):
+        ''' Compute the numerical gradient for a FES.
+            Returns a new gradient grid.
+        '''
+        if self.nsets > 1:
+            print('Can only compute the gradient of a scalar dataset')
+            return None
+
+        if self.dim != 2:
+            print('Can only compute the gradient of a 2d dataset for now')
+            return None
+
+        grad = colvars_grid()
+        grad.dim = self.dim
+        grad.nsets = self.dim
+        grad.pbc = self.pbc
+        grad.dx = self.dx
+        grad.filenames = self.filenames
+        grad.nframes = self.nframes
+        grad.histdata = [[] for _ in range(grad.nsets)]
+
+        # Gradient grid is shorter by one along non-periodic dimensions
+        grad.nx = [self.nx[i] if self.pbc[i] else self.nx[i] - 1 for i in range(self.dim)]
+        # Gradient grid is shifted by half a bin width
+        grad.xmin = list(np.array(self.xmin) + np.array(self.dx) * 0.5)
+
+        for data in self.histdata[0]: # Assuming single dataset
+            # Need a properly shaped array
+            p = data.reshape(self.nx)
+
+            # Extend periodic dimensions by replicating first column/row
+            if (self.pbc[0]):
+                p = np.concatenate((p, p[:1]))
+            if (self.pbc[1]):
+                p = np.concatenate((p, p[:,:1]), axis=1)
+
+            # Truncated / shifted arrays
+            pp = p[1:, 1:]
+            pm = p[1:, :-1]
+            mp = p[:-1, 1:]
+            mm = p[:-1, :-1]
+
+            # 4-point formula in 2D
+            pgradx = 0.5 * ((pm + pp) - (mm + mp)) / self.dx[0]
+            pgrady = 0.5 * ((mp + pp) - (mm + pm)) / self.dx[1]
+
+            grad.histdata[0].append(pgradx.reshape(-1)) # Flattened array
+            grad.histdata[1].append(pgrady.reshape(-1)) # Flattened array
+
+        # Data contains last items in history
+        grad.data.append(grad.histdata[0][-1])
+        grad.data.append(grad.histdata[1][-1])
+
+        return grad
+
+
+class ABF_dataset:
+    def __init__(self, prefix, label='no label'):
+        # Special file names for CZAR data
+        if prefix[-5:] == '.czar':
+            prefix = prefix[:-5]
+            grad = '.hist.czar.grad'
+            pmf = '.hist.czar.pmf'
+            if (path.exists(prefix + '.hist.weight')): # from ABF-AR
+                count = '.hist.count'
+            else: # From eABF
+                count = '.hist.zcount'
+        else:
+            count = '.hist.count'
+            grad = '.hist.grad'
+            pmf = '.hist.pmf'
+        print('Loading count for ' + prefix)
+        self.c=colvars_grid(prefix + count)
+        print('Loading gradient for ' + prefix)
+        self.g=colvars_grid(prefix + grad)
+        print('Loading free energy surface for ' + prefix)
+        self.p=colvars_grid(prefix + pmf)
+        self.label=label
+
+    def load(self, prefix):
+        # Special file names for CZAR data
+        if prefix[-5:] == '.czar':
+            prefix = prefix[:-5]
+            grad = '.hist.czar.grad'
+            pmf = '.hist.czar.pmf'
+            if (path.exists(prefix + '.hist.weight')): # from ABF-AR
+                count = '.hist.count'
+            else: # From eABF
+                count = '.hist.zcount'
+        else:
+            count = '.hist.count'
+            grad = '.hist.grad'
+            pmf = '.hist.pmf'
+        print('Loading count for ' + prefix)
+        self.c.read(prefix + count)
+        print('Loading gradient for ' + prefix)
+        self.g.read(prefix + grad)
+        print('Loading free energy surface for ' + prefix)
+        self.p.read(prefix + pmf)
+
+    def calc_conv(self, gref=None, ref=None):
+        self.S, self.V = self.c.entropy()
+        self.heterog = np.log(self.V) - self.S
+        self.gconv = self.g.convergence(gref)
+        self.conv, self.KL = self.p.convergence(ref, do_KL=True)
