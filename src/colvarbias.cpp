b744ef7bf888fc0a18690da7b80c285bcca79b21
hanatok <summersnow9403@gmail.com>
2022-01-28 04:07:57 -0600

    Make b_scaled_biasing_force a feature

--- src/colvarbias.cpp
+++ src/colvarbias.cpp
@@ -36,7 +36,6 @@ colvarbias::colvarbias(char const *key)
   colvarbias::reset();
   state_file_step = 0L;
   matching_state = false;
-  b_scaled_biasing_force = false;
   biasing_force_scaling_factors = NULL;
 }
 
@@ -118,9 +117,10 @@ int colvarbias::init(std::string const &conf)
   }
 
   // Use the scaling factors from a grid?
-  get_keyval(conf, "scaledBiasingForce", b_scaled_biasing_force,
-             b_scaled_biasing_force);
-  if (b_scaled_biasing_force) {
+  get_keyval_feature(this, conf, "scaledBiasingForce",
+                     f_cvb_scale_biasing_force,
+                     is_enabled(f_cvb_scale_biasing_force), parse_echo);
+  if (is_enabled(f_cvb_scale_biasing_force)) {
     std::string biasing_force_scaling_factors_in_filename;
     get_keyval(conf, "scaledBiasingForceFactorsGrid",
                biasing_force_scaling_factors_in_filename, std::string());
@@ -131,15 +131,6 @@ int colvarbias::init(std::string const &conf)
     if (!is.is_open()) {
       cvm::error("Error opening the grid file " +
       biasing_force_scaling_factors_in_filename + " for reading");
-    }
-    // check if all CVs support grids
-    for (i = 0; i < num_variables(); i++) {
-      if (variables(i)->enable(f_cv_grid) == COLVARS_ERROR) {
-        cvm::error("scaledBiasingForceFactorsGrid requires grids for "
-                   "component " + variables(i)->name + ", which is not "
-                   "available.\n");
-        return COLVARS_ERROR;
-      }
     }
     biasing_force_scaling_factors = new colvar_grid_scalar(colvars);
     biasing_force_scaling_factors->read_multicol(is, true);
@@ -203,6 +194,9 @@ int colvarbias::init_dependencies() {
 
     init_feature(f_cvb_write_ti_pmf, "write_TI_PMF", f_type_user);
     require_feature_self(f_cvb_write_ti_pmf, f_cvb_calc_ti_samples);
+
+    init_feature(f_cvb_scale_biasing_force, "scale_biasing_force", f_type_user);
+    require_feature_children(f_cvb_scale_biasing_force, f_cv_grid);
 
     // check that everything is initialized
     for (i = 0; i < colvardeps::f_cvb_ntot; i++) {
@@ -390,7 +384,7 @@ void colvarbias::communicate_forces()
   }
   cvm::real biasing_force_factor = 1.0;
   size_t i = 0;
-  if (b_scaled_biasing_force) {
+  if (is_enabled(f_cvb_scale_biasing_force)) {
     for (i = 0; i < num_variables(); i++) {
       biasing_force_scaling_factors_bin[i] = biasing_force_scaling_factors->current_bin_scalar(i);
     }
