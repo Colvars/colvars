f843f98fa7ae345965bba989c885a5303fc5158a
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2022-02-22 12:25:49 -0500

    Do not override path to GROMACS

--- gromacs/tests/interface/010_multi-sim/run.sh
+++ gromacs/tests/interface/010_multi-sim/run.sh
@@ -5,8 +5,6 @@
 # It's best to have Gromacs compiled in double precision
 # Reference files have been generated with Gromacs version 2020.6
 
-BINARY_MPI=mdrun_mpi_d
-
 # Attempt replica exchange every ten steps
 mpirun  -np 16 $BINARY_MPI -multidir a b c d -s test.tpr  -deffnm test -replex 10 -reseed 376 -colvars test.dat &> test.out
 # Copy result files to be tested in the main folder
