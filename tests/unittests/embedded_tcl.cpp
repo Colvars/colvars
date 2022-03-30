fe2fe2f642ebed7552a2f97943c36d3d1411c401
Jérôme Hénin <heninj@gmail.com>
2022-03-30 17:02:41 +0200

    Detect absence of scripting support in test

--- tests/unittests/embedded_tcl.cpp
+++ tests/unittests/embedded_tcl.cpp
@@ -10,7 +10,15 @@ extern "C" int main(int argc, char *argv[]) {
   colvarproxy *proxy = new colvarproxy();
   proxy->colvars = new colvarmodule(proxy);
 
-  proxy->tcl_run_script("puts \"\n(Tcl) Tcl script running successfully using embedded interpreter.\"");
+  int res = proxy->tcl_run_script("puts \"\n(Tcl) Tcl script running successfully using embedded interpreter.\"");
+
+  if (res != COLVARS_OK) {
+    std::cout << "Error running Tcl script.\n";
+    if (res == COLVARS_NOT_IMPLEMENTED)
+      std::cout << "Tcl scripts are not enabled in this build of the Colvars library.\n";
+    return 1;
+  }
+
   proxy->tcl_run_script("puts \"(Tcl) Currently defined variables:\n(Tcl) [info vars]\"");
   proxy->tcl_run_script("puts \"(Tcl) Currently defined procedures:\n(Tcl) [info procs]\"");
 
