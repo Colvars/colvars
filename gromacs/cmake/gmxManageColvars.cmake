c4aa08b9499153d14e950890ef7f77f46e2d118c
Wei Zhang <wzhangpku@gmail.com>
2023-02-10 22:41:20 +0100

    Update gromacs-2021.6 patch

--- gromacs/cmake/gmxManageColvars.cmake
+++ gromacs/cmake/gmxManageColvars.cmake
@@ -6,13 +6,8 @@
 # Colvars repository at GitHub.
 
 function(gmx_manage_colvars)
-  find_package(Torch REQUIRED)
-  set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
   file(GLOB COLVARS_SOURCES ${PROJECT_SOURCE_DIR}/src/external/colvars/*.cpp)
   add_library(colvars OBJECT ${COLVARS_SOURCES})
-
-  target_link_libraries(colvars "${TORCH_LIBRARIES}")
-  target_compile_options(colvars PRIVATE -DTORCH)
 
   # Colvars requires a correct definition of __cplusplus, which MSVC doesn't give by default
   target_compile_options(colvars PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/Zc:__cplusplus>)
@@ -21,8 +16,18 @@ endfunction()
 function(gmx_include_colvars_headers)
   target_include_directories(libgromacs PRIVATE ${PROJECT_SOURCE_DIR}/src/external/colvars)
 
-  find_package(Torch REQUIRED)
-  target_link_libraries(libgromacs PRIVATE "${TORCH_LIBRARIES}")
 endfunction()
 
+function(gmx_set_colvars_torch)
+
+  find_package(Torch)
+
+  if (Torch_FOUND)
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
+    target_link_libraries(colvars "${TORCH_LIBRARIES}")
+    target_compile_options(colvars PRIVATE -DTORCH)
+    target_link_libraries(libgromacs PRIVATE "${TORCH_LIBRARIES}")
+  endif()
+
+endfunction()
 
