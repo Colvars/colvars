12ba1486fb438a14cd98fe340dccac4dfb88c42a
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2023-07-19 17:24:49 -0400

    Move specializations to source file

--- src/colvars_memstream.h
+++ src/colvars_memstream.h
@@ -1,5 +1,12 @@
 // -*- c++ -*-
 
+// This file is part of the Collective Variables module (Colvars).
+// The original version of Colvars and its updates are located at:
+// https://github.com/Colvars/colvars
+// Please update all Colvars source files before making any changes.
+// If you wish to distribute your changes, please submit them to the
+// Colvars repository at GitHub.
+
 #ifndef MEMORY_STREAM_H
 #define MEMORY_STREAM_H
 
@@ -107,20 +114,7 @@ protected:
   int error_code_ = 0;
 
   /// Add the requester number of bytes to the array capacity; return false if buffer is external
-  inline bool expand_ouput_buffer(size_t add_bytes)
-  {
-    if (external_output_buffer_) {
-      // Cannot resize an external buffer
-      error_code_ = 1;
-    } else {
-      if ((internal_buffer_.size() + add_bytes) <= max_length_) {
-        internal_buffer_.resize((internal_buffer_.size() + add_bytes));
-      } else {
-        error_code_ = 1;
-      }
-    }
-    return (error_code_ == 0);
-  }
+  bool expand_ouput_buffer(size_t add_bytes);
 
   /// Move the buffer position past the data just written
   inline void incr_write_pos(size_t c) { data_length_ += c; }
@@ -157,44 +151,6 @@ template <typename T> void cvm::memory_stream::write_object(T const &t)
 template <typename T> cvm::memory_stream &operator<<(cvm::memory_stream &os, T const &t)
 {
   os.write_object<T>(t);
-  return os;
-}
-
-template <> void cvm::memory_stream::write_object(std::string const &t)
-{
-  size_t const string_length = t.size();
-  size_t const new_data_size = sizeof(size_t) + sizeof(char) * string_length;
-  if (expand_ouput_buffer(new_data_size)) {
-    std::memcpy(output_location(), &string_length, sizeof(size_t));
-    incr_write_pos(sizeof(size_t));
-    std::memcpy(output_location(), t.c_str(), t.size() * sizeof(char));
-    incr_write_pos(t.size() * sizeof(char));
-  }
-}
-
-template <> cvm::memory_stream &operator<<(cvm::memory_stream &os, std::string const &t)
-{
-  os.write_object<std::string>(t);
-  return os;
-}
-
-template <> void cvm::memory_stream::write_object(cvm::vector1d<cvm::real> const &t)
-{
-  return write_vector<cvm::real>(t.data_array());
-}
-
-template <>
-cvm::memory_stream &operator<<(cvm::memory_stream &os, cvm::vector1d<cvm::real> const &t)
-{
-  os.write_vector<cvm::real>(t.data_array());
-  return os;
-}
-
-template <> void cvm::memory_stream::write_object(colvarvalue const &t) {}
-
-template <> cvm::memory_stream &operator<<(cvm::memory_stream &os, colvarvalue const &t)
-{
-  os.write_object<colvarvalue>(t);
   return os;
 }
 
@@ -235,38 +191,6 @@ template <typename T> cvm::memory_stream &operator>>(cvm::memory_stream &is, T &
   return is;
 }
 
-template <> void cvm::memory_stream::read_object(std::string &t)
-{
-  begin_reading();
-  size_t string_length = 0;
-  if (has_remaining(sizeof(size_t))) {
-    std::memcpy(&string_length, input_location(), sizeof(size_t));
-    incr_read_pos(sizeof(size_t));
-    if (has_remaining(string_length * sizeof(char))) {
-      t.assign(reinterpret_cast<char const *>(input_location()), string_length);
-      incr_read_pos(string_length * sizeof(char));
-      done_reading();
-    }
-  }
-}
-
-template <> cvm::memory_stream &operator>>(cvm::memory_stream &is, std::string &t)
-{
-  is.read_object<std::string>(t);
-  return is;
-}
-
-template <> void cvm::memory_stream::read_object(cvm::vector1d<cvm::real> &t)
-{
-  return read_vector<cvm::real>(t.data_array());
-}
-
-template <> cvm::memory_stream &operator>>(cvm::memory_stream &is, cvm::vector1d<cvm::real> &t)
-{
-  is.read_vector<cvm::real>(t.data_array());
-  return is;
-}
-
 template <typename T> void cvm::memory_stream::read_vector(std::vector<T> &t)
 {
   static_assert(IS_TRIVIALLY_COPYABLE(T), "Cannot use read_vector() on complex type");
@@ -301,5 +225,32 @@ template <typename T> cvm::memory_stream &operator<<(cvm::memory_stream &os,
 {
   return os;
 }
+
+// Declare specializations
+
+template <> void cvm::memory_stream::write_object(std::string const &t);
+
+template <> cvm::memory_stream &operator<<(cvm::memory_stream &os, std::string const &t);
+
+template <> void cvm::memory_stream::write_object(colvarvalue const &t);
+
+template <> cvm::memory_stream &operator<<(cvm::memory_stream &os, colvarvalue const &x);
+
+template <> void cvm::memory_stream::write_object(cvm::vector1d<cvm::real> const &t);
+
+template <>
+cvm::memory_stream &operator<<(cvm::memory_stream &os, cvm::vector1d<cvm::real> const &t);
+
+template <> void cvm::memory_stream::read_object(std::string &t);
+
+template <> cvm::memory_stream &operator>>(cvm::memory_stream &is, std::string &t);
+
+template <> void cvm::memory_stream::read_object(colvarvalue &t);
+
+template <> cvm::memory_stream &operator>>(cvm::memory_stream &is, colvarvalue &t);
+
+template <> void cvm::memory_stream::read_object(cvm::vector1d<cvm::real> &t);
+
+template <> cvm::memory_stream &operator>>(cvm::memory_stream &is, cvm::vector1d<cvm::real> &t);
 
 #endif
