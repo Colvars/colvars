dd17e3757a4e6b639d900d20e96363f6076a7fe7
HanatoK <summersnow9403@gmail.com>
2023-08-02 05:13:38 -0500

    Rewrite the arithmetic PCV code
    The new code has following improvements with respect to the old code:
    1. Correctly calculate s and z with logsumexp and shifted softmax to
       avoid overflow and underflow;
    2. Correctly calculate the derivatives of s and z with respect to vector
       sub CVs (also use logsumexp and shifted softmax);
    3. Implement debug gradients for s and z with respect to sub CVs that
       have no explicit gradients;
    4. Add new tests for derivatives;

--- src/colvar_arithmeticpath.h
+++ src/colvar_arithmeticpath.h
@@ -14,22 +14,11 @@ namespace ArithmeticPathCV {
 using std::vector;
 
 template <typename T>
-T logsumexp(const vector<T>& a, const vector<T>& b, T* sign_factor = nullptr) {
+T logsumexp(const vector<T>& a, const vector<T>& b) {
     const auto max_a = *std::max_element(a.begin(), a.end());
     T sum = T();
     for (size_t i = 0; i < a.size(); ++i) {
         sum += b[i] * cvm::exp(a[i] - max_a);
-    }
-    if (sign_factor) {
-        if (sum > 0) {
-            *sign_factor = 1.0;
-        } else if (sum < 0) {
-            sum = -sum;
-            *sign_factor = -1.0;
-        } else {
-            sum = 1.0;
-            *sign_factor = 0.0;
-        }
     }
     return max_a + cvm::logn(sum);
 }
@@ -45,108 +34,68 @@ T logsumexp(const vector<T>& a) {
 }
 
 
-enum path_sz {S, Z};
-
-template <typename element_type, typename scalar_type, path_sz path_type>
+template <typename scalar_type>
 class ArithmeticPathBase {
 public:
     ArithmeticPathBase() {}
-    virtual ~ArithmeticPathBase() {}
-    virtual void initialize(size_t p_num_elements, size_t p_total_frames, double p_lambda, const vector<element_type>& p_element, const vector<double>& p_weights);
-    virtual void updateDistanceToReferenceFrames() = 0;
-    virtual void computeValue();
-    virtual void computeDerivatives();
-    virtual vector<element_type> computeDerivatives(const size_t frame_index);
-    virtual void compute();
-    virtual void reComputeLambda(const vector<scalar_type>& rmsd_between_refs);
+    ~ArithmeticPathBase() {}
+    void initialize(size_t p_num_elements, size_t p_total_frames, scalar_type p_lambda, const vector<scalar_type>& p_weights);
+    void reComputeLambda(const vector<scalar_type>& rmsd_between_refs);
+    template <typename element_type>
+    void computeValue(const vector<vector<element_type>>& frame_element_distances, scalar_type *s = nullptr, scalar_type *z = nullptr);
+    // can only be called after computeValue() for element-wise derivatives and store derivatives of i-th frame to dsdx and dzdx
+    template <typename element_type>
+    void computeDerivatives(const vector<vector<element_type>>& frame_element_distances, vector<vector<element_type>> *dsdx = nullptr, vector<vector<element_type>> *dzdx = nullptr);
 protected:
     scalar_type lambda;
-    vector<scalar_type> weights;
+    vector<scalar_type> squared_weights;
     vector<scalar_type> frame_indexes;
     size_t num_elements;
     size_t total_frames;
-    vector< vector<element_type> > frame_element_distances;
-    scalar_type s;
-    scalar_type z;
-    vector<element_type> dsdx;
-    vector<element_type> dzdx;
-private:
-    // intermediate variables
     vector<scalar_type> exponents;
-    vector<scalar_type> exponents2;
-    vector<scalar_type> exponents3;
     scalar_type normalization_factor;
-    scalar_type log_sum_exp_0;
-    scalar_type log_sum_exp_1;
+    scalar_type saved_s;
 };
 
-template <typename element_type, typename scalar_type, path_sz path_type>
-void ArithmeticPathBase<element_type, scalar_type, path_type>::initialize(size_t p_num_elements, size_t p_total_frames, double p_lambda, const vector<element_type>& p_element, const vector<double>& p_weights) {
+template <typename scalar_type>
+void ArithmeticPathBase<scalar_type>::initialize(size_t p_num_elements, size_t p_total_frames, scalar_type p_lambda, const vector<scalar_type>& p_weights) {
     lambda = p_lambda;
-    weights = p_weights;
+    for (size_t i = 0; i < p_weights.size(); ++i) squared_weights.push_back(p_weights[i] * p_weights[i]);
     num_elements = p_num_elements;
     total_frames = p_total_frames;
-    frame_element_distances.resize(total_frames, p_element);
     frame_indexes.resize(total_frames);
+    for (size_t i = 0; i < total_frames; ++i) frame_indexes[i] = i;
     exponents.resize(total_frames);
-    exponents2.resize(total_frames);
-    exponents3.resize(total_frames);
-    for (size_t i_frame = 0; i_frame < frame_element_distances.size(); ++i_frame) {
-        for (size_t j_elem = 0; j_elem < num_elements; ++j_elem) {
-            frame_element_distances[i_frame][j_elem].reset();
-        }
-        frame_indexes[i_frame] = i_frame;
-    }
-    s = scalar_type(0);
-    z = scalar_type(0);
-    dsdx = p_element;
-    dzdx = p_element;
-    log_sum_exp_0 = scalar_type(0);
-    log_sum_exp_1 = scalar_type(0);
     normalization_factor = 1.0 / static_cast<scalar_type>(total_frames - 1);
+    saved_s = scalar_type();
 }
 
-template <typename element_type, typename scalar_type, path_sz path_type>
-void ArithmeticPathBase<element_type, scalar_type, path_type>::computeValue() {
-    updateDistanceToReferenceFrames();
-    for (size_t i_frame = 0; i_frame < frame_element_distances.size(); ++i_frame) {
-        scalar_type exponent_tmp = scalar_type(0);
+template <typename scalar_type>
+template <typename element_type>
+void ArithmeticPathBase<scalar_type>::computeValue(
+    const vector<vector<element_type>>& frame_element_distances,
+    scalar_type *s, scalar_type *z)
+{
+    for (size_t i_frame = 0; i_frame < total_frames; ++i_frame) {
+        scalar_type exponent_tmp = scalar_type();
         for (size_t j_elem = 0; j_elem < num_elements; ++j_elem) {
-            exponent_tmp += weights[j_elem] * frame_element_distances[i_frame][j_elem] * weights[j_elem] * frame_element_distances[i_frame][j_elem];
+            exponent_tmp += squared_weights[j_elem] * frame_element_distances[i_frame][j_elem] * frame_element_distances[i_frame][j_elem];
         }
         exponents[i_frame] = exponent_tmp * -1.0 * lambda;
     }
-    log_sum_exp_0 = logsumexp(exponents);
-    log_sum_exp_1 = logsumexp(exponents, frame_indexes);
-    s = normalization_factor * cvm::exp(log_sum_exp_1 - log_sum_exp_0);
-    z = -1.0 / lambda * log_sum_exp_0;
-}
-
-template <typename element_type, typename scalar_type, path_sz path_type>
-void ArithmeticPathBase<element_type, scalar_type, path_type>::compute() {
-    computeValue();
-    computeDerivatives();
-}
-
-template <typename element_type, typename scalar_type, path_sz path_type>
-void ArithmeticPathBase<element_type, scalar_type, path_type>::computeDerivatives() {
-    for (size_t j_elem = 0; j_elem < num_elements; ++j_elem) {
-        for (size_t i_frame = 0; i_frame < frame_element_distances.size(); ++i_frame) {
-            exponents2[i_frame] = frame_element_distances[i_frame][j_elem];
-            exponents3[i_frame] = static_cast<scalar_type>(i_frame) * frame_element_distances[i_frame][j_elem];
-        }
-        scalar_type sign_factor_2, sign_factor_3;
-        const auto log_sum_exp_2 = logsumexp(exponents, exponents2, &sign_factor_2);
-        const auto log_sum_exp_3 = logsumexp(exponents, exponents3, &sign_factor_3);
-        const auto tmp_factor = 2.0 * weights[j_elem] * weights[j_elem];
-        dsdx[j_elem] = -tmp_factor * lambda * normalization_factor *
-                        (sign_factor_3 * cvm::exp(log_sum_exp_3 - log_sum_exp_0) - sign_factor_2 * cvm::exp(log_sum_exp_2 + log_sum_exp_1 - 2.0 * log_sum_exp_0));
-        dzdx[j_elem] = tmp_factor * sign_factor_2 * cvm::exp(log_sum_exp_2 - log_sum_exp_0);
+    const scalar_type log_sum_exp_0 = logsumexp(exponents);
+    const scalar_type log_sum_exp_1 = logsumexp(exponents, frame_indexes);
+    saved_s = normalization_factor * cvm::exp(log_sum_exp_1 - log_sum_exp_0);
+    if (s != nullptr) {
+        *s = saved_s;
+    }
+    if (z != nullptr) {
+        *z = -1.0 / lambda * log_sum_exp_0;
     }
 }
 
-template <typename element_type, typename scalar_type, path_sz path_type>
-void ArithmeticPathBase<element_type, scalar_type, path_type>::reComputeLambda(const vector<scalar_type>& rmsd_between_refs) {
+template <typename scalar_type>
+void ArithmeticPathBase<scalar_type>::reComputeLambda(const vector<scalar_type>& rmsd_between_refs) {
     scalar_type mean_square_displacements = 0.0;
     for (size_t i_frame = 1; i_frame < total_frames; ++i_frame) {
         cvm::log(std::string("Distance between frame ") + cvm::to_str(i_frame) + " and " + cvm::to_str(i_frame + 1) + " is " + cvm::to_str(rmsd_between_refs[i_frame - 1]) + std::string("\n"));
@@ -157,30 +106,41 @@ void ArithmeticPathBase<element_type, scalar_type, path_type>::reComputeLambda(c
 }
 
 // frame-wise derivatives for frames using optimal rotation
-template <typename element_type, typename scalar_type, path_sz path_type>
-vector<element_type> ArithmeticPathBase<element_type, scalar_type, path_type>::computeDerivatives(const size_t frame_index) {
-    vector<element_type> result(num_elements);
-    for (size_t j_elem = 0; j_elem < num_elements; ++j_elem) {
-        element_type dsdxj_numerator_part1(dsdx[j_elem]);
-        element_type dsdxj_numerator_part2(dsdx[j_elem]);
-        element_type dzdxj_numerator(dsdx[j_elem]);
-        dsdxj_numerator_part1.reset();
-        dsdxj_numerator_part2.reset();
-        dzdxj_numerator.reset();
-        element_type derivative_tmp = -2.0 * lambda * weights[j_elem] * weights[j_elem] * frame_element_distances[frame_index][j_elem];
-        dsdxj_numerator_part1 += s_numerator_frame[frame_index] * derivative_tmp;
-        dsdxj_numerator_part2 += s_denominator_frame[frame_index] * derivative_tmp;
-        dzdxj_numerator += s_denominator_frame[frame_index] * derivative_tmp;
-        dsdxj_numerator_part1 *= denominator_s;
-        dsdxj_numerator_part2 *= numerator_s;
-        if (path_type == path_sz::S) {
-            result[j_elem] = (dsdxj_numerator_part1 - dsdxj_numerator_part2) / (denominator_s * denominator_s) * normalization_factor;
-        }
-        if (path_type == path_sz::Z) {
-            result[j_elem] = -1.0 / lambda * dzdxj_numerator / denominator_s;
+template <typename scalar_type>
+template <typename element_type>
+void ArithmeticPathBase<scalar_type>::computeDerivatives(
+    const vector<vector<element_type>>& frame_element_distances,
+    vector<vector<element_type>> *dsdx,
+    vector<vector<element_type>> *dzdx)
+{
+    const vector<scalar_type> softmax_out = softmax(exponents);
+    vector<scalar_type> tmps;
+    tmps.reserve(total_frames);
+    for (size_t i_frame = 0; i_frame < total_frames; ++i_frame) {
+        tmps.push_back(
+            (static_cast<scalar_type>(i_frame) -
+             static_cast<scalar_type>(total_frames - 1) * saved_s) *
+             normalization_factor);
+    }
+    if (dsdx != nullptr) {
+        for (size_t i_frame = 0; i_frame < total_frames; ++i_frame) {
+            for (size_t j_elem = 0; j_elem < num_elements; ++j_elem) {
+                (*dsdx)[i_frame][j_elem] =
+                    -2.0 * squared_weights[j_elem] * lambda *
+                    frame_element_distances[i_frame][j_elem] *
+                    softmax_out[i_frame] * tmps[i_frame];
+            }
         }
     }
-    return result;
+    if (dzdx != nullptr) {
+        for (size_t i_frame = 0; i_frame < total_frames; ++i_frame) {
+            for (size_t j_elem = 0; j_elem < num_elements; ++j_elem) {
+                (*dzdx)[i_frame][j_elem] =
+                    2.0 * squared_weights[j_elem] * softmax_out[i_frame] *
+                    frame_element_distances[i_frame][j_elem];
+            }
+        }
+    }
 }
 }
 
--- src/colvarcomp.h
+++ src/colvarcomp.h
@@ -26,7 +26,6 @@
 #include "colvarmodule.h"
 #include "colvaratoms.h"
 #include "colvar.h"
-#include "colvar_arithmeticpath.h"
 #include "colvar_geometricpath.h"
 
 
@@ -1716,37 +1715,42 @@ public:
     virtual void apply_force(colvarvalue const &force);
 };
 
+struct ArithmeticPathImpl;
+
 class colvar::aspath
-  : public colvar::CartesianBasedPath, public ArithmeticPathCV::ArithmeticPathBase<cvm::atom_pos, cvm::real, ArithmeticPathCV::path_sz::S>
+  : public colvar::CartesianBasedPath
 {
-protected:
-    virtual void updateDistanceToReferenceFrames();
+private:
+    std::unique_ptr<ArithmeticPathImpl> impl_;
+    friend struct ArithmeticPathImpl;
 public:
     aspath(std::string const &conf);
-    virtual ~aspath() {}
+    virtual ~aspath();
     virtual void calc_value();
     virtual void calc_gradients();
     virtual void apply_force(colvarvalue const &force);
 };
 
 class colvar::azpath
-  : public colvar::CartesianBasedPath, public ArithmeticPathCV::ArithmeticPathBase<cvm::atom_pos, cvm::real, ArithmeticPathCV::path_sz::Z>
+  : public colvar::CartesianBasedPath
 {
-protected:
-    virtual void updateDistanceToReferenceFrames();
+private:
+    std::unique_ptr<ArithmeticPathImpl> impl_;
+    friend struct ArithmeticPathImpl;
 public:
     azpath(std::string const &conf);
-    virtual ~azpath() {}
+    virtual ~azpath();
     virtual void calc_value();
     virtual void calc_gradients();
     virtual void apply_force(colvarvalue const &force);
 };
 
 class colvar::aspathCV
-  : public colvar::CVBasedPath, public ArithmeticPathCV::ArithmeticPathBase<colvarvalue, cvm::real, ArithmeticPathCV::path_sz::S>
+  : public colvar::CVBasedPath
 {
-protected:
-    virtual void updateDistanceToReferenceFrames();
+private:
+    std::unique_ptr<ArithmeticPathImpl> impl_;
+    friend struct ArithmeticPathImpl;
 public:
     aspathCV(std::string const &conf);
     virtual ~aspathCV();
@@ -1757,10 +1761,11 @@ public:
 
 
 class colvar::azpathCV
-  : public colvar::CVBasedPath, public ArithmeticPathCV::ArithmeticPathBase<colvarvalue, cvm::real, ArithmeticPathCV::path_sz::Z>
+  : public colvar::CVBasedPath
 {
-protected:
-    virtual void updateDistanceToReferenceFrames();
+private:
+    std::unique_ptr<ArithmeticPathImpl> impl_;
+    friend struct ArithmeticPathImpl;
 public:
     azpathCV(std::string const &conf);
     virtual ~azpathCV();
