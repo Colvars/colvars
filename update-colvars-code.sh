53bbf6a8b16128b7ad97fe641c4c262f1784c405
Zhang <bzfzhang@downpour.zib.de>
2023-08-10 17:18:18 +0200

    Add Torch in lammps' cmake

--- update-colvars-code.sh
+++ update-colvars-code.sh
@@ -300,6 +300,11 @@ then
     condcopy "${src}" "${target}/lib/colvars/${tgt}"
   done
 
+  # Update cmake file 
+  src=${source}/lammps/COLVARS.cmake
+  tgt=$(basename ${src})
+  condcopy "${source}/lammps/COLVARS.cmake" "${target}/cmake/Modules/Packages/{$tgt}"
+
   for src in \
     ${source}/lammps/src/COLVARS/colvarproxy_lammps{.cpp,.h,_version.h} \
     ${source}/lammps/src/COLVARS/fix_colvars.{cpp,h} \
@@ -368,7 +373,7 @@ then
     patch -p1 -N -d ${target} < namd/Makefile.patch
   fi
 
-  # For Torchann
+  # For torchann
   patch -p1 -N -d ${target} < namd/config.patch
   sed -i '/^COLVARSCXXFLAGS =*/s/$/ $(EXTRACOLVARSFLAGS)/' ${target}/Makefile
 
