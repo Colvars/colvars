e376fcf5516baaafbcb5b71f0f737019b012a40f
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2023-09-20 22:30:44 -0400

    Enforce intra-node communication on recent OpenMPI versions

--- devel-tools/load-openmpi.sh
+++ devel-tools/load-openmpi.sh
@@ -1,6 +1,12 @@
 if declare -f module >& /dev/null ; then
+    # Ensure that Lmod is properly initialized
+    source /etc/profile
     # Default modulefile in OpenMPI RPM package
     if module load mpi/openmpi-x86_64 >& /dev/null ; then
         echo "Loaded OpenMPI version $(mpirun --version | head -n 1 | rev | cut -d' ' -f1 | rev)"
+        if ompi_info --param btl vader | grep -q vader ; then
+            # Enforce intra-node communication on recent OpenMPI versions
+            export OMPI_MCA_btl="vader,self"
+        fi
     fi
 fi
