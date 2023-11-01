144601e9a50d458cd8402d807bae0c02de2521c5
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2023-11-01 19:03:54 -0400

    Fix order of commands
    [test-lammps]

--- devel-tools/load-openmpi.sh
+++ devel-tools/load-openmpi.sh
@@ -1,6 +1,7 @@
+# Ensure that Lmod is properly initialized
+source /etc/profile
+
 if declare -f module >& /dev/null ; then
-    # Ensure that Lmod is properly initialized
-    source /etc/profile
     # Default modulefile in OpenMPI RPM package
     if module load mpi/openmpi-x86_64 >& /dev/null ; then
         echo "Loaded OpenMPI version $(mpirun --version | head -n 1 | rev | cut -d' ' -f1 | rev)"
