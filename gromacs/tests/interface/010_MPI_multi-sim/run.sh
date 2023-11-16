0b1607639f17925203316ca63f7926a452148990
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2023-11-16 16:49:47 -0500

    Enable interface tests for GROMACS (MDModules version)
    Enable interface tests for GROMACS (MDModules version)
    
    fix multisim test with MDModule

--- gromacs/tests/interface/010_MPI_multi-sim/run.sh
+++ gromacs/tests/interface/010_MPI_multi-sim/run.sh
@@ -5,7 +5,15 @@
 # It's best to have Gromacs compiled in double precision
 # Reference files have been generated with Gromacs version 2024-dev
 
-mpirun -np 16 $BINARY_MPI -multidir a b c d -s test.tpr  -deffnm test -replex 10 -reseed 376 &> test.out
+# if script called from `run_tests.sh` in the parent folder, retrieve the correct command binary from the argument
+if [ "$1" ]
+then
+    COMMAND=$1
+else
+    COMMAND="mpirun -np 16 gmx_mpi_d"
+fi
+
+$COMMAND -multidir a b c d -s test.tpr  -deffnm test -replex 10 -reseed 376 2>&1 | tee test.out
 
 # Copy result files to be tested in the main folder
 cp a/test.colvars.traj a/test.colvars.state .
