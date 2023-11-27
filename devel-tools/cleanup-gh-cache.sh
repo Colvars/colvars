2a6df906827e67d22a863f1e18dc1dff9dce1f77
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2023-11-27 13:23:30 -0500

    Refine cache cleanup rules (keep only master) and move to script

---
+++ devel-tools/cleanup-gh-cache.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+key=${1:-key Linux-build-}
+
+if [ -z "${key}" ] ; then
+    echo "Error: please specify a cache key as argument." >& 2
+    exit 1
+fi
+
+gh actions-cache --help > /dev/null || gh extension install actions/gh-actions-cache
+
+# Delete all caches except those generated from master
+for cache in $(gh actions-cache list --key ${key} | cut -f 1); do
+    branch=$(gh actions-cache list --key ${cache} | cut -f 3)
+    if [ ${branch} != 'refs/heads/master' ] ; then
+        gh actions-cache delete --confirm "${cache}"
+    fi
+done
+
+# Now keep one cache for master
+caches=($(gh actions-cache list --key ${key} | cut -f 1))
+i=${#caches[@]}
+while [ ${i} -ge 1 ] ; do
+    if [ -n "${caches[${i}]}" ] ; then
+        gh actions-cache delete --confirm "${caches[${i}]}"
+    fi
+    i=$((${i} - 1))
+done
