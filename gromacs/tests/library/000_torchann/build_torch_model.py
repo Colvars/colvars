f75efd15ba153d014532d437a23dae320555378a
Zhang <bzfzhang@lightning.zib.de>
2024-11-14 13:53:14 +0100

    Updated references files generated by Gromacs with double precision

--- gromacs/tests/library/000_torchann/build_torch_model.py
+++ gromacs/tests/library/000_torchann/build_torch_model.py
@@ -6,6 +6,8 @@ class MyModel(torch.nn.Module):
     def forward(self, x):
         return x
 
+print ("PyTorch Version:", torch.__version__)
+
 model = MyModel()
 scripted_cv_filename = f'./identity.pt'
 torch.jit.script(model).save(scripted_cv_filename)
