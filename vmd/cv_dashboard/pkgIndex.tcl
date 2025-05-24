f5291a5b3f9149c45d84993881327f2355a4d2bf
Jérôme Hénin <heninj@gmail.com>
2025-05-24 12:06:01 +0200

    Better handling of multiple versions
    within pkgIndex.tcl

--- vmd/cv_dashboard/pkgIndex.tcl
+++ vmd/cv_dashboard/pkgIndex.tcl
@@ -1,6 +1,6 @@
-# Tcl package index file, version 1.1
+# Tcl package index file
 
-# This file is generated manually
+# This file is generated *manually*
 # and sourced either when an application starts up or
 # by a "package unknown" script.  It invokes the
 # "package ifneeded" command to set up package-related
@@ -9,11 +9,45 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-set dir [file dirname [info script]]
-set version_file [open "${dir}/VERSION"]
-gets $version_file CV_DASHBOARD_VERSION
-close $version_file
-# Convert to Tcl-style package version number
-set CV_DASHBOARD_TCL_VERSION [string map { "-" "." } $CV_DASHBOARD_VERSION]
+proc init {} {
+  set dir [file dirname [info script]]
+  set version_file [open "${dir}/VERSION"]
+  gets $version_file git_date
+  close $version_file
+  # Convert to Tcl-style package version number
+  set CV_DASHBOARD_VERSION [string map { "-" "." } $git_date]
 
-package ifneeded cv_dashboard $CV_DASHBOARD_TCL_VERSION "set env(CV_DASHBOARD_DIR) [list $dir]; [list source [file join $dir cv_dashboard.tcl]]"
+  # Compare with version with local package
+  source [file join $dir cv_dashboard_update.tcl]
+  set package_dir [::cv_dashboard::get_local_dir]
+
+  if { $package_dir ne $dir && [file exists $package_dir]} {
+    # Compare versions
+    set version_file [open [file join $package_dir "cv_dashboard" VERSION]]
+    gets $version_file git_date
+    close $version_file
+    set LOCAL_VERSION [string map { "-" "." } $git_date]
+
+    if { [string compare $LOCAL_VERSION $CV_DASHBOARD_VERSION] > 0 } {
+      puts "Overriding current version ($CV_DASHBOARD_VERSION) with local installation of cv_dashboard from $package_dir ($LOCAL_VERSION)"
+      source [file join $package_dir "cv_dashboard" pkgIndex.tcl]
+      return
+    }
+  }
+
+  # Only update local install
+  if { $package_dir eq $dir } {
+    set updated [::cv_dashboard::self_update]
+
+    if $updated {
+      # Source ourselves! Hoping this doesn't loop
+      source [file join $package_dir "cv_dashboard" pkgIndex.tcl]
+      return
+    }
+  }
+
+  # Finish initializing package
+  package ifneeded cv_dashboard $CV_DASHBOARD_VERSION "set env(CV_DASHBOARD_DIR) [list $dir]; [list source [file join $dir cv_dashboard.tcl]]"
+}
+
+init
