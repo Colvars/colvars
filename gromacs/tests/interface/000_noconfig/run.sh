052d3a9aeac737cb3d4e66443a397267db97c2e5
Jérôme Hénin <heninj@gmail.com>
2025-07-04 14:48:27 +0200

    Use custom test script if available

--- gromacs/tests/interface/000_noconfig/run.sh
+++ gromacs/tests/interface/000_noconfig/run.sh
@@ -1,6 +1,14 @@
 # -*- sh-basic-offset: 2; sh-indentation: 2; -*-
 TMPDIR=/tmp
 
+# if script called from `run_tests.sh` in the parent folder, retrieve the correct command binary from the argument
+if [ -x "$1" ]
+then
+    BINARY=$1
+else
+    BINARY="gmx_d"
+fi
+
 process_output () {
     output=${basename}.part0002
     for file in ${output}.* ; do
@@ -21,19 +29,17 @@ process_output () {
     fi
 }
 
-# For GROMACS 2024 with Colvars MdModule
-
-options="-ntmpi 1 -ntomp 2"
+options="-ntomp 2"
 
 basename=test
-gmx_d grompp -f nocvconfig.mdp -c ../Common/system.gro -p ../Common/system.top -o ${basename}.tpr  2> ${basename}.grompp.err 1> ${basename}.grompp.out
-gmx_d mdrun -s ${basename}.tpr -deffnm ${basename} ${options} 2> ${basename}.err 1> ${basename}.out
+$BINARY grompp -f nocvconfig.mdp -c ../Common/system.gro -p ../Common/system.top -o ${basename}.tpr  2> ${basename}.grompp.err 1> ${basename}.grompp.out
+$BINARY mdrun -s ${basename}.tpr -deffnm ${basename} ${options} 2> ${basename}.err 1> ${basename}.out
 process_output
 
 basename=test.restart
 # Restart tpr
-gmx_d convert-tpr -s ${basename%.restart}.tpr -nsteps 100 -o ${basename}.tpr 2> ${basename}.grompp.err 1> ${basename}.grompp.out
-gmx_d mdrun -s ${basename}.tpr -deffnm ${basename} -noappend -cpi ${basename%.restart}.cpt ${options} 2> ${basename}.err 1> ${basename}.out
+$BINARY convert-tpr -s ${basename%.restart}.tpr -nsteps 100 -o ${basename}.tpr 2> ${basename}.grompp.err 1> ${basename}.grompp.out
+$BINARY mdrun -s ${basename}.tpr -deffnm ${basename} -noappend -cpi ${basename%.restart}.cpt ${options} 2> ${basename}.err 1> ${basename}.out
 process_output
 
 
