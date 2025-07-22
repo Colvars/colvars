Colvars/colvars

commit 5344dc0916c3ff50a75a55922dc1592184a4e50a

Change more widgets to ttk
and improve strip comments option

vmd/cv_dashboard/cv_dashboard_editor.tcl
@@ -63,13 +63,13 @@ ${indent}${indent}group2 { atomNumbers 3 4 }\n${indent}}\n}\n"
 
 
   ############# Templates #########################################
-  tk::labelframe  $w.editor.fl.templates -bd 2 -text "Templates" -padx 2 -pady 2
+  ttk::labelframe  $w.editor.fl.templates -text "Templates" -padding 2 -style cv.TLabelframe -labelanchor n
   set templates $w.editor.fl.templates
 
   foreach d { colvar component other } {
-    tk::label $templates.template_label_$d -font $::cv_dashboard::font -text "$d templates:"
-    ttk::combobox $templates.pick_template_$d -justify left -state readonly -exportselection no
-    $templates.pick_template_$d configure -values [dict keys [set ::cv_dashboard::templates_$d]]
+    ttk::label $templates.template_label_$d -font $::cv_dashboard::font -text "$d templates:"
+    ttk::combobox $templates.pick_template_$d -justify left -state readonly -exportselection no \
+      -values [dict keys [set ::cv_dashboard::templates_$d]]
     bind $templates.pick_template_$d <<keyb_enter>> \
       "::cv_dashboard::insert_template $templates.pick_template_$d [list [set ::cv_dashboard::templates_$d]] $w.editor.fr.text"
     ttk::button $templates.insert_template_$d -text "Insert \[Enter\]" \
@@ -82,11 +82,11 @@ ${indent}${indent}group2 { atomNumbers 3 4 }\n${indent}}\n}\n"
     incr gridrow
   }
 
-  ttk::button $templates.strip_comments -text "Strip comments (Ctrl-m)" \
-        -command "::cv_dashboard::strip_comments $w.editor.fr.text" \
+  ttk::button $templates.strip_comments -text "Strip comments \[Ctrl-m\]" \
+        -command [list ::cv_dashboard::strip_comments $w.editor.fr.text] \
         -padding "2 0 2 0"
-  grid $templates.strip_comments -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
-  bind $w <Control-m> "::cv_dashboard::strip_comments $w.editor.fr.text"
+  grid $templates.strip_comments -row $gridrow -column 3 -columnspan 1 -pady 2 -padx 2 -sticky nsew
+  bind $w.editor <Control-m> [list ::cv_dashboard::strip_comments $w.editor.fr.text]
   incr gridrow
 
   grid columnconfigure $templates 0 -weight 0
@@ -94,11 +94,12 @@ ${indent}${indent}group2 { atomNumbers 3 4 }\n${indent}}\n}\n"
   grid columnconfigure $templates 2 -weight 0
 
   ############# Various helpers ###################################
-  tk::labelframe  $w.editor.fl.helpers -bd 2 -text "Editing helpers" -padx 2 -pady 2
+  ttk::labelframe  $w.editor.fl.helpers -text "Editing helpers" -padding 2 -style cv.TLabelframe -labelanchor n
+
   set helpers $w.editor.fl.helpers
 
   ############# Atoms from seltext ################################
-  tk::label $helpers.seltext_label -font $::cv_dashboard::font -text "Atoms from selection text:"
+  ttk::label $helpers.seltext_label -font $::cv_dashboard::font -text "Atoms from selection text:"
   ttk::entry $helpers.seltext
   # Bind Return key in seltext entry to proc creating the atomNumbers line
   bind $helpers.seltext <<keyb_enter>> "::cv_dashboard::atoms_from_sel textbox"
@@ -111,7 +112,7 @@ ${indent}${indent}group2 { atomNumbers 3 4 }\n${indent}}\n}\n"
   incr gridrow
 
   ############# Atoms from representation ################################
-  tk::label $helpers.rep_label -font $::cv_dashboard::font -text "Atoms from representation:"
+  ttk::label $helpers.rep_label -font $::cv_dashboard::font -text "Atoms from representation:"
   ttk::combobox $helpers.reps -justify left -state readonly -exportselection no
   ttk::button $helpers.refresh_reps -text "Refresh list" -command ::cv_dashboard::refresh_reps
   bind $helpers.reps <<ComboboxSelected>> "::cv_dashboard::atoms_from_sel reps"
@@ -127,8 +128,8 @@ ${indent}${indent}group2 { atomNumbers 3 4 }\n${indent}}\n}\n"
   ############# Atoms from atom, bond, angle, dihedral labels ####################
   ttk::button $helpers.labeled_atoms -text "Insert labeled atoms" -command {::cv_dashboard::insert_labels Atoms}
   ttk::button $helpers.labeled_var -text "Insert labeled..." -command {::cv_dashboard::insert_labels combo}
-  ttk::combobox $helpers.labels -justify left -state readonly -exportselection no
-  $helpers.labels configure -values [list Bonds Angles Dihedrals]
+  ttk::combobox $helpers.labels -justify left -state readonly -exportselection no \
+    -values [list Bonds Angles Dihedrals]
   $helpers.labels set Bonds
 
   grid $helpers.labeled_atoms -row $gridrow -column 0 -pady 2 -padx 2
@@ -153,7 +154,7 @@ ${indent}${indent}group2 { atomNumbers 3 4 }\n${indent}}\n}\n"
 
 
   ############# Doc links #########################################
-  tk::labelframe  $w.editor.fl.docs -bd 2 -text "Links to online documentation" -padx 2 -pady 2
+  ttk::labelframe  $w.editor.fl.docs -text "Links to online documentation" -padding 2 -style cv.TLabelframe -labelanchor n
   set docs $w.editor.fl.docs
 
   ttk::button $docs.onlinedoc1 -text "Collective variables" -style cv_link.TButton -padding "4 2 4 2"\
@@ -625,13 +626,13 @@ proc ::cv_dashboard::edit_bias { {add false} {biases ""} {cvs ""} } {
   incr gridrow
 
   ############# Templates #########################################
-  # tk::labelframe  $w.editor.fl.templates -bd 2 -text "Templates" -padx 2 -pady 2
+  ttk::labelframe  $w.editor.fl.templates -text "Templates" -padding 2 -style cv.TLabelframe -labelanchor n
   set templates $w.bias_editor
 
   set d "bias"
-  tk::label $templates.template_label_$d -font $::cv_dashboard::font -text "$d templates:"
-  ttk::combobox $templates.pick_template_$d -justify left -state readonly -exportselection no
-  $templates.pick_template_$d configure -values [dict keys [set ::cv_dashboard::templates_$d]]
+  ttk::label $templates.template_label_$d -font $::cv_dashboard::font -text "$d templates:"
+  ttk::combobox $templates.pick_template_$d -justify left -state readonly -exportselection no \
+    -values [dict keys [set ::cv_dashboard::templates_$d]]
   bind $templates.pick_template_$d <<keyb_enter>> \
     "::cv_dashboard::insert_template $templates.pick_template_$d [list [set ::cv_dashboard::templates_$d]] $w.bias_editor.text"
   ttk::button $templates.insert_template_$d -text "Insert \[Enter\]" \

vmd/cv_dashboard/cv_dashboard_main.tcl
@@ -16,6 +16,7 @@ proc ::cv_dashboard::createWindow {} {
 
   # TTK styles
   ttk::style configure cv_link.TButton -foreground blue
+  ttk::style configure cv.TLabelframe -borderwidth 2 -labeloutside false
 
   # Top bars of buttons
   set gridrow 0
@@ -91,7 +92,8 @@ proc ::cv_dashboard::createWindow {} {
   grid [ttk::separator $main.sep1 -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
 
   incr gridrow
-  grid [tk::label $main.actions_text -font $::cv_dashboard::font -text "Colvar list actions"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $main.actions_text -font $::cv_dashboard::font -text "Colvar list actions" -anchor n]  \
+      -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
 
@@ -111,7 +113,8 @@ proc ::cv_dashboard::createWindow {} {
   incr gridrow
   grid [ttk::separator $main.sep_plots -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
   incr gridrow
-  grid [tk::label $main.viz_text -font $::cv_dashboard::font -text "Plots and real-time visualizations"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $main.viz_text -font $::cv_dashboard::font -text "Plots and real-time visualizations" -anchor n] \
+      -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
   incr gridrow
   grid [ttk::button $main.plot -text "Timeline plot" -command ::cv_dashboard::plot -padding "2 0 2 0"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::button $main.plot2cv -text "Pairwise plot" -command {::cv_dashboard::plot 2cv} -padding "2 0 2 0"] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
@@ -152,7 +155,8 @@ proc ::cv_dashboard::createWindow {} {
   incr gridrow
   grid [ttk::separator $main.sep_auto -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
   incr gridrow
-  grid [tk::label $main.auto_text -font $::cv_dashboard::font -text "Automatic colvars"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $main.auto_text -font $::cv_dashboard::font -text "Automatic colvars" -anchor n] \
+      -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
   incr gridrow
   grid [ttk::button $main.cvfromprotein -text "Protein/NA auto-colvars" -command ::cv_dashboard::auto_cv -padding "2 0 2 0"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::button $main.cvfromlabels -text "Colvars from VMD labels" -command ::cv_dashboard::cvs_from_labels -padding "2 0 2 0"] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
@@ -162,12 +166,12 @@ proc ::cv_dashboard::createWindow {} {
   incr gridrow
   grid [ttk::separator $main.sep_options -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
   incr gridrow
-  grid [tk::label $main.options_text -font $::cv_dashboard::font -text "General options"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $main.options_text -font $::cv_dashboard::font -text "General options" -anchor n] \
+      -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
-  grid [tk::label $main.molTxt -font $::cv_dashboard::font -text "Molecule:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
-  ttk::combobox $main.mol -justify left -state readonly
-  $main.mol configure -values [molinfo list]
+  grid [ttk::label $main.molTxt -font $::cv_dashboard::font -text "Molecule:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  ttk::combobox $main.mol -justify left -state readonly -values [molinfo list]
   if { $::cv_dashboard::mol != -1 } {
     $main.mol set $::cv_dashboard::mol
   }
@@ -179,17 +183,16 @@ proc ::cv_dashboard::createWindow {} {
 
   # Units
   incr gridrow
-  grid [tk::label $main.unitTxt -font $::cv_dashboard::font -text "Units:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
-  ttk::combobox $main.units -justify left -state readonly
-  $main.units configure -values [array names ::cv_dashboard::text_to_units]
+  grid [ttk::label $main.unitTxt -font $::cv_dashboard::font -text "Units:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  ttk::combobox $main.units -justify left -state readonly -values [array names ::cv_dashboard::text_to_units]
   refresh_units
   grid $main.units -row $gridrow -column 1 -columnspan 2 -pady 2 -padx 2 -sticky nsew
   bind $main.units <<ComboboxSelected>> ::cv_dashboard::change_units
 
   # Frame display and track checkbox
   incr gridrow
-  grid [tk::label $main.frameTxt -font $::cv_dashboard::font -text "Frame:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
-  grid [tk::label $main.frame -font $::cv_dashboard::font -textvariable ::cv_dashboard::current_frame] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $main.frameTxt -font $::cv_dashboard::font -text "Frame:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $main.frame -font $::cv_dashboard::font -textvariable ::cv_dashboard::current_frame] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
   grid [ttk::checkbutton $main.trackFrame -text "Track VMD frame" -command ::cv_dashboard::change_track_frame -variable ::cv_dashboard::track_frame] \
     -row $gridrow -column 2  -pady 2 -padx 2 -sticky nsew
 
@@ -264,7 +267,8 @@ proc ::cv_dashboard::createBiasesTab {} {
   grid [ttk::separator $biases.sep1 -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
 
   incr gridrow
-  grid [tk::label $biases.actions_text -font $::cv_dashboard::font -text "Bias list actions"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $biases.actions_text -font $::cv_dashboard::font -text "Bias list actions" -anchor n] \
+      -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
   grid [ttk::button $biases.edit -text "Edit bias \[Ctrl-e\]" -command ::cv_dashboard::edit_bias -padding "2 0 2 0"] \
@@ -337,12 +341,13 @@ proc ::cv_dashboard::createStatsTab { gridrow } {
   grid [ttk::separator $stats.sep_stats -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
 
   incr gridrow
-  grid [tk::label $stats.stats_title -font $::cv_dashboard::font -text "Energy and force statistics"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $stats.stats_title -font $::cv_dashboard::font -text "Energy and force statistics" -anchor n] \
+       -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
   # Energy/Force display
   set ::cv_dashboard::colvar_energy 0.0
-  grid [tk::label $stats.energyTxt -font $::cv_dashboard::font -text "Total energy:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $stats.energyTxt -font $::cv_dashboard::font -text "Total energy:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $stats.energy -textvariable ::cv_dashboard::colvar_energy -state readonly] \
     -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
 
@@ -350,13 +355,13 @@ proc ::cv_dashboard::createStatsTab { gridrow } {
   set ::cv_dashboard::atom_forces_rms 0.0
   set ::cv_dashboard::atom_forces_max 0.0
   set ::cv_dashboard::atom_forces_max_id -1
-  grid [tk::label $stats.rmsForceTxt -font $::cv_dashboard::font -text "RMS force:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $stats.rmsForceTxt -font $::cv_dashboard::font -text "RMS force:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $stats.rmsForce -textvariable ::cv_dashboard::atom_forces_rms -state readonly] \
     -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
-  grid [tk::label $stats.maxForceIDTxt -font $::cv_dashboard::font -text "Max force atom index:"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $stats.maxForceIDTxt -font $::cv_dashboard::font -text "Max force atom index:"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
-  grid [tk::label $stats.maxForceTxt -font $::cv_dashboard::font -text "Max force:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $stats.maxForceTxt -font $::cv_dashboard::font -text "Max force:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $stats.maxForce -textvariable ::cv_dashboard::atom_forces_max -state readonly] \
     -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $stats.maxForceID -textvariable ::cv_dashboard::atom_forces_max_id -state readonly] \

vmd/cv_dashboard/cv_dashboard_settings.tcl
@@ -15,22 +15,22 @@ proc ::cv_dashboard::createSettingsWindow { } {
   grid [ttk::separator $settings.sep1 -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
 
   incr gridrow
-  grid [tk::label $settings.graphics_text -font $::cv_dashboard::font -text "Graphics settings"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.graphics_text -font $::cv_dashboard::font -text "Graphics settings" -anchor n] \
+      -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
-  grid [tk::label $settings.material_text -font $::cv_dashboard::font -text "Display material:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
-  ttk::combobox $settings.material -justify left -state readonly
-  $settings.material configure -values [material list]
+  grid [ttk::label $settings.material_text -font $::cv_dashboard::font -text "Display material:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  ttk::combobox $settings.material -justify left -state readonly -values [material list]
   grid $settings.material -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
   $settings.material set "Opaque"
 
   incr gridrow
-  grid [tk::label $settings.atom_radius_text -font $::cv_dashboard::font -text "Sphere radius:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.atom_radius_text -font $::cv_dashboard::font -text "Sphere radius:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $settings.atom_radius -textvariable ::cv_dashboard::atom_radius] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
   set ::cv_dashboard::atom_radius 0.5
 
   incr gridrow
-  grid [tk::label $settings.sel_text_label -font $::cv_dashboard::font -text "Intersect sel. text:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.sel_text_label -font $::cv_dashboard::font -text "Intersect sel. text:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $settings.sel_text -textvariable ::cv_dashboard::sel_text] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
   set ::cv_dashboard::sel_text "all"
 
@@ -40,14 +40,15 @@ proc ::cv_dashboard::createSettingsWindow { } {
   grid [ttk::separator $settings.sepgradients -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
 
   incr gridrow
-  grid [tk::label $settings.gradients_text -font $::cv_dashboard::font -text "Gradients / Forces settings"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.gradients_text -font $::cv_dashboard::font -text "Gradients / Forces settings" -anchor n]\
+        -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
   grid [ttk::radiobutton $settings.grad_scale_choice_norm -text "Max. arrow length" -value "norm" -variable ::cv_dashboard::grad_scale_choice \
     -command {::cv_dashboard::update_shown_gradients; ::cv_dashboard::update_shown_forces} -padding "2 0 2 0"] \
     -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $settings.grad_norm -textvariable ::cv_dashboard::grad_norm] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
-  grid [tk::label $settings.grad_norm_unit -font $::cv_dashboard::font -text "Angstrom"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.grad_norm_unit -font $::cv_dashboard::font -text "Angstrom"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
   bind $settings.grad_norm <<keyb_enter>> "$settings.grad_scale_choice_norm invoke; ::cv_dashboard::update_shown_gradients"
 
   incr gridrow
@@ -55,15 +56,15 @@ proc ::cv_dashboard::createSettingsWindow { } {
     -command {::cv_dashboard::update_shown_gradients; ::cv_dashboard::update_shown_forces} -padding "2 0 2 0"] \
     -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $settings.grad_scale -textvariable ::cv_dashboard::grad_scale] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
-  grid [tk::label $settings.grad_scale_unit -font $::cv_dashboard::font -text "gradient: A*L/(cv/width)"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.grad_scale_unit -font $::cv_dashboard::font -text "gradient: A*L/(cv/width)"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
   bind $settings.grad_scale <<keyb_enter>> "$settings.grad_scale_choice_scale invoke; ::cv_dashboard::update_shown_gradients"
 
   $settings.grad_scale_choice_norm invoke ;# Default to norm
 
   incr gridrow
-  grid [tk::label $settings.grad_radius_text -font $::cv_dashboard::font -text "Arrow radius:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.grad_radius_text -font $::cv_dashboard::font -text "Arrow radius:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $settings.grad_radius -textvariable ::cv_dashboard::grad_radius] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
-  grid [tk::label $settings.grad_radius_unit -font $::cv_dashboard::font -text "Angstrom"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.grad_radius_unit -font $::cv_dashboard::font -text "Angstrom"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
   set ::cv_dashboard::grad_radius 0.3
 
   # Rotation display settings
@@ -72,17 +73,17 @@ proc ::cv_dashboard::createSettingsWindow { } {
   grid [ttk::separator $settings.seprotations -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
 
   incr gridrow
-  grid [tk::label $settings.rot_text -font $::cv_dashboard::font -text "Rotation display settings"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.rot_text -font $::cv_dashboard::font -text "Rotation display settings" -anchor n] \
+       -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
-  grid [tk::label $settings.rot_scale_text -font $::cv_dashboard::font -text "Rotation object scale:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.rot_scale_text -font $::cv_dashboard::font -text "Rotation object scale:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $settings.rot_scale -textvariable ::cv_dashboard::rot_scale] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
   set ::cv_dashboard::rot_scale 1.0
 
   incr gridrow
-  grid [tk::label $settings.rot_color_text -font $::cv_dashboard::font -text "Rotation object color:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
-  grid [ttk::combobox $settings.rot_color -justify left -state readonly] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
-  $settings.rot_color configure -values [colorinfo colors]
+  grid [ttk::label $settings.rot_color_text -font $::cv_dashboard::font -text "Rotation object color:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::combobox $settings.rot_color -justify left -state readonly -values [colorinfo colors]] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
   $settings.rot_color set "yellow"
 
   # Volmap-specific settings
@@ -91,13 +92,14 @@ proc ::cv_dashboard::createSettingsWindow { } {
   grid [ttk::separator $settings.sepvolmap -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
 
   incr gridrow
-  grid [tk::label $settings.volmap_text -font $::cv_dashboard::font -text "Volmaps settings"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.volmap_text -font $::cv_dashboard::font -text "Volmaps settings" -anchor n] \
+        -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
-  grid [tk::label $settings.volmap_contour_text -font $::cv_dashboard::font -text "Contour level:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.volmap_contour_text -font $::cv_dashboard::font -text "Contour level:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $settings.volmap_contour -textvariable ::cv_dashboard::volmap_contour] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
   set ::cv_dashboard::volmap_contour 0.5
-  grid [tk::label $settings.volmap_contour_unit -font $::cv_dashboard::font -text "(% of min-max range)"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.volmap_contour_unit -font $::cv_dashboard::font -text "(% of min-max range)"] -row $gridrow -column 2 -pady 2 -padx 2 -sticky nsew
 
   incr gridrow
   grid [ttk::checkbutton $settings.volmap_periodic_x -text "+/-X images" -variable ::cv_dashboard::volmap_periodic_x] \
@@ -114,9 +116,10 @@ proc ::cv_dashboard::createSettingsWindow { } {
   grid [ttk::separator $settings.sephist -orient horizontal] -row $gridrow -column 0 -columnspan 3 -pady 5 -sticky ew
 
   incr gridrow
-  grid [tk::label $settings.hist_text -font $::cv_dashboard::font -text "Histogram settings"] -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.hist_text -font $::cv_dashboard::font -text "Histogram settings" -anchor n] \
+       -row $gridrow -column 0 -columnspan 3 -pady 2 -padx 2 -sticky nsew
   incr gridrow
-  grid [tk::label $settings.nbins_text -font $::cv_dashboard::font -text "Number of bins:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
+  grid [ttk::label $settings.nbins_text -font $::cv_dashboard::font -text "Number of bins:"] -row $gridrow -column 0 -pady 2 -padx 2 -sticky nsew
   grid [ttk::entry $settings.nbins -textvariable ::cv_dashboard::nbins] -row $gridrow -column 1 -pady 2 -padx 2 -sticky nsew
   set ::cv_dashboard::nbins 60
 

commit 82dc20277f82dd9096c4445574d518b96be7f525

Fix Dashboard bug introduced by 5344dc0

vmd/cv_dashboard/cv_dashboard_editor.tcl
@@ -626,7 +626,6 @@ proc ::cv_dashboard::edit_bias { {add false} {biases ""} {cvs ""} } {
   incr gridrow
 
   ############# Templates #########################################
-  ttk::labelframe  $w.editor.fl.templates -text "Templates" -padding 2 -style cv.TLabelframe -labelanchor n
   set templates $w.bias_editor
 
   set d "bias"