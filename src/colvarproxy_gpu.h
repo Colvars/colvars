56715f1670a080681b8042d91e92cd44f6d1aeb8
HanatoK <summersnow9403@gmail.com>
2025-08-04 14:54:12 -0500

    refactor: simplify the GPU management functions

--- src/colvarproxy_gpu.h
+++ src/colvarproxy_gpu.h
@@ -6,35 +6,11 @@
 
 class colvarproxy_gpu {
 public:
-  colvarproxy_gpu():
-    support_gpu(false),
-    gpu_code_type_used(colvars_gpu::gpu_code_t::CPU),
-    gpu_id(0) {
-    gpu_code_type_supported.push_back(colvars_gpu::gpu_code_t::CPU);
-#if defined(COLVARS_CUDA)
-    gpu_code_type_supported.push_back(colvars_gpu::gpu_code_t::CUDA);
-    support_gpu = true;
-#elif defined(COLVARS_HIP)
-    gpu_code_type_supported.push_back(gpu_code_type::HIP);
-    support_gpu = true;
-#elif defined(COLVARS_SYCL)
-    gpu_code_type_supported.push_back(gpu_code_type::SYCL);
-    support_gpu = true;
-#endif // COLVARS_SYCL
-  }
-  virtual int set_gpu(colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr);
+  colvarproxy_gpu(): support_gpu(false) {}
   bool has_gpu_support() const {
     return support_gpu;
   }
-  colvars_gpu::gpu_code_t get_gpu_code_type() const {
-    return gpu_code_type_used;
-  }
   virtual colvars_gpu::gpu_stream_t get_default_stream() {return 0;}
-  int set_gpu_code_type(colvars_gpu::gpu_code_t gpu_code_type_in);
-  virtual int create_stream(colvars_gpu::gpu_stream_t* stream, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr);
-  virtual int sync_stream(colvars_gpu::gpu_stream_t stream);
-  virtual int sync_all_streams();
-  virtual int get_default_device(colvars_gpu::gpu_dev_id_t* device) const;
   template <typename T>
   int allocate_host(T **pp, const size_t len) {
     return allocate_host_T((void **)pp, len, sizeof(T));
@@ -44,19 +20,19 @@ public:
     return deallocate_host_T((void **)pp);
   }
   template <typename T>
-  int allocate_device(T **pp, const size_t len, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr) {
-    return allocate_device_T((void **)pp, len, sizeof(T), gpu_id_in);
+  int allocate_device(T **pp, const size_t len) {
+    return allocate_device_T((void **)pp, len, sizeof(T));
   }
   template <typename T>
-  int reallocate_device(T **pp, const size_t len, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr) {
+  int reallocate_device(T **pp, const size_t len) {
     int error_code = COLVARS_OK;
     error_code |= deallocate_device(pp);
-    error_code |= allocate_device_T((void **)pp, len, sizeof(T), gpu_id_in);
+    error_code |= allocate_device_T((void **)pp, len, sizeof(T));
     return error_code;
   }
   template <typename T>
-  int allocate_device_async(T **pp, const size_t len, colvars_gpu::gpu_stream_t stream, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr) {
-    return allocate_device_T_async((void **)pp, len, sizeof(T), stream, gpu_id_in);
+  int allocate_device_async(T **pp, const size_t len, colvars_gpu::gpu_stream_t stream) {
+    return allocate_device_T_async((void **)pp, len, sizeof(T), stream);
   }
   template <typename T>
   int deallocate_device(T **pp) {
@@ -100,10 +76,10 @@ public:
   }
   virtual int allocate_host_T(void **pp, const size_t len, const size_t sizeofT);
   virtual int deallocate_host_T(void **pp);
-  virtual int allocate_device_T(void **pp, const size_t len, const size_t sizeofT, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr);
+  virtual int allocate_device_T(void **pp, const size_t len, const size_t sizeofT);
   virtual int deallocate_device_T(void **pp);
   virtual int clear_device_array_T(void *data, const size_t ndata, const size_t sizeofT);
-  virtual int allocate_device_T_async(void **pp, const size_t len, const size_t sizeofT, colvars_gpu::gpu_stream_t stream, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr);
+  virtual int allocate_device_T_async(void **pp, const size_t len, const size_t sizeofT, colvars_gpu::gpu_stream_t stream);
   virtual int deallocate_device_T_async(void **pp, colvars_gpu::gpu_stream_t stream);
   virtual int clear_device_array_T_async(void *data, const size_t ndata, const size_t sizeofT, colvars_gpu::gpu_stream_t stream);
   virtual int copy_HtoD_T(const void *h_array, void *d_array, size_t array_len, const size_t sizeofT);
@@ -122,10 +98,6 @@ public:
   ~colvarproxy_gpu();
 protected:
   bool support_gpu;
-  colvars_gpu::gpu_code_t gpu_code_type_used;
-  std::vector<colvars_gpu::gpu_code_t> gpu_code_type_supported;
-  std::vector<colvars_gpu::gpu_stream_t> gpu_streams;
-  colvars_gpu::gpu_dev_id_t gpu_id;
 };
 
 #endif // COLVARPROXY_GPU_H
