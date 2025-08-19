2bafda0346c0b6789f661ec211dea31675f7aabe
Jérôme Hénin <heninj@gmail.com>
2025-08-19 16:42:39 +0200

    Remove unnecessary line enabling fit gradient
    it is enabled by default

--- src/colvarcomp_distances.cpp
+++ src/colvarcomp_distances.cpp
@@ -1186,8 +1186,6 @@ int colvar::eigenvector::init(std::string const &conf)
     atoms->enable(f_ag_rotate);
     atoms->set_ref_pos_from_aos(ref_pos);
     atoms->center_ref_pos();
-    atoms->enable(f_ag_fit_gradients); // cancel out if group is fitted on itself
-                                        // and cvc is translationally invariant
   }
   atoms->setup_rotation_derivative();
 
