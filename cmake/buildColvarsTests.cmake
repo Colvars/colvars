45195942a62b16187e94a39916409404152c0a60
Jérôme Hénin <heninj@gmail.com>
2025-08-20 11:46:38 +0200

    Add debugGradients to relevant functional tests
    Create CMake target for copying test files.

--- cmake/buildColvarsTests.cmake
+++ cmake/buildColvarsTests.cmake
@@ -7,6 +7,12 @@ if(BUILD_TESTS)
 
     if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 
+      # Create a separate target for copying test files
+      add_custom_target(copy_test_files ALL
+        COMMENT "Copying test input files and configurations"
+        VERBATIM
+      )
+
       # TODO automate this step and make it OS-portable
       # add_custom_command(TARGET colvars POST_BUILD
       #   COMMAND bash build_tests.sh
@@ -23,24 +29,22 @@ if(BUILD_TESTS)
       foreach(TEST_CONFIG_FILE ${TEST_CONFIG_FILES})
         get_filename_component(TEST_NAME ${TEST_CONFIG_FILE} DIRECTORY)
         get_filename_component(TEST_NAME ${TEST_NAME} NAME)
-        add_custom_command(TARGET colvars POST_BUILD
-          COMMAND ${CMAKE_COMMAND}
-          -E copy ${TEST_CONFIG_FILE}
-          ${CMAKE_BINARY_DIR}/tests/functional/${TEST_NAME}/test.in)
         add_test(NAME ${TEST_NAME}
           COMMAND run_colvars_test ${TEST_NAME}/test.in trajectory.xyz
           WORKING_DIRECTORY
           ${CMAKE_CURRENT_BINARY_DIR}/tests/functional)
       endforeach()
 
-      # Copy other input files (coordinates, index files, etc)
-      file(GLOB TEST_INPUT_FILES ${COLVARS_SOURCE_DIR}/tests/input_files/*)
-      foreach(TEST_INPUT_FILE ${TEST_INPUT_FILES})
-        add_custom_command(TARGET colvars POST_BUILD
-          COMMAND ${CMAKE_COMMAND}
-          -E copy ${TEST_INPUT_FILE}
-          ${CMAKE_BINARY_DIR}/tests/functional)
-      endforeach(TEST_INPUT_FILE)
+      # Copy input files (coordinates, index files, etc)
+      add_custom_command(TARGET copy_test_files
+        COMMAND ${CMAKE_COMMAND} -E copy_directory
+          ${COLVARS_SOURCE_DIR}/tests/input_files
+          ${CMAKE_BINARY_DIR}/tests/functional
+        COMMENT "Copying test input files"
+      )
+
+      # Make the main target depend on the copy target
+      add_dependencies(colvars copy_test_files)
 
     endif()
   endif()
