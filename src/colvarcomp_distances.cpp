6fd8cf3bbeb732a65b2999888f512eab35b38cfa
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2025-08-28 14:19:46 -0400

    Import eigenvector fix

--- src/colvarcomp_distances.cpp
+++ src/colvarcomp_distances.cpp
@@ -1132,7 +1132,7 @@ int colvar::eigenvector::init(std::string const &conf)
     atoms->enable(f_ag_rotate);
     atoms->ref_pos = ref_pos;
     atoms->center_ref_pos();
-    atoms->disable(f_ag_fit_gradients); // cancel out if group is fitted on itself
+    atoms->enable(f_ag_fit_gradients); // cancel out if group is fitted on itself
                                         // and cvc is translationally invariant
   }
   atoms->setup_rotation_derivative();
