688e880066805006a96e9e01ce96a20ae328aa53
HanatoK <summersnow9403@gmail.com>
2025-08-29 17:11:07 -0500

    First (failed) attempt to make jacobo_4x4 to work with float32

--- src/cuda/colvartypes_kernel.cu
+++ src/cuda/colvartypes_kernel.cu
@@ -386,6 +386,7 @@ __global__ void jacobi_4x4_kernel(
     eigvals[1] = A[1*4+1];
     eigvals[2] = A[2*4+2];
     eigvals[3] = A[3*4+3];
+    // printf("eigvals = %lf %lf %lf %lf\n", eigvals[0], eigvals[1], eigvals[2], eigvals[3]);
     q->q0 = V[0];
     q->q1 = V[4];
     q->q2 = V[8];
@@ -400,6 +401,246 @@ __global__ void jacobi_4x4_kernel(
     }
   }
 }
+
+// The following float version does not pass some debug gradient tests but I don't know why
+#if 0
+template <int p, int q>
+__inline__ __device__ void apply_jacobi_float(
+  float* __restrict A,
+  const float c,
+  const float s,
+  const float c2,
+  const float s2,
+  const float cs) {
+  #pragma unroll
+  for (int i = 0; i < 4; ++i) {
+    const float oip = A[i*4+p];
+    const float oiq = A[i*4+q];
+    if (i != p && i != q) {
+      A[i*4+p] = c * oip - s * oiq;
+      A[p*4+i] = A[i*4+p];
+      A[i*4+q] = c * oiq + s * oip;
+      A[q*4+i] = A[i*4+q];
+    }
+  }
+  const float opp = A[p*4+p];
+  const float oqq = A[q*4+q];
+  const float opq = A[p*4+q];
+  A[p*4+p] = c2 * opp + s2 * oqq - 2.0f * cs * opq;
+  A[q*4+q] = s2 * opp + c2 * oqq + 2.0f * cs * opq;
+  A[p*4+q] = 0;
+  A[q*4+p] = 0;
+}
+
+template <int p, int q>
+__inline__ __device__ void multiply_jacobi_float(
+  float* __restrict V, float c, float s) {
+  #pragma unroll
+  for (int i = 0; i < 4; ++i) {
+    const float oip = V[i*4+p];
+    const float oiq = V[i*4+q];
+    V[i*4+p] = c * oip - s * oiq;
+    V[i*4+q] = s * oip + c * oiq;
+  }
+}
+
+__inline__ __device__ void compute_c_s_float(
+  float a_pq, float a_pp, float a_qq, float& c, float& s, float& c2, float& s2, float& cs) {
+  const float theta = 0.5f * (a_qq - a_pp) / a_pq;
+  const float t = 1.0f / (sqrtf(theta * theta + 1.0f) + fabsf(theta));
+  c = rsqrtf(t * t + 1.0f);
+  s = theta < 0 ? -t * c : t * c;
+  c2 = c * c;
+  s2 = s * s;
+  cs = c * s;
+}
+
+// Use exactly 2 threads
+__global__ void jacobi_4x4_kernel_float32(
+  double* A_in, double* eigvals, int* max_reached,
+  cvm::quaternion* q,
+  bool monitor_crossings,
+  cvm::real crossing_threshold,
+  cvm::quaternion* q_old,
+  int* discontinuous_rotation) {
+  __shared__ float A[4*4];
+  __shared__ float V[4*4];
+  const int idx = threadIdx.x;
+  if (max_reached && idx == 0) {
+    max_reached[0] = 0;
+    __threadfence();
+  }
+  if (idx == 0) {
+    memset(V, 0, sizeof(float)*4*4);
+    V[0*4+0] = 1;
+    V[1*4+1] = 1;
+    V[2*4+2] = 1;
+    V[3*4+3] = 1;
+    A[0] = A_in[0];
+    A[1] = A_in[1];
+    A[2] = A_in[2];
+    A[3] = A_in[3];
+    A[4] = A_in[4];
+    A[5] = A_in[5];
+    A[6] = A_in[6];
+    A[7] = A_in[7];
+    A[8] = A_in[8];
+    A[9] = A_in[9];
+    A[10] = A_in[10];
+    A[11] = A_in[11];
+    A[12] = A_in[12];
+    A[13] = A_in[13];
+    A[14] = A_in[14];
+    A[15] = A_in[15];
+  }
+  __syncthreads();
+  const int p_ids[] = {0, 2, 0, 1, 0, 1};
+  const int q_ids[] = {1, 3, 2, 3, 3, 2};
+  float off_diag_sum =
+    fabsf(A[0*4+1]) + fabsf(A[0*4+2]) + fabsf(A[0*4+3]) +
+    fabsf(A[1*4+2]) + fabsf(A[1*4+3]) +
+    fabsf(A[2*4+3]);
+  int iteration = 0;
+  while (off_diag_sum > JACOBI_TOLERANCE) {
+    float c = 0, s = 0, c2 = 0, s2 = 0, cs = 0;
+    bool rotate = false;
+    int p = p_ids[idx];
+    int q = q_ids[idx];
+    float a_pq = A[p*4+q];
+    if (fabsf(a_pq) > 0) {
+      rotate = true;
+      const float a_pp = A[p*4+p];
+      const float a_qq = A[q*4+q];
+      compute_c_s_float(a_pq, a_pp, a_qq, c, s, c2, s2, cs);
+    }
+    __syncwarp();
+    if (idx == 0 && rotate) {
+      apply_jacobi_float<0, 1>(A, c, s, c2, s2, cs);
+      multiply_jacobi_float<0, 1>(V, c, s);
+    }
+    __syncwarp();
+    if (idx == 1 && rotate) {
+      apply_jacobi_float<2, 3>(A, c, s, c2, s2, cs);
+      multiply_jacobi_float<2, 3>(V, c, s);
+    }
+    __syncwarp();
+    rotate = false;
+    p = p_ids[idx+2];
+    q = q_ids[idx+2];
+    a_pq = A[p*4+q];
+    if (fabsf(a_pq) > 0) {
+      rotate = true;
+      const float a_pp = A[p*4+p];
+      const float a_qq = A[q*4+q];
+      compute_c_s_float(a_pq, a_pp, a_qq, c, s, c2, s2, cs);
+    }
+    __syncwarp();
+    if (idx == 0 && rotate) {
+      apply_jacobi_float<0, 2>(A, c, s, c2, s2, cs);
+      multiply_jacobi_float<0, 2>(V, c, s);
+    }
+    __syncwarp();
+    if (idx == 1 && rotate) {
+      apply_jacobi_float<1, 3>(A, c, s, c2, s2, cs);
+      multiply_jacobi_float<1, 3>(V, c, s);
+    }
+    __syncwarp();
+    rotate = false;
+    p = p_ids[idx+4];
+    q = q_ids[idx+4];
+    a_pq = A[p*4+q];
+    if (fabsf(a_pq) > 0) {
+      rotate = true;
+      const float a_pp = A[p*4+p];
+      const float a_qq = A[q*4+q];
+      compute_c_s_float(a_pq, a_pp, a_qq, c, s, c2, s2, cs);
+    }
+    __syncwarp();
+    if (idx == 0 && rotate) {
+      apply_jacobi_float<0, 3>(A, c, s, c2, s2, cs);
+      multiply_jacobi_float<0, 3>(V, c, s);
+    }
+    __syncwarp();
+    if (idx == 1 && rotate) {
+      apply_jacobi_float<1, 2>(A, c, s, c2, s2, cs);
+      multiply_jacobi_float<1, 2>(V, c, s);
+    }
+    __syncwarp();
+    off_diag_sum =
+      fabs(A[0*4+1]) + fabs(A[0*4+2]) + fabs(A[0*4+3]) +
+      fabs(A[1*4+2]) + fabs(A[1*4+3]) +
+      fabs(A[2*4+3]);
+    // Check the number of iterations
+    ++iteration;
+    if (iteration > JACOBI_MAX_ITERATION) {
+      if (idx == 0 && max_reached) atomicAdd(max_reached, 1);
+      break;
+    }
+  }
+  // Sort
+  float p;
+  if (idx == 0) {
+    int k;
+    // We only require to sort the leading eigenvector to the first column
+    k = 0;
+    p = A[0*4+0];
+    // Find the index of the leading eigenvalue
+    #pragma unroll
+    for (int i0 = 1; i0 < 4; ++i0) {
+      if (A[i0*4+i0] > p) {
+        p = A[i0*4+i0];
+        k = i0;
+      }
+    }
+    if (k != 0) {
+      // Move the leading eigenvalue
+      A[k*4+k] = A[0*4+0];
+      A[0*4+0] = p;
+      // Move the corresponding leading eigenvector
+      #pragma unroll
+      for (int j0 = 0; j0 < 4; ++j0) {
+        p = V[j0*4+0];
+        V[j0*4+0] = V[j0*4+k];
+        V[j0*4+k] = p;
+      }
+    }
+    A_in[0] = V[0];
+    A_in[1] = V[4];
+    A_in[2] = V[8];
+    A_in[3] = V[12];
+    A_in[4] = V[1];
+    A_in[5] = V[5];
+    A_in[6] = V[9];
+    A_in[7] = V[13];
+    A_in[8] = V[2];
+    A_in[9] = V[6];
+    A_in[10] = V[10];
+    A_in[11] = V[14];
+    A_in[12] = V[3];
+    A_in[13] = V[7];
+    A_in[14] = V[11];
+    A_in[15] = V[15];
+    eigvals[0] = A[0*4+0];
+    eigvals[1] = A[1*4+1];
+    eigvals[2] = A[2*4+2];
+    eigvals[3] = A[3*4+3];
+    // printf("eigvals = %lf %lf %lf %lf\n", eigvals[0], eigvals[1], eigvals[2], eigvals[3]);
+    q->q0 = V[0];
+    q->q1 = V[4];
+    q->q2 = V[8];
+    q->q3 = V[12];
+    if (monitor_crossings) {
+      if (q_old->norm2() > 0) {
+        q->match(*q_old);
+        if (q_old->inner(*q) < (1.0 - crossing_threshold)) {
+          atomicAdd(discontinuous_rotation, 1);
+        }
+      }
+    }
+  }
+}
+#endif
+
 #undef JACOBI_MAX_ITERATION
 #undef JACOBI_TOLERANCE
 
@@ -421,6 +662,7 @@ int jacobi_4x4(
     &crossing_threshold, &q_old,
     &discontinuous_rotation};
   cudaKernelNodeParams kernelNodeParams = {0};
+  // kernelNodeParams.func           = (void*)jacobi_4x4_kernel_float32;
   kernelNodeParams.func           = (void*)jacobi_4x4_kernel;
   kernelNodeParams.gridDim        = dim3(1, 1, 1);
   kernelNodeParams.blockDim       = dim3(2, 1, 1);
