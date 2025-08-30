af13da10416fb10f4d2dfa15787c11bb88d332ab
HanatoK <summersnow9403@gmail.com>
2025-08-29 21:14:53 -0500

    refactor: refactor the debug interface and fix the number of atoms of fitting group

--- src/colvaratoms.h
+++ src/colvaratoms.h
@@ -922,7 +922,8 @@ public:
     bool use_cpu_buffers = false);
   // For debug gradients
   int read_positions_gpu_debug(
-    size_t change_atom_i, int xyz, bool to_cpu, double sign, cudaStream_t stream);
+    bool change_fitting_group, size_t change_atom_i,
+    int xyz, bool to_cpu, double sign, cudaStream_t stream);
   int calc_required_properties_gpu_debug(bool to_cpu, cudaStream_t stream);
   void do_feature_side_effects_gpu(int id);
 #elif defined (COLVARS_SYCL)
