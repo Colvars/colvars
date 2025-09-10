c3530e064908f481b7a1ed2797de6d8f5dbd0045
HanatoK <summersnow9403@gmail.com>
2025-09-10 12:52:48 -0500

    test: use clang-tidy-20 in ubuntu

--- devel-tools/run_clang_tidy.sh
+++ devel-tools/run_clang_tidy.sh
@@ -10,6 +10,14 @@ fi
 # Get the build directory
 COLVARS_BUILD_DIR="$(pwd)"
 
+# Get the clang-tidy binary
+if [[ -v CLANG_TIDY ]]; then
+  CLANG_TIDY_BINARY=$CLANG_TIDY
+  echo "Using $CLANG_TIDY_BINARY as clang-tidy"
+else
+  CLANG_TIDY_BINARY="clang-tidy"
+fi
+
 # Get the Colvars source directory
 COLVARS_SOURCE_DIR="$TOPDIR/src"
 # Find all source files that are required to build the run_colvars_test
@@ -21,7 +29,7 @@ num_test_failed=0
 all_output=""
 # Run clang-tidy over all files and save the results
 for colvars_src_file in $COLVARS_SOURCE_FILES; do
-  clang_tidy_command="clang-tidy -p=$COLVARS_BUILD_DIR --warnings-as-errors='*' $colvars_src_file"
+  clang_tidy_command="$CLANG_TIDY_BINARY -p=$COLVARS_BUILD_DIR --warnings-as-errors='*' $colvars_src_file"
   echo "Running $clang_tidy_command"
   output="$(eval $clang_tidy_command 2>&1)" || exit_code=$?
   all_output+="$output"
