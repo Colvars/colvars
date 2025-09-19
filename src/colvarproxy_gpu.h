131f867d0e8557ad88d3535ecf6750f237c647b9
HanatoK <summersnow9403@gmail.com>
2025-09-19 09:50:06 -0500

    feat: implement GPU kernels for cvm::atom_group

--- src/colvarproxy_gpu.h
+++ src/colvarproxy_gpu.h
@@ -29,17 +29,33 @@ public:
   colvars_gpu::gpu_code_t get_gpu_code_type() const {
     return gpu_code_type_used;
   }
+  virtual colvars_gpu::gpu_stream_t get_default_stream() {return 0;}
   int set_gpu_code_type(colvars_gpu::gpu_code_t gpu_code_type_in);
   virtual int create_stream(colvars_gpu::gpu_stream_t* stream, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr);
-  virtual int sync_stream(colvars_gpu::gpu_stream_t* stream);
+  virtual int sync_stream(colvars_gpu::gpu_stream_t stream);
   virtual int sync_all_streams();
   virtual int get_default_device(colvars_gpu::gpu_dev_id_t* device) const;
+  template <typename T>
+  int allocate_host(T **pp, const size_t len) {
+    return allocate_host_T((void **)pp, len, sizeof(T));
+  }
+  template <typename T>
+  int deallocate_host(T **pp) {
+    return deallocate_host_T((void **)pp);
+  }
   template <typename T>
   int allocate_device(T **pp, const size_t len, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr) {
     return allocate_device_T((void **)pp, len, sizeof(T), gpu_id_in);
   }
   template <typename T>
-  int allocate_device_async(T **pp, const size_t len, colvars_gpu::gpu_stream_t* stream, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr) {
+  int reallocate_device(T **pp, const size_t len, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr) {
+    int error_code = COLVARS_OK;
+    error_code |= deallocate_device(pp);
+    error_code |= allocate_device_T((void **)pp, len, sizeof(T), gpu_id_in);
+    return error_code;
+  }
+  template <typename T>
+  int allocate_device_async(T **pp, const size_t len, colvars_gpu::gpu_stream_t stream, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr) {
     return allocate_device_T_async((void **)pp, len, sizeof(T), stream, gpu_id_in);
   }
   template <typename T>
@@ -47,23 +63,55 @@ public:
     return deallocate_device_T((void **)pp);
   }
   template <typename T>
-  int deallocate_device_async(T **pp, colvars_gpu::gpu_stream_t* stream) {
+  int deallocate_device_async(T **pp, colvars_gpu::gpu_stream_t stream) {
     return deallocate_device_T_async((void **)pp, stream);
   }
   template <typename T>
   int clear_device_array(T *data, const size_t ndata) {
-    clear_device_array_T(data, ndata, sizeof(T));
+    return clear_device_array_T(data, ndata, sizeof(T));
   }
   template <typename T>
-  int clear_device_array_async(T *data, const size_t ndata, colvars_gpu::gpu_stream_t* stream) {
-    clear_device_array_T_async(data, ndata, sizeof(T), stream);
+  int clear_device_array_async(T *data, const size_t ndata, colvars_gpu::gpu_stream_t stream) {
+    return clear_device_array_T_async(data, ndata, sizeof(T), stream);
   }
+  template <typename T>
+  int copy_HtoD(const T *h_array, T *d_array, size_t array_len) {
+    return copy_HtoD_T(h_array, d_array, array_len, sizeof(T));
+  }
+  template <typename T>
+  int copy_HtoD_async(const T *h_array, T *d_array, size_t array_len, colvars_gpu::gpu_stream_t stream) {
+    return copy_HtoD_T_async(h_array, d_array, array_len, sizeof(T), stream);
+  }
+  template <typename T>
+  int copy_DtoH(const T *d_array, T *h_array, size_t array_len) {
+    return copy_DtoH_T(d_array, h_array, array_len, sizeof(T));
+  }
+  template <typename T>
+  int copy_DtoH_async(const T *d_array, T *h_array, size_t array_len, colvars_gpu::gpu_stream_t stream) {
+    return copy_DtoH_T_async(d_array, h_array, array_len, sizeof(T), stream);
+  }
+  template <typename T>
+  int copy_DtoD(const T *d_src, T *d_dst, size_t array_len) {
+    return copy_DtoD_T(d_src, d_dst, array_len, sizeof(T));
+  }
+  template <typename T>
+  int copy_DtoD_async(const T *d_src, T *d_dst, size_t array_len, colvars_gpu::gpu_stream_t stream) {
+    return copy_DtoD_T_async(d_src, d_dst, array_len, sizeof(T), stream);
+  }
+  virtual int allocate_host_T(void **pp, const size_t len, const size_t sizeofT);
+  virtual int deallocate_host_T(void **pp);
   virtual int allocate_device_T(void **pp, const size_t len, const size_t sizeofT, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr);
   virtual int deallocate_device_T(void **pp);
   virtual int clear_device_array_T(void *data, const size_t ndata, const size_t sizeofT);
-  virtual int allocate_device_T_async(void **pp, const size_t len, const size_t sizeofT, colvars_gpu::gpu_stream_t* stream, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr);
-  virtual int deallocate_device_T_async(void **pp, colvars_gpu::gpu_stream_t* stream);
-  virtual int clear_device_array_T_async(void *data, const size_t ndata, const size_t sizeofT, colvars_gpu::gpu_stream_t* stream);
+  virtual int allocate_device_T_async(void **pp, const size_t len, const size_t sizeofT, colvars_gpu::gpu_stream_t stream, colvars_gpu::gpu_dev_id_t* gpu_id_in = nullptr);
+  virtual int deallocate_device_T_async(void **pp, colvars_gpu::gpu_stream_t stream);
+  virtual int clear_device_array_T_async(void *data, const size_t ndata, const size_t sizeofT, colvars_gpu::gpu_stream_t stream);
+  virtual int copy_HtoD_T(const void *h_array, void *d_array, size_t array_len, const size_t sizeofT);
+  virtual int copy_HtoD_T_async(const void *h_array, void *d_array, size_t array_len, const size_t sizeofT, colvars_gpu::gpu_stream_t stream);
+  virtual int copy_DtoH_T(const void *d_array, void *h_array, size_t array_len, const size_t sizeofT);
+  virtual int copy_DtoH_T_async(const void *d_array, void *h_array, size_t array_len, const size_t sizeofT, colvars_gpu::gpu_stream_t stream);
+  virtual int copy_DtoD_T(const void *d_src, void *d_dst, size_t array_len, const size_t sizeofT);
+  virtual int copy_DtoD_T_async(const void *d_src, void *d_dst, size_t array_len, const size_t sizeofT, colvars_gpu::gpu_stream_t stream);
   virtual float* proxy_atoms_masses_gpu_float() {return nullptr;}
   virtual float* proxy_atoms_charges_gpu_float() {return nullptr;}
   virtual cvm::real* proxy_atoms_masses_gpu() {return nullptr;}
