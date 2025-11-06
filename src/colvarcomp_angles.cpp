0050b1ce884e22338552619215b8f9526a446c2f
HanatoK <summersnow9403@gmail.com>
2025-11-06 12:43:40 -0600

    fix: remove the cvm::log line that was intended only for debugging

--- src/colvarcomp_angles.cpp
+++ src/colvarcomp_angles.cpp
@@ -157,7 +157,6 @@ void colvar::dipole_angle::calc_value()
   group1->calc_dipole(g1_pos);
 
   r21 = group1->dipole();
-  cvm::log("r21 = " + cvm::to_str(r21) + " ; g1_pos = " + cvm::to_str(g1_pos) + "\n");
   r21l = r21.norm();
   r23  = is_enabled(f_cvc_pbc_minimum_image) ?
     cvm::position_distance(g2_pos, g3_pos) :
