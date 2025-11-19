29d2753bd5860907d9cbdf4986e9cdd0c71cf26b
Jérôme Hénin <heninj@gmail.com>
2025-11-19 14:26:30 +0100

    chore: undo formatting changes

--- src/colvargrid.cpp
+++ src/colvargrid.cpp
@@ -10,8 +10,8 @@
 #include <ctime>
 
 #include "colvarmodule.h"
-#include "colvarparse.h"
 #include "colvarvalue.h"
+#include "colvarparse.h"
 #include "colvar.h"
 #include "colvargrid.h"
 #include "colvargrid_def.h"
@@ -41,22 +41,22 @@ int colvar_grid_count::parse_params(std::string const &conf,
   return colvar_grid<size_t>::parse_params(conf, parse_mode);
 }
 
-std::istream &colvar_grid_count::read_restart(std::istream &is)
+std::istream & colvar_grid_count::read_restart(std::istream &is)
 {
   return colvar_grid<size_t>::read_restart(is);
 }
 
-cvm::memory_stream &colvar_grid_count::read_restart(cvm::memory_stream &is)
+cvm::memory_stream & colvar_grid_count::read_restart(cvm::memory_stream &is)
 {
   return colvar_grid<size_t>::read_restart(is);
 }
 
-std::ostream &colvar_grid_count::write_restart(std::ostream &os)
+std::ostream & colvar_grid_count::write_restart(std::ostream &os)
 {
   return colvar_grid<size_t>::write_restart(os);
 }
 
-cvm::memory_stream &colvar_grid_count::write_restart(cvm::memory_stream &os)
+cvm::memory_stream & colvar_grid_count::write_restart(cvm::memory_stream &os)
 {
   return colvar_grid<size_t>::write_restart(os);
 }
@@ -82,41 +82,48 @@ cvm::memory_stream &colvar_grid_count::write_raw(cvm::memory_stream &os,
   return colvar_grid<size_t>::write_raw(os, buf_size);
 }
 
-std::istream &colvar_grid_count::read_multicol(std::istream &is, bool add)
+std::istream & colvar_grid_count::read_multicol(std::istream &is, bool add)
 {
   return colvar_grid<size_t>::read_multicol(is, add);
 }
 
-int colvar_grid_count::read_multicol(std::string const &filename, std::string description, bool add)
+int colvar_grid_count::read_multicol(std::string const &filename,
+                                     std::string description,
+                                     bool add)
 {
   return colvar_grid<size_t>::read_multicol(filename, description, add);
 }
 
-std::ostream &colvar_grid_count::write_multicol(std::ostream &os) const
+std::ostream & colvar_grid_count::write_multicol(std::ostream &os) const
 {
   return colvar_grid<size_t>::write_multicol(os);
 }
 
-int colvar_grid_count::write_multicol(std::string const &filename, std::string description) const
+int colvar_grid_count::write_multicol(std::string const &filename,
+                                      std::string description) const
 {
   return colvar_grid<size_t>::write_multicol(filename, description);
 }
 
-std::ostream &colvar_grid_count::write_opendx(std::ostream &os) const
+std::ostream & colvar_grid_count::write_opendx(std::ostream &os) const
 {
   return colvar_grid<size_t>::write_opendx(os);
 }
 
-int colvar_grid_count::write_opendx(std::string const &filename, std::string description) const
+int colvar_grid_count::write_opendx(std::string const &filename,
+                                    std::string description) const
 {
   return colvar_grid<size_t>::write_opendx(filename, description);
 }
 
 
-colvar_grid_scalar::colvar_grid_scalar() : colvar_grid<cvm::real>(), samples(NULL) {}
+
+colvar_grid_scalar::colvar_grid_scalar()
+  : colvar_grid<cvm::real>(), samples(NULL)
+{}
 
 colvar_grid_scalar::colvar_grid_scalar(colvar_grid_scalar const &g)
-    : colvar_grid<cvm::real>(g), samples(NULL)
+  : colvar_grid<cvm::real>(g), samples(NULL)
 {
 }
 
@@ -144,7 +151,7 @@ std::string colvar_grid_scalar::get_state_params() const
 }
 
 int colvar_grid_scalar::parse_params(std::string const &conf,
-                                     colvarparse::Parse_Mode const parse_mode)
+                                    colvarparse::Parse_Mode const parse_mode)
 {
   return colvar_grid<cvm::real>::parse_params(conf, parse_mode);
 }
@@ -190,33 +197,36 @@ cvm::memory_stream &colvar_grid_scalar::write_raw(cvm::memory_stream &os,
   return colvar_grid<cvm::real>::write_raw(os, buf_size);
 }
 
-std::istream &colvar_grid_scalar::read_multicol(std::istream &is, bool add)
+std::istream & colvar_grid_scalar::read_multicol(std::istream &is, bool add)
 {
   return colvar_grid<cvm::real>::read_multicol(is, add);
 }
 
-int colvar_grid_scalar::read_multicol(std::string const &filename, std::string description,
+int colvar_grid_scalar::read_multicol(std::string const &filename,
+                                      std::string description,
                                       bool add)
 {
   return colvar_grid<cvm::real>::read_multicol(filename, description, add);
 }
 
-std::ostream &colvar_grid_scalar::write_multicol(std::ostream &os) const
+std::ostream & colvar_grid_scalar::write_multicol(std::ostream &os) const
 {
   return colvar_grid<cvm::real>::write_multicol(os);
 }
 
-int colvar_grid_scalar::write_multicol(std::string const &filename, std::string description) const
+int colvar_grid_scalar::write_multicol(std::string const &filename,
+                                       std::string description) const
 {
   return colvar_grid<cvm::real>::write_multicol(filename, description);
 }
 
-std::ostream &colvar_grid_scalar::write_opendx(std::ostream &os) const
+std::ostream & colvar_grid_scalar::write_opendx(std::ostream &os) const
 {
   return colvar_grid<cvm::real>::write_opendx(os);
 }
 
-int colvar_grid_scalar::write_opendx(std::string const &filename, std::string description) const
+int colvar_grid_scalar::write_opendx(std::string const &filename,
+                                     std::string description) const
 {
   return colvar_grid<cvm::real>::write_opendx(filename, description);
 }
@@ -226,8 +236,7 @@ cvm::real colvar_grid_scalar::maximum_value() const
 {
   cvm::real max = data[0];
   for (size_t i = 0; i < nt; i++) {
-    if (data[i] > max)
-      max = data[i];
+    if (data[i] > max) max = data[i];
   }
   return max;
 }
@@ -237,8 +246,7 @@ cvm::real colvar_grid_scalar::minimum_value() const
 {
   cvm::real min = data[0];
   for (size_t i = 0; i < nt; i++) {
-    if (data[i] < min)
-      min = data[i];
+    if (data[i] < min) min = data[i];
   }
   return min;
 }
@@ -248,14 +256,13 @@ cvm::real colvar_grid_scalar::minimum_pos_value() const
   cvm::real minpos = data[0];
   size_t i;
   for (i = 0; i < nt; i++) {
-    if (data[i] > 0) {
+    if(data[i] > 0) {
       minpos = data[i];
       break;
     }
   }
   for (i = 0; i < nt; i++) {
-    if (data[i] > 0 && data[i] < minpos)
-      minpos = data[i];
+    if (data[i] > 0 && data[i] < minpos) minpos = data[i];
   }
   return minpos;
 }
@@ -278,7 +285,7 @@ cvm::real colvar_grid_scalar::entropy() const
 {
   cvm::real sum = 0.0;
   for (size_t i = 0; i < nt; i++) {
-    if (data[i] > 0) {
+    if (data[i] >0) {
       sum += -1.0 * data[i] * cvm::logn(data[i]);
     }
   }
@@ -289,14 +296,13 @@ cvm::real colvar_grid_scalar::entropy() const
   return bin_volume * sum;
 }
 
-
 /// \brief Return the RMSD between this grid's data and another one
 /// Grids must have the same dimensions.
 cvm::real colvar_grid_scalar::grid_rmsd(colvar_grid_scalar const &other_grid) const
 {
   if (other_grid.data.size() != this->data.size()) {
     cvm::error("Error: trying to subtract two grids with "
-               "different size.\n");
+                "different size.\n");
     return -1.;
   }
 
@@ -307,18 +313,18 @@ cvm::real colvar_grid_scalar::grid_rmsd(colvar_grid_scalar const &other_grid) co
       size_t n = samples->get_value(i);
       cvm::real us = n ? data[i] / n : 0.0;
       n = other_grid.samples->get_value(i);
-      cvm::real them = n ? other_grid.data[i] / n : 0.0;
+      cvm::real them = n ? other_grid.data[i ] / n : 0.0;
       cvm::real d = us - them;
-      sum2 += d * d;
+      sum2 += d*d;
     }
   } else {
     for (size_t i = 0; i < data.size(); i++) {
       cvm::real d = other_grid.data[i] - data[i];
-      sum2 += d * d;
+      sum2 += d*d;
     }
   }
 
-  return sqrt(sum2 / this->data.size());
+  return sqrt(sum2/this->data.size());
 }
 
 
@@ -404,33 +410,36 @@ cvm::memory_stream &colvar_grid_gradient::write_raw(cvm::memory_stream &os,
   return colvar_grid<cvm::real>::write_raw(os, buf_size);
 }
 
-std::istream &colvar_grid_gradient::read_multicol(std::istream &is, bool add)
+std::istream & colvar_grid_gradient::read_multicol(std::istream &is, bool add)
 {
   return colvar_grid<cvm::real>::read_multicol(is, add);
 }
 
-int colvar_grid_gradient::read_multicol(std::string const &filename, std::string description,
+int colvar_grid_gradient::read_multicol(std::string const &filename,
+                                        std::string description,
                                         bool add)
 {
   return colvar_grid<cvm::real>::read_multicol(filename, description, add);
 }
 
-std::ostream &colvar_grid_gradient::write_multicol(std::ostream &os) const
+std::ostream & colvar_grid_gradient::write_multicol(std::ostream &os) const
 {
   return colvar_grid<cvm::real>::write_multicol(os);
 }
 
-int colvar_grid_gradient::write_multicol(std::string const &filename, std::string description) const
+int colvar_grid_gradient::write_multicol(std::string const &filename,
+                                         std::string description) const
 {
   return colvar_grid<cvm::real>::write_multicol(filename, description);
 }
 
-std::ostream &colvar_grid_gradient::write_opendx(std::ostream &os) const
+std::ostream & colvar_grid_gradient::write_opendx(std::ostream &os) const
 {
   return colvar_grid<cvm::real>::write_opendx(os);
 }
 
-int colvar_grid_gradient::write_opendx(std::string const &filename, std::string description) const
+int colvar_grid_gradient::write_opendx(std::string const &filename,
+                                       std::string description) const
 {
   return colvar_grid<cvm::real>::write_opendx(filename, description);
 }
@@ -470,20 +479,22 @@ void colvar_grid_gradient::write_1D_integral(std::ostream &os)
       integral += (value(ix) - corr) * cv[0]->width;
     }
 
-    if (integral < min)
-      min = integral;
+    if ( integral < min ) min = integral;
     int_vals.push_back(integral);
   }
 
   bin = 0.0;
-  for (int i = 0; i < nx[0]; i++, bin += 1.0) {
+  for ( int i = 0; i < nx[0]; i++, bin += 1.0 ) {
     os << std::setw(10) << cv[0]->lower_boundary.real_value + cv[0]->width * bin << " "
-       << std::setw(cvm::cv_width) << std::setprecision(cvm::cv_prec) << int_vals[i] - min << "\n";
+       << std::setw(cvm::cv_width)
+       << std::setprecision(cvm::cv_prec)
+       << int_vals[i] - min << "\n";
   }
 
   os << std::setw(10) << cv[0]->lower_boundary.real_value + cv[0]->width * bin << " "
-     << std::setw(cvm::cv_width) << std::setprecision(cvm::cv_prec) << int_vals[nx[0]] - min
-     << "\n";
+     << std::setw(cvm::cv_width)
+     << std::setprecision(cvm::cv_prec)
+     << int_vals[nx[0]] - min << "\n";
 
   return;
 }
@@ -495,13 +506,13 @@ cvm::real colvar_grid_gradient::grid_rmsd(colvar_grid_gradient const &other_grid
 {
   if (other_grid.multiplicity() != this->multiplicity()) {
     cvm::error("Error: trying to subtract two grids with "
-               "different multiplicity.\n");
+                "different multiplicity.\n");
     return -1.;
   }
 
   if (other_grid.data.size() != this->data.size()) {
     cvm::error("Error: trying to subtract two grids with "
-               "different size.\n");
+                "different size.\n");
     return -1.;
   }
 
@@ -511,9 +522,9 @@ cvm::real colvar_grid_gradient::grid_rmsd(colvar_grid_gradient const &other_grid
   for (ix = new_index(); index_ok(ix); incr(ix)) {
     for (imult = 0; imult < this->multiplicity(); imult++) {
       cvm::real d = this->value_output(ix, imult) - other_grid.value_output(ix, imult);
-      sum2 += d * d;
+      sum2 += d*d;
     }
   }
-  return sqrt(sum2 / this->data.size());
+  return sqrt(sum2/this->data.size());
 }
 
