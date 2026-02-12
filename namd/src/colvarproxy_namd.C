94ca4f45570dc0ec0b6c7f50671e5b51c76315fd
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2026-02-12 16:50:10 -0500

    fix: Use Lattice::wrap_nearest_delta() for triclinic unit cells in NAMD

--- namd/src/colvarproxy_namd.C
+++ namd/src/colvarproxy_namd.C
@@ -890,16 +890,16 @@ void colvarproxy_namd::update_atom_properties(int index)
 
 
 cvm::rvector colvarproxy_namd::position_distance(cvm::atom_pos const &pos1,
-                                                 cvm::atom_pos const &pos2)
-  const
+                                                 cvm::atom_pos const &pos2) const
 {
   Position const p1(pos1.x, pos1.y, pos1.z);
   Position const p2(pos2.x, pos2.y, pos2.z);
-  // return p2 - p1
-  Vector const d = globalmaster->get_lattice()->delta(p2, p1);
+  Lattice const *lattice = globalmaster->get_lattice();
+  // For triclinic cells, use Lattice::wrap_nearest_delta instead (computes the shift vector)
+  Vector const d = lattice->orthogonal() ? lattice->delta(p2, p1)
+                                         : ((p2 - p1) + lattice->wrap_nearest_delta(p2 - p1));
   return cvm::rvector(d.x, d.y, d.z);
 }
-
 
 
 enum e_pdb_field {
