b0f433083db31831b03264af6f235184a2f2db30
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2026-09-09 10:07:54 -0400

    fix: Enforce smp off for reusable CVCs

--- src/colvarcomp.cpp
+++ src/colvarcomp.cpp
@@ -410,11 +410,12 @@ int colvar::cvc::setup()
 {
   int error_code = COLVARS_OK;
   if (is_enabled(f_cvc_reusing_cvcs)) {
-    if (cvmodule->proxy->get_smp_mode() == colvarproxy_smp::smp_mode_t::cvcs) {
+    if (cvmodule->proxy->get_smp_mode() != colvarproxy_smp::smp_mode_t::none) {
       error_code |= cvmodule->error(
           "reusable components are currently not compatible with SMP object-based parallelism"
-          "(\"smp on\" or \"smp cvcs\"); please use \"smp off\" or other value",
-         COLVARS_INPUT_ERROR);
+          "(\"smp on\" or \"smp cvcs\"); please disable it by using \"smp off\" as a global "
+          "keyword",
+          COLVARS_INPUT_ERROR);
     }
     for (auto ci = precomputed_cvcs.begin(); ci != precomputed_cvcs.end(); ci++) {
       if (ci->first != ci->second->function_type()) {
